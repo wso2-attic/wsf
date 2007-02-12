4371b443fe8fe114199659e3d0ae0d05434ddfb5
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-02-12 06:19:01 +0000

    Changed the prefix
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@857 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -1,61 +1,39 @@
 #!/bin/bash
-#./autogen.sh
 
 set -e
 
-if [ -d  "wsf_c" ];then
-    cd wsf_c
-    svn up
-    cd ..
-fi
-
-cd wsf_c
-
-if [ -d  "axis2c" ];then
-    cd axis2c;
-    svn up;
-    cd ..
-    cp build/axis2/Makefile.am axis2c
-fi
+svn up;
+#cp build/axis2c/Makefile.am axis2c
 
 
-#if [ -d  "sandesha2" ];then
-#    cd sandesha2;
-#    svn up;
-#    cd ..
-#else
-#    svn co https://svn.apache.org/repos/asf/webservices/sandesha/trunk/c;
-#    mv c sandesha2;
-#    cp build/sandesha2/Makefile.am sandesha2
-#    cp build/sandesha2/src/core/Makefile.am sandesha2/src/core
-#    cp build/sandesha2/src/util/Makefile.am sandesha2/src/util
-#    cp build/sandesha2/src/wsrm/Makefile.am sandesha2/src/wsrm
-#    cp build/sandesha2/src/transport/Makefile.am sandesha2/src/transport
-#    cp build/sandesha2/src/handlers/Makefile.am sandesha2/src/handlers
-#    cp build/sandesha2/src/storage/Makefile.am sandesha2/src/storage
-#    cp build/sandesha2/src/storage/permanent/Makefile.am sandesha2/src/storage/permanent
-#    cp build/sandesha2/src/storage/beans/Makefile.am sandesha2/src/storage/beans
-#    cp build/sandesha2/src/storage/inmemory/Makefile.am sandesha2/src/storage/inmemory
-#    cp build/sandesha2/src/client/Makefile.am sandesha2/src/client
-#    cp build/sandesha2/src/msgprocessors/Makefile.am sandesha2/src/msgprocessors
-#    cp build/sandesha2/src/polling/Makefile.am sandesha2/src/polling
-#    cp build/sandesha2/src/workers/Makefile.am sandesha2/src/workers
-#    cp build/sandesha2/autogen.sh sandesha2
-#fi
+cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c
+cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
+cp build/wsf_c/sandesha2c/src/util/Makefile.am wsf_c/sandesha2c/src/util
+cp build/wsf_c/sandesha2c/src/wsrm/Makefile.am wsf_c/sandesha2c/src/wsrm
+cp build/wsf_c/sandesha2c/src/transport/Makefile.am wsf_c/sandesha2c/src/transport
+cp build/wsf_c/sandesha2c/src/handlers/Makefile.am wsf_c/sandesha2c/src/handlers
+cp build/wsf_c/sandesha2c/src/storage/Makefile.am wsf_c/sandesha2c/src/storage
+cp build/wsf_c/sandesha2c/src/storage/permanent/Makefile.am wsf_c/sandesha2c/src/storage/permanent
+cp build/wsf_c/sandesha2c/src/storage/beans/Makefile.am wsf_c/sandesha2c/src/storage/beans
+cp build/wsf_c/sandesha2c/src/storage/inmemory/Makefile.am wsf_c/sandesha2c/src/storage/inmemory
+cp build/wsf_c/sandesha2c/src/client/Makefile.am wsf_c/sandesha2c/src/client
+cp build/wsf_c/sandesha2c/src/msgprocessors/Makefile.am wsf_c/sandesha2c/src/msgprocessors
+cp build/wsf_c/sandesha2c/src/polling/Makefile.am wsf_c/sandesha2c/src/polling
+cp build/wsf_c/sandesha2c/src/workers/Makefile.am wsf_c/sandesha2c/src/workers
+cp build/wsf_c/sandesha2c/autogen.sh wsf_c/sandesha2c
 
-cd ..
 
-cp build/wsf_c/axis2/configure.ac wsf_c/axis2
-cp build/wsf_c/axis2/rampart/samples/Makefile.am wsf_c/axis2/rampart/samples
-cp build/wsf_c/axis2/rampart/samples/callback/Makefile.am wsf_c/axis2/rampart/samples/callback
-cp build/wsf_c/axis2/rampart/samples/callback/htpasswd_callback/Makefile.am wsf_c/axis2/rampart/samples/callback/htpasswd_callback
-cp build/wsf_c/axis2/rampart/configure.ac wsf_c/axis2/rampart
-cp build/wsf_c/axis2/rampart/Makefile.am wsf_c/axis2/rampart
-cp build/wsf_c/axis2/rampart/autogen.sh wsf_c/axis2/rampart
-#cp build/wsf_c/sandesha2/src/core/Makefile.am wsf_c/sandesha2/src/core
-#cp build/wsf_c/sandesha2/Makefile.am wsf_c/sandesha2/
-cp build/wsf_c/axis2/Makefile.am wsf_c/axis2/
-cp wsf_c/axis2/samples/server/axis2.xml .
+cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c
+cp build/wsf_c/axis2c/rampart/samples/Makefile.am wsf_c/axis2c/rampart/samples
+cp build/wsf_c/axis2c/rampart/samples/callback/Makefile.am wsf_c/axis2c/rampart/samples/callback
+cp build/wsf_c/axis2c/rampart/samples/callback/htpasswd_callback/Makefile.am wsf_c/axis2c/rampart/samples/callback/htpasswd_callback
+cp build/wsf_c/axis2c/rampart/configure.ac wsf_c/axis2c/rampart
+cp build/wsf_c/axis2c/rampart/Makefile.am wsf_c/axis2c/rampart
+cp build/wsf_c/axis2c/rampart/autogen.sh wsf_c/axis2c/rampart
+cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
+cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c/
+cp build/wsf_c/axis2c/Makefile.am wsf_c/axis2c/
+cp wsf_c/axis2c/samples/server/axis2.xml .
 
-./autogen.sh && ./configure --with-wsf && make
+./autogen.sh && ./configure --with-wsf --prefix=`php-config --extension-dir`/wsf_c && make
 
