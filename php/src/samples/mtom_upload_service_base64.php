11370489c68eff0b2e4f1df74d9963ba81174da8
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-02-19 08:58:51 +0000

    sample added on base64 image saving
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@950 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/src/samples/mtom_upload_service_base64.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function getAttachment($inMessage) {
+    
+    $cid2stringMap = $inMessage->attachments;
+    $cid2contentMap = $inMessage->cid2contentType;
+    $imageName;
+    
+    $dom = new DomDocument();
+    $dom->loadXML($resPayload);
+    $images = $dom->documentElement->getElementsByTagName('image');
+    $image= $images->item(0);
+    file_put_contents("base64image.txt",$image->nodeValue);
+
+    $resPayload = <<<XML
+     <ns1:response xmlns:ns1="http://php.axis2.org/samples/mtom">Image Saved</ns1:response>
+    XML;
+
+    $returnMessage = new WSMessage($resPayload);
+
+    return $returnMessage;
+}
+
+$operations = array("upload" => "getAttachment");
+
+$server = new WSService(
+            array(
+                "operations" => $operations, 
+                "requestXOP"=>TRUE));
+        
+$server->reply();
+
+?>
