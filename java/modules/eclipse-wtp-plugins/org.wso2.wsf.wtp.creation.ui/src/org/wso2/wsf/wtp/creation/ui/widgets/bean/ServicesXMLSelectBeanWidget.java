00e3a6b5376eb16dc528ed4fe89859b141e396df
sandakith <sandakith@a5903396-d722-0410-b921-86c7d4935375>
2007-04-16 11:42:17 +0000

    wsf eclipse creation ui plugin
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@1844 a5903396-d722-0410-b921-86c7d4935375

---
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.wtp.creation.ui/src/org/wso2/wsf/wtp/creation/ui/widgets/bean/ServicesXMLSelectBeanWidget.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.wsf.wtp.creation.ui.widgets.bean;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wst.command.internal.env.ui.widgets.SimpleWidgetDataContributor;
+import org.eclipse.wst.command.internal.env.ui.widgets.WidgetDataEvents;
+import org.wso2.wsf.wtp.creation.core.data.DataModel;
+import org.wso2.wsf.wtp.creation.core.messages.WSASCreationUIMessages;
+
+
+public class ServicesXMLSelectBeanWidget extends SimpleWidgetDataContributor 
+{
+	private DataModel 	model;
+	private Button	 	browseButton;
+	private Text 		servicesXMLPath;
+	private Button 		generateServicesXML;
+	private Button 		haveServicesXML;
+
+	public ServicesXMLSelectBeanWidget( DataModel model )
+	{
+		this.model = model;  
+	}
+
+	public WidgetDataEvents addControls( Composite parent, final Listener statusListener )
+	{
+		final Composite  mainComp = new Composite( parent, SWT.NONE );
+		GridLayout layout   = new GridLayout();
+		layout.numColumns = 3;
+		GridData   griddata = new GridData( GridData.FILL_HORIZONTAL );
+		Label      label    = new Label( mainComp, SWT.NONE );
+
+		griddata = new GridData( GridData.FILL_HORIZONTAL );
+		griddata.horizontalSpan = 3;
+		mainComp.setLayout( layout );
+		mainComp.setLayoutData( griddata );
+		label.setText( WSASCreationUIMessages.LABEL_WSAS_PREFERENCE_PAGE );
+		model.setGenerateServicesXML(true);
+		model.setServicesXML(false);
+
+		griddata = new GridData( GridData.FILL_HORIZONTAL );
+		griddata.horizontalSpan = 3;
+		haveServicesXML = new Button( mainComp, SWT.CHECK );
+		haveServicesXML.setText(WSASCreationUIMessages.LABEL_HAVE_SERVICES_XML_FILE);	
+		haveServicesXML.setLayoutData(griddata);
+		haveServicesXML.addSelectionListener( new SelectionAdapter()
+		{
+			public void widgetSelected(SelectionEvent e)
+			{
+				if (haveServicesXML.getSelection()){
+					haveServicesXML.setSelection(true);
+					generateServicesXML.setSelection(false);
+					enableServicesXMLBrowse();
+				}else{
+					if (!generateServicesXML.getSelection()){
+						haveServicesXML.setSelection(true); 
+						generateServicesXML.setSelection(false);
+						enableServicesXMLBrowse();
+					}else{
+						haveServicesXML.setSelection(false);
+						disableServicesXMLBrowse();
+					}
+				}
+				model.setGenerateServicesXML(false);
+				model.setServicesXML(true);
+
+				// Need to trigger a validation at this point to ensure
+				// that the next button is enabled properly just in case
+				// this is the last page in the wizard.
+				statusListener.handleEvent( null );
+			}     
+		});
+		griddata = new GridData( GridData.FILL_HORIZONTAL );
+		griddata.horizontalSpan = 2;
+
+		servicesXMLPath = new Text( mainComp, SWT.CHECK );
+		servicesXMLPath.setText("");
+		servicesXMLPath.setLayoutData(griddata);
+		servicesXMLPath.addModifyListener( new ModifyListener()
+		{
+			public void modifyText(ModifyEvent e)
+			{
+				model.setPathToServicesXML( servicesXMLPath.getText() );
+				// Need to trigger a validation at this point to ensure
+				// that the next button is enabled properly just in case
+				// this is the last page in the wizard.
+				statusListener.handleEvent( null );
+			}
+		});
+
+		griddata = new GridData( GridData.FILL_HORIZONTAL );
+		griddata.horizontalSpan = 1;
+		browseButton = new Button( mainComp, SWT.NULL );
+		browseButton.setText(WSASCreationUIMessages.LABEL_BROWSE);
+		browseButton.setLayoutData(griddata);
+		browseButton.addSelectionListener( new SelectionAdapter()
+		{
+			public void widgetSelected(SelectionEvent e)
+			{
+				handleBrowse(mainComp.getShell());
+				// Need to trigger a validation at this point to ensure
+				// that the next button is enabled properly just in case
+				// this is the last page in the wizard.
+				statusListener.handleEvent( null );
+			}     
+		}); 
+
+
+		griddata = new GridData( GridData.FILL_HORIZONTAL );
+		griddata.horizontalSpan = 3;
+		generateServicesXML = new Button( mainComp, SWT.CHECK );
+		generateServicesXML.setText( WSASCreationUIMessages.LABEL_DEFAULT_SERVICES_XML_FILE );
+		generateServicesXML.setLayoutData(griddata);
+		generateServicesXML.setSelection(true);
+		generateServicesXML.addSelectionListener( new SelectionAdapter()
+		{
+			public void widgetSelected(SelectionEvent e)
+			{
+				if (generateServicesXML.getSelection()){
+					generateServicesXML.setSelection(true); 
+					haveServicesXML.setSelection(false);
+					disableServicesXMLBrowse();
+				}else{
+					if (!haveServicesXML.getSelection()){
+						generateServicesXML.setSelection(true); 
+						haveServicesXML.setSelection(false);
+					}else{
+						generateServicesXML.setSelection(false);
+					}
+				}
+				model.setGenerateServicesXML(true);
+				model.setServicesXML(false);
+				// Need to trigger a validation at this point to ensure
+				// that the next button is enabled properly just in case
+				// this is the last page in the wizard.
+				statusListener.handleEvent( null );
+			}     
+		}); 
+
+		disableServicesXMLBrowse();
+
+		return this;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.wst.command.env.ui.widgets.WidgetContributor#getStatus()
+	 */
+	public IStatus getStatus() 
+	{
+		IStatus result = null;
+
+		if( haveServicesXML.getSelection() && servicesXMLPath.getText().equals("") )
+		{
+			result = new Status( IStatus.ERROR, 
+								"id", 
+								0, 
+								WSASCreationUIMessages.ERROR_INVALID_SERVICES_XML, 
+								null ); 
+		}
+
+		return result;
+	}
+
+	/**
+	 * enable the services.xml text and browse button
+	 */
+	private void enableServicesXMLBrowse(){
+		browseButton.setEnabled(true);
+		servicesXMLPath.setEnabled(true);
+	}
+
+	/**
+	 * disable the services.xml text and browse button
+	 */
+	private void disableServicesXMLBrowse(){
+		browseButton.setEnabled(false);
+		servicesXMLPath.setEnabled(false);
+	}
+
+	/**
+	 * Pops up the file browse dialog box
+	 */
+	private void handleBrowse(Shell parent) {
+		FileDialog fileDialog = new FileDialog(parent);
+		fileDialog.setFilterExtensions(new String[] { WSASCreationUIMessages.FILE_XML });
+		String fileName = fileDialog.open();
+		if (fileName != null) {
+			servicesXMLPath.setText(fileName);
+			model.setPathToServicesXML( servicesXMLPath.getText() );
+		}
+	}
+}
+
+
+
---
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.wtp.creation.ui/src/org/wso2/wsf/wtp/creation/ui/wsrt/WSASWebService.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.wsf.wtp.creation.ui.wsrt;
+
+import java.util.Vector;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.wst.command.internal.env.core.ICommandFactory;
+import org.eclipse.wst.command.internal.env.core.SimpleCommandFactory;
+import org.eclipse.wst.common.environment.IEnvironment;
+import org.eclipse.wst.ws.internal.wsrt.AbstractWebService;
+import org.eclipse.wst.ws.internal.wsrt.IContext;
+import org.eclipse.wst.ws.internal.wsrt.ISelection;
+import org.eclipse.wst.ws.internal.wsrt.WebServiceInfo;
+import org.eclipse.wst.ws.internal.wsrt.WebServiceScenario;
+import org.wso2.wsf.wtp.creation.core.command.WSASBUCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASBUServiceCreationCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASBuildProjectCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASDefaultingCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASServicesXMLValidationCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASSkelImplCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASTDCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASTDServiceCreationCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASWSDL2JavaCommand;
+import org.wso2.wsf.wtp.creation.core.command.WSASWebservicesServerCommand;
+import org.wso2.wsf.wtp.creation.core.data.DataModel;
+
+public class WSASWebService extends AbstractWebService
+{
+//	private WSASWebServiceInfo wsasWebServiceInfo;
+
+	public WSASWebService(WebServiceInfo info)
+	{
+		super(info);
+	}
+
+	public ICommandFactory assemble(IEnvironment env, IContext ctx,
+			ISelection sel, String project, String earProject)
+	{
+		return null;
+	}
+
+	public ICommandFactory deploy(IEnvironment env, IContext ctx, ISelection sel,
+			String project, String earProject)
+	{
+		return null;
+	}
+
+	public ICommandFactory develop(IEnvironment env, IContext ctx, ISelection sel,
+			String project, String earProject)
+	{
+		Vector commands = new Vector();
+		DataModel model = new DataModel();
+		//EclipseEnvironment environment = (EclipseEnvironment)env;
+
+		model.setWebProjectName(project);
+
+		if (ctx.getScenario().getValue() == WebServiceScenario.BOTTOMUP)	{ 
+			commands.add(new WSASDefaultingCommand( model,this, ctx.getScenario().getValue() ) );
+			commands.add(new WSASBUCommand( model ) );
+			commands.add(new WSASServicesXMLValidationCommand());
+			commands.add(new WSASBUServiceCreationCommand(model,this,project));
+			commands.add(new WSASWebservicesServerCommand(model, ctx.getScenario().getValue() ));
+		} 
+		else if (ctx.getScenario().getValue() == WebServiceScenario.TOPDOWN) {  
+			commands.add(new WSASDefaultingCommand( model,this, ctx.getScenario().getValue()  ) );
+			commands.add(new WSASTDCommand( model) );
+			commands.add(new WSASWSDL2JavaCommand( model) );
+			commands.add( new WSASBuildProjectCommand(ResourcesPlugin.getWorkspace().getRoot().getProject(project),true));
+			commands.add(new WSASTDServiceCreationCommand(model,this,project));
+			commands.add(new WSASWebservicesServerCommand(model, ctx.getScenario().getValue()));
+			//yes, again invoke the build command to redeploy the service after complete full build
+			commands.add( new WSASBuildProjectCommand(ResourcesPlugin.getWorkspace().getRoot().getProject(project),true));
+			commands.add(new WSASSkelImplCommand(this.getWebServiceInfo(),model));
+		} 
+		else 
+		{
+			return null;
+		}
+
+		return new SimpleCommandFactory(commands);
+	}
+
+	public ICommandFactory install(IEnvironment env, IContext ctx, ISelection sel,
+			String project, String earProject)
+	{
+		return null;
+	}
+
+	public ICommandFactory run(IEnvironment env, IContext ctx, ISelection sel,
+			String project, String earProject)
+	{
+		return null;
+	}
+}
