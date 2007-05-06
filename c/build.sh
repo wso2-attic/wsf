00c2ebc781e1b8f08b919e04bf69b880c3daa3aa
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2007-05-06 14:07:03 +0000

    updating build files for xmpp transport
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2222 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -9,6 +9,7 @@ else
 #    svn co https://svn.apache.org/repos/asf/webservices/axis2/trunk/c
 #    mv c axis2;
     cp build/axis2c/Makefile.am axis2c
+    cp build/axis2c/samples/server/Makefile.am axis2c/samples/server
     cp build/axis2c/configure.ac axis2c
 fi
 
@@ -40,8 +41,10 @@ cp build/sandesha2c/autogen.sh sandesha2c
 
 
 rm -rf axis2c/src/core/transport/xmpp
+rm -rf axis2c/samples/server/listener
 cp -r build/xmpp/xmpp axis2c/src/core/transport
 cp -r build/xmpp/include/*.h axis2c/include
+cp -r build/xmpp/samples/listener axis2c/samples/server
 cp build/axis2c/src/core/transport/http/server/simple_axis2_server/Makefile.am axis2c/src/core/transport/http/server/simple_axis2_server/
 cp build/axis2c/src/core/transport/http/sender/Makefile.am axis2c/src/core/transport/http/sender
 cp build/axis2c/src/core/transport/http/sender/ssl/Makefile.am axis2c/src/core/transport/http/sender/ssl/
