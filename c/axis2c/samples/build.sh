051a015b2e3f43272d78c6d6245bc00af5cb66e0
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2007-05-06 13:54:58 +0000

    updated samples build files
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2220 a5903396-d722-0410-b921-86c7d4935375

--- c/axis2c/samples/build.sh
+++ c/axis2c/samples/build.sh
@@ -2,7 +2,7 @@
 
 ./autogen.sh
 
-./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.0 --with-axiom=${AXIS2C_HOME}/include/axis2-1.0
+./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.0 --enable-xmpp=yes --with-axiom=${AXIS2C_HOME}/include/axis2-1.0
 make 
 make install
 
