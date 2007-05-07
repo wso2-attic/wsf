8f8e173b75acf8f127efafd24360a9895c8efabb
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-05-07 07:20:47 +0000

    build.bat updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2248 a5903396-d722-0410-b921-86c7d4935375

--- php/build.bat
+++ php/build.bat
@@ -31,7 +31,9 @@
 
 @copy sandesha2\samples\bin\*.exe .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\bin\samples\
 
-@xcopy /E /Q /I sandesha2\samples\bin\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services\RMSampleService
+@if exist .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services rmdir /s /q .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services
+
+rem @xcopy /E /Q /I sandesha2\samples\bin\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services\RMSampleService
 
 
 @copy /Y sandesha2\axis2.xml .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\axis2.xml
@@ -41,6 +43,10 @@
 @copy /Y sandesha2\sqlite_schema.bat  .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\sqlite_schema.bat
 
 @copy /Y sandesha2\init_mysql.bat .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\init_mysql.bat
+
+@copy E:\sqlite-source-3_3_8\sqlite3.dll .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
+
+@copy E:\sqlite-source-3_3_8\sqlite3.exe .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
 
 @cd ..\..\..\
 
