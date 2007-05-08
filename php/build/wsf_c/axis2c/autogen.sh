f1c6570ce42433a6acf981d8d035a1b7e326fb13
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-05-08 09:31:04 +0000

    Fixed build break
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2323 a5903396-d722-0410-b921-86c7d4935375

--- php/build/wsf_c/axis2c/autogen.sh
+++ php/build/wsf_c/axis2c/autogen.sh
@@ -11,10 +11,6 @@ echo 'Running autogen.sh in Guththila'
 cd ../guththila
 sh autogen.sh
 
-echo 'Running autogen.sh in Rampart'
-cd ../rampart
-sh autogen.sh
-
 echo 'Running autogen.sh in Axis2/C'
 cd ..
 
