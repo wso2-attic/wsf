1208d251d01a7177aa1fe743a56df0296bebaf2a
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-05-10 06:07:05 +0000

    Getting sandesha2 to  work with WSF/C
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2405 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -18,33 +18,6 @@ else
     cp build/axis2c/configure.ac axis2c
 fi
 
-if [ -d  "sandesha2c" ];then
-    cd sandesha2c;
-    svn up;
-    cd ..
-else
-    svn co https://svn.apache.org/repos/asf/webservices/sandesha/trunk/c;
-    mv c sandesha2c;
-fi
-
-cp build/sandesha2c/src/client/Makefile.am sandesha2c/src/client/
-cp build/sandesha2c/src/core/Makefile.am sandesha2c/src/core/
-cp build/sandesha2c/src/handlers/Makefile.am sandesha2c/src/handlers/
-cp build/sandesha2c/src/msgprocessors/Makefile.am sandesha2c/src/msgprocessors/
-cp build/sandesha2c/src/polling/Makefile.am sandesha2c/src/polling/
-cp build/sandesha2c/src/storage/Makefile.am sandesha2c/src/storage/
-cp build/sandesha2c/src/transport/Makefile.am sandesha2c/src/transport/
-cp build/sandesha2c/src/util/Makefile.am sandesha2c/src/util/
-cp build/sandesha2c/src/workers/Makefile.am sandesha2c/src/workers/
-cp build/sandesha2c/src/wsrm/Makefile.am sandesha2c/src/wsrm/
-cp build/sandesha2c/src/storage/beans/Makefile.am sandesha2c/src/storage/beans
-cp build/sandesha2c/src/storage/inmemory/Makefile.am sandesha2c/src/storage/inmemory
-cp build/sandesha2c/src/storage/mysql/Makefile.am sandesha2c/src/storage/mysql
-cp build/sandesha2c/src/storage/sqlite/Makefile.am sandesha2c/src/storage/sqlite
-cp build/sandesha2c/Makefile.am sandesha2c
-cp build/sandesha2c/autogen.sh sandesha2c
-
-
 rm -rf axis2c/src/core/transport/xmpp
 rm -rf axis2c/samples/server/listener
 cp -r build/xmpp/xmpp axis2c/src/core/transport
