3458ccaea1f349c862f309cc31d8696e9f1440e1
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-05-10 14:45:06 +0000

    Getting axis2/c from WSF repository
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2437 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -6,30 +6,30 @@ set -e
 #cp build/axis2c/Makefile.am axis2c
 
 
-cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c
-cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
-cp build/wsf_c/sandesha2c/src/util/Makefile.am wsf_c/sandesha2c/src/util
-cp build/wsf_c/sandesha2c/src/wsrm/Makefile.am wsf_c/sandesha2c/src/wsrm
-cp build/wsf_c/sandesha2c/src/transport/Makefile.am wsf_c/sandesha2c/src/transport
-cp build/wsf_c/sandesha2c/src/handlers/Makefile.am wsf_c/sandesha2c/src/handlers
-cp build/wsf_c/sandesha2c/src/storage/Makefile.am wsf_c/sandesha2c/src/storage
-cp build/wsf_c/sandesha2c/src/storage/mysql/Makefile.am wsf_c/sandesha2c/src/storage/mysql
-cp build/wsf_c/sandesha2c/src/storage/sqlite/Makefile.am wsf_c/sandesha2c/src/storage/sqlite
-cp build/wsf_c/sandesha2c/src/storage/beans/Makefile.am wsf_c/sandesha2c/src/storage/beans
-cp build/wsf_c/sandesha2c/src/storage/inmemory/Makefile.am wsf_c/sandesha2c/src/storage/inmemory
-cp build/wsf_c/sandesha2c/src/client/Makefile.am wsf_c/sandesha2c/src/client
-cp build/wsf_c/sandesha2c/src/msgprocessors/Makefile.am wsf_c/sandesha2c/src/msgprocessors
-cp build/wsf_c/sandesha2c/src/polling/Makefile.am wsf_c/sandesha2c/src/polling
-cp build/wsf_c/sandesha2c/src/workers/Makefile.am wsf_c/sandesha2c/src/workers
-cp build/wsf_c/sandesha2c/autogen.sh wsf_c/sandesha2c
+#cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c
+#cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
+#cp build/wsf_c/sandesha2c/src/util/Makefile.am wsf_c/sandesha2c/src/util
+#cp build/wsf_c/sandesha2c/src/wsrm/Makefile.am wsf_c/sandesha2c/src/wsrm
+#cp build/wsf_c/sandesha2c/src/transport/Makefile.am wsf_c/sandesha2c/src/transport
+#cp build/wsf_c/sandesha2c/src/handlers/Makefile.am wsf_c/sandesha2c/src/handlers
+#cp build/wsf_c/sandesha2c/src/storage/Makefile.am wsf_c/sandesha2c/src/storage
+#cp build/wsf_c/sandesha2c/src/storage/mysql/Makefile.am wsf_c/sandesha2c/src/storage/mysql
+#cp build/wsf_c/sandesha2c/src/storage/sqlite/Makefile.am wsf_c/sandesha2c/src/storage/sqlite
+#cp build/wsf_c/sandesha2c/src/storage/beans/Makefile.am wsf_c/sandesha2c/src/storage/beans
+#cp build/wsf_c/sandesha2c/src/storage/inmemory/Makefile.am wsf_c/sandesha2c/src/storage/inmemory
+#cp build/wsf_c/sandesha2c/src/client/Makefile.am wsf_c/sandesha2c/src/client
+#cp build/wsf_c/sandesha2c/src/msgprocessors/Makefile.am wsf_c/sandesha2c/src/msgprocessors
+#cp build/wsf_c/sandesha2c/src/polling/Makefile.am wsf_c/sandesha2c/src/polling
+#cp build/wsf_c/sandesha2c/src/workers/Makefile.am wsf_c/sandesha2c/src/workers
+#cp build/wsf_c/sandesha2c/autogen.sh wsf_c/sandesha2c
 cp build/wsf_c/axis2c/autogen.sh wsf_c/axis2c
 
 
-cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c
+#cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c
 cp build/wsf_c/axis2c/autogen.sh wsf_c/axis2c
-cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
-cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c/
-cp build/wsf_c/axis2c/Makefile.am wsf_c/axis2c/
+#cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
+#cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c/
+#cp build/wsf_c/axis2c/Makefile.am wsf_c/axis2c/
 cp wsf_c/sandesha2c/config/axis2.xml .
 cp wsf_c/sandesha2c/config/sqlite_schema.sh /tmp
 cp wsf_c/sandesha2c/config/mysql_schema.sh /tmp
