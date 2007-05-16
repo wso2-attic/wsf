8066ff82c0c9a6edc135bf2a3db90162e050411f
sandakith <sandakith@a5903396-d722-0410-b921-86c7d4935375>
2007-05-16 06:57:45 +0000

    adding the toplevel menu delegate commands
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@2695 a5903396-d722-0410-b921-86c7d4935375

---
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.wtp.ide/src/org/wso2/wsf/wtp/server/ui/WSASStartMenuDelegate.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.wsf.wtp.server.ui;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.IWorkbenchWindowPulldownDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionDelegate;
+import org.wso2.wsf.wtp.server.command.WSASStartCommand;
+
+public class WSASStartMenuDelegate
+	extends ActionDelegate
+	implements IWorkbenchWindowPulldownDelegate {
+
+    MessageBox box = new MessageBox(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+    IStatus status;
+    
+	/**
+	 * @see ActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		
+		try {
+			status = WSASStartCommand.run();
+			if(status.getCode() == 11){
+				box.setMessage(status.getMessage());box.open();
+			}else{
+				box.setMessage("WSO2 Web Services Server Started Successfully !! ");box.open();
+			}
+		} catch (InvocationTargetException e) {
+			status = new Status( IStatus.ERROR,"id",1,e.getMessage(),null );
+			box.setMessage("WSAS Failed to Start \n"+"Reason"+e.getMessage());box.open();
+		}
+
+	}
+
+	/**
+	 * @see IWorkbenchWindowPulldownDelegate#getMenu(Control)
+	 */
+	public Menu getMenu(Control parent) {
+		return null;
+	}
+
+	/**
+	 * @see IWorkbenchWindowActionDelegate#dispose()
+	 */
+	public void dispose() {
+	}
+
+	/**
+	 * @see IWorkbenchWindowActionDelegate#init(IWorkbenchWindow)
+	 */
+	public void init(IWorkbenchWindow window) {
+	}
+	
+}
+
