e7bfd4b5cb070b984c904da56846b863d2e02299
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-05-30 08:09:42 +0000

    Getting axis2c from svn:externals
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3386 a5903396-d722-0410-b921-86c7d4935375

--- c/axis2c/samples/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./autogen.sh
-
-./configure --prefix=${AXIS2C_HOME} --with-axis2_util=${AXIS2C_HOME}/include/axis2-1.0 --with-axiom=${AXIS2C_HOME}/include/axis2-1.0
-make 
-make install
-
--- c/axis2c/samples/client/echo/README.txt
+++
@@ -1,5 +0,0 @@
-Client Sample -echo
--------------------
-
-This is a sample to help test addressing.
-This sample works with echo service.
