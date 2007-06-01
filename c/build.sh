604893b8681fde658116821323d84606bd97ee9a
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-01 09:31:33 +0000

    Added --enable-xmpp option
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3444 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -17,6 +17,7 @@ cp -r build/xmpp/xmpp axis2c/src/core/transport
 cp -r build/xmpp/include/*.h axis2c/include
 cp -r build/xmpp/samples/listener axis2c/samples/server
 cp build/rampartc/configure.ac rampartc
+cp build/src/core/engine/Makefile.am src/core/engine/
 
 ./autogen.sh
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-rampart=yes --enable-xmpp=no --with-axis2=`pwd`/axis2c/include 
