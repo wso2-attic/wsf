92100fa275248c647722b0801534bceb8f84e8ea
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-01 05:41:23 +0000

    Fixed build break
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3431 a5903396-d722-0410-b921-86c7d4935375

--- c/samples.sh
+++ c/samples.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/sh
 set -e
-export LD_LIBRARY_PATH=$1/lib
-echo $LD_LIBRARY_PATH
-cd axis2c/samples
-./autogen.sh
-./configure --prefix=$1
-make
-make install
-cd ../../sandesha2c/samples
-./autogen.sh
-./configure --prefix=$1
-make
-make install
-cd ../../rampartc/samples
-./autogen.sh
-./configure --prefix=$1
-#make
-make install
+export LD_LIBRARY_PATH=$1/lib; cd axis2c/samples; ./autogen.sh; ./configure --prefix=$1; make; make install ;cd ../../sandesha2c/samples; ./autogen.sh; ./configure --prefix=$1; make; make install; cd ../../rampartc/samples; ./autogen.sh; ./configure --prefix=$1 ; make; make install
