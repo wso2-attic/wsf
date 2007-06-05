d1ce564fb289743f10f6b14b8abf719d7c53a32c
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-05 11:48:33 +0000

    Fixed build break
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3540 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -17,6 +17,7 @@ cp -r build/xmpp/samples/listener axis2c/samples/server
 cp build/rampartc/configure.ac rampartc
 cp build/savanc/configure.ac savanc
 cp build/axis2c/configure.ac axis2c
+cp build/axis2c/Makefile.am axis2c
 cp build/sandesha2c/configure.ac sandesha2c
 cp build/axis2c/src/core/engine/Makefile.am axis2c/src/core/engine/
 cp build/axis2c/src/core/transport/Makefile.am axis2c/src/core/transport/Makefile.am
