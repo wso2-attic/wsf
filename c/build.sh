5d145624f827e053ce0a49cbd0dd764c5c0ca3d6
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-06-14 04:47:14 +0000

    Added make install
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3770 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -3,15 +3,6 @@
 
 set -e 
 
-#if [ -d  "axis2c" ];then
-#    cd axis2c;
-#    svn up;
-#    cd ..
-#fi
-
-#    cp build/axis2c/configure.ac axis2c
-
-#rm -rf axis2c/src/core/transport/xmpp
 rm -rf axis2c/samples/server/listener
 cp -r build/xmpp/samples/listener axis2c/samples/server
 cp build/rampartc/configure.ac rampartc
@@ -27,5 +18,6 @@ cp build/neethi/Makefile.am neethi
 ./autogen.sh
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-rampart=yes --enable-wsclient=yes --enable-sandesha=yes --enable-xmpp=no --with-axis2=`pwd`/axis2c/include 
 make
-
+make install
+make samples
 
