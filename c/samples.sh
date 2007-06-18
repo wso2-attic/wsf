37e41a79d5b717b6a05a82a2331237f0872da89c
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-18 09:58:58 +0000

    Changed the axis2 samples install directory
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3969 a5903396-d722-0410-b921-86c7d4935375

--- c/samples.sh
+++ c/samples.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 set -e
 export LD_LIBRARY_PATH=$1/lib; 
-cd axis2c/samples; ./autogen.sh; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install ;
+cd axis2c/samples; ./autogen.sh; ./configure --prefix=$1/bin/samples/axis2 --with-axis2=$1/include/axis2-1.1; make; make install ;
 if test -e ../../sandesha2c/Makefile; then
 cd ../../sandesha2c/samples; ./autogen.sh; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install; 
 fi
