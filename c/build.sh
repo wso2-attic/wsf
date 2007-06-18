a3aad820bc07d9d313a65c688698aa32bd9b8554
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-06-18 05:59:32 +0000

    Fixed more build problems
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@3936 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -12,8 +12,8 @@ cp build/rampartc/Makefile.am rampartc
 cp build/sandesha2c/configure.ac sandesha2c
 cp build/axis2c/src/core/engine/Makefile.am axis2c/src/core/engine/
 cp build/axis2c/src/core/transport/Makefile.am axis2c/src/core/transport/Makefile.am
-cp build/axis2c/neethi/configure.ac axis2c/neethi
-cp build/axis2c/neethi/Makefile.am axis2c/neethi
+cp build/neethi/configure.ac axis2c/neethi
+cp build/neethi/Makefile.am axis2c/neethi
 
 ./autogen.sh
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-xmpp=no --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes
