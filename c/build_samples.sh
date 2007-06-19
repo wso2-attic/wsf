350ad2479bee8a28cc84cce1f93d067d3a0d9a92
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-19 10:35:00 +0000

    Stopped running autogen.sh to 'make samples' target
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4039 a5903396-d722-0410-b921-86c7d4935375

--- c/build_samples.sh
+++ c/build_samples.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 set -e
 export LD_LIBRARY_PATH=$1/lib; 
-cd axis2c/samples; ./autogen.sh; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install ;
+cd axis2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install ;
 if test -e ../../sandesha2c/Makefile; then
-cd ../../sandesha2c/samples; ./autogen.sh; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install; 
+cd ../../sandesha2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.1; make; make install; 
 fi
 if test -e ../../rampartc/Makefile; then
-cd ../../rampartc/samples; ./autogen.sh; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.1; make; make install
+cd ../../rampartc/samples; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.1; make; make install
 fi
