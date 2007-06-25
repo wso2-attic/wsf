51a16e5f3d49b83b08c68e354faaff88347d7e4c
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-25 08:16:41 +0000

    Changed the permissions
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4253 a5903396-d722-0410-b921-86c7d4935375

---
+++ c/wsclient/samples/flicker_soap_photo_search.sh
@@ -0,0 +1,10 @@
+if [ $# -ne 1 ]
+then
+    echo "Usage : $0 FlickerAPIKey"
+    echo "Visit http://www.flickr.com/services/api/keys/ to aget a Flicker API key"
+    exit
+fi
+if test -z $WSFC_HOME; then 
+    WSFC_HOME=$PWD/../../..
+fi
+echo "<x:FlickrRequest xmlns:x='urn:flickr'><method>flickr.photos.search</method><api_key>$1</api_key><tags>peace</tags></x:FlickrRequest>" | $WSFC_HOME/bin/wsclient --soap --no-mtom --no-wsa  http://api.flickr.com/services/soap/
