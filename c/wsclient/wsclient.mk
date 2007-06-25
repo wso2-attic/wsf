c55059c7b979cdddab438763fd8f66210ded672f
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2007-06-25 08:43:21 +0000

    The lines - /I.\..\axis2c\neethi\include and  /I.\..\axis2c\neethi\src\util added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4256 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/wsclient.mk
+++ c/wsclient/wsclient.mk
@@ -2,7 +2,7 @@ wsclient:
 	@echo off
 	@if not exist int.msvc mkdir int.msvc
 	@cl.exe /nologo /D "WIN32" /w /D "_WINDOWS" /D "AXIS2_DECLARE_EXPORT" /D "_MBCS" src\*.c \
-	/I.\..\axis2c\build\axis2c-bin-1.1.0-win32\include /Foint.msvc\ /c
+	/I.\..\axis2c\build\axis2c-bin-1.1.0-win32\include /I.\..\axis2c\neethi\include /I.\..\axis2c\neethi\src\util /Foint.msvc\ /c
 
 	@link.exe /LIBPATH:.\..\axis2c\build\axis2c-bin-1.1.0-win32\lib int.msvc\*.obj axutil.lib  axis2_engine.lib \
 	axis2_parser.lib axiom.lib axis2_http_sender.lib mod_rampart.lib /OUT:.\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\wsclient.exe
