51ea9b02fde852948955dfc16f129e83d12e428e
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2007-06-26 06:19:04 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4321 a5903396-d722-0410-b921-86c7d4935375

--- c/build/neethi/util/neethi_options.h
+++ c/build/neethi/util/neethi_options.h
@@ -165,6 +165,12 @@ extern "C"
         const axutil_env_t *env,
         axiom_node_t *parent_node);
 
+	AXIS2_EXTERN axis2_status_t AXIS2_CALL
+	neethi_options_create_asym_node(
+    neethi_options_t *options,
+    const axutil_env_t *env,
+    axiom_node_t *parent_node);
+
 
     /** @} */
 #ifdef __cplusplus
