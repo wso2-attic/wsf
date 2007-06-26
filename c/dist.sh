1a5ce05dff47bd588ea74a11caf9f4c9f6160965
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-26 05:18:13 +0000

    Removed unwanted files from tarball created by 'make dist'
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4309 a5903396-d722-0410-b921-86c7d4935375

--- c/dist.sh
+++ c/dist.sh
@@ -3,7 +3,13 @@ cd sandesha2c/samples
 find . -name "*.c" | xargs sed -i "s/8888/9090/g"
 find . -name "*.c" | xargs sed -i "s/7777/6060/g"
 cd ../../
+cd wso2-wsf-c-src-1.0
 mv axis2c/xdocs docs/axis2c
 mv savanc/xdocs docs/savanc
 mv sandesha2c/xdocs docs/sandesha2c
 mv rampartc/xdocs docs/rampartc
+find -name "*.libs"| xargs rm -rf
+find -name "*.deps"| xargs rm -rf
+find -name "*.svn"| xargs rm -rf
+find -name "*.Makefile"| xargs rm -rf
+cd ..
