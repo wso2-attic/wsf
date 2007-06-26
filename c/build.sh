a5f063515fee9bbdc8df503a84cb80f5c127b491
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-06-26 10:53:17 +0000

    Added savanc docs to tarball created by 'make dist'
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4338 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -17,6 +17,7 @@ cp build/neethi/src/Makefile.am axis2c/neethi/src
 cp build/axis2c/tools/tcpmon/Makefile.am axis2c/tools/tcpmon
 cp build/axis2c/tools/tcpmon/configure.ac axis2c/tools/tcpmon
 cp build/savanc/configure.ac savanc
+cp build/savanc/Makefile.am savanc
 cp build/axis2c/xdocs/doxygenconf axis2c/xdocs/api
 cp build/axis2c/dist.sh axis2c
 
