c03c4af6e7deb38bfda58089e17011f47cea7882
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2007-06-27 09:45:31 +0000

    Added echo bat file and also added installation of wsclient samples to bin folder
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4378 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/wsclient.mk
+++ c/wsclient/wsclient.mk
@@ -7,6 +7,7 @@ wsclient:
 	@link.exe /LIBPATH:.\..\axis2c\build\axis2c-bin-1.1.0-win32\lib int.msvc\*.obj axutil.lib  axis2_engine.lib \
 	axis2_parser.lib axiom.lib neethi_util.lib axis2_http_sender.lib mod_rampart.lib /OUT:.\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\wsclient.exe
 	@if exist int.msvc rmdir /s /q int.msvc
+	@xcopy samples\* .\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\wsclient
 
 	
 	
