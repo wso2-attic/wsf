79cc1cac1da3905b1576492cab59f4cbfb95fc73
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-07-02 14:09:08 +0000

    Fixed samples copying issue
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4645 a5903396-d722-0410-b921-86c7d4935375

--- c/dist.sh
+++ c/dist.sh
@@ -10,11 +10,17 @@ mv sandesha2c/xdocs docs/sandesha2c
 mv rampartc/xdocs docs/rampartc
 mv wsclient/docs docs/wsclient
 
-mv axis2c/samples examples/axis2c
-mv savanc/samples examples/savanc
-mv sandesha2c/samples examples/sandesha2c
-mv rampartc/samples examples/rampartc
-mv wsclient/samples examples/wsclient
+mkdir examples/axis2c
+mkdir examples/savanc
+mkdir examples/sandesha2c
+mkdir examples/rampartc
+mkdir examples/wsclient
+
+cp -r axis2c/samples/* examples/axis2c
+cp -r savanc/samples/* examples/savanc
+cp -r sandesha2c/samples/* examples/sandesha2c
+cp -r rampartc/samples/* examples/rampartc
+cp -r wsclient/samples/* examples/wsclient
 
 find -name "*.libs"| xargs rm -rf
 find -name "*.deps"| xargs rm -rf
