06fc5146d69b7765b70d3c1ca58d2043e3d774bc
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-03 07:35:39 +0000

    configure.in file added for win32 build, build.bat and clean.bat updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4683 a5903396-d722-0410-b921-86c7d4935375

--- php/build.bat
+++ php/build.bat
@@ -1,93 +1,23 @@
 @echo off
-@call vcvars32.bat /nologo
 
 @call clean.bat
 
-@cd .\wsf_c\axis2c\build
+@copy configure.in wsf_c\configure.in
 
-@if exist axis2c-bin-1.0.0-win32 rmdir /s /q axis2c-bin-1.0.0-win32
+@cd wsf_c
 
-@cd win32
-
-@nmake dist
-
-@if exist ..\axis2c-bin-1.0.0-win32\bin\samples\logging rmdir ..\axis2c-bin-1.0.0-win32\bin\samples\logging
-
-@cd .\..\..\..\rampartc\build\win32
-
-@nmake dist
-
-@cd .\..
-
-@xcopy /E /Q /I rampartc-0.90\modules\rampart .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\modules\rampart
-
-@copy /Y rampartc-0.90\modules\rampart\mod_rampart.lib .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
-@copy /Y rampartc-0.90\modules\rampart\mod_rampart.dll .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
-
-
-@cd ..\..\sandesha2c\build\win32
-
-@nmake dist
+@call build.bat
 
 @cd ..
 
-@xcopy /E /Q /I sandesha2\modules\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\modules\sandesha2
 
-@copy sandesha2\bin\samples\*.exe .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\bin\samples\
+@mkdir wso2-wsf-php-bin-1.0
 
-@if exist .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services rmdir /s /q .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services
+@xcopy /E /Q /I wsf_c\wso2-wsf-c-bin-1.0 wso2-wsf-php-bin-1.0\wsf_c
 
-rem @xcopy /E /Q /I sandesha2\services\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\services\RMSampleService
+@if exist wso2-wsf-php-bin-1.0\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.0\wsf_c\services
 
+@if exist wso2-wsf-php-bin-1.0\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.0\wsf_c\modules\savan
 
-@copy /Y sandesha2\axis2.xml .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\axis2.xml
-
-@copy /Y sandesha2\sqlite_schema.bat  .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\sqlite_schema.bat
-
-@copy /Y %SQLITE_HOME%\sqlite3.dll .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
-
-@copy /Y %SQLITE_HOME%\sqlite3.exe .\..\..\axis2c\build\axis2c-bin-1.0.0-win32\lib\
-
-
-@cd ..\..\..\
-
-@mkdir wso2-wsf-php-bin-1.0alpha2
-
-@xcopy /E /Q /I wsf_c\axis2c\build\axis2c-bin-1.0.0-win32 wso2-wsf-php-bin-1.0alpha2\wsf_c
-
-@xcopy /E /Q /I src\scripts wso2-wsf-php-bin-1.0alpha2\scripts
-
-@mkdir wso2-wsf-php-bin-1.0alpha2\docs
-
-@xcopy /E /Q /I src\xdocs wso2-wsf-php-bin-1.0alpha2\docs\
-
-
-@XCOPY /E /Q /I src\samples wso2-wsf-php-bin-1.0alpha2\samples
-
-@copy /Y src\README* wso2-wsf-php-bin-1.0alpha2\docs\
-
-@copy /Y src\LICENSE wso2-wsf-php-bin-1.0alpha2\
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@if exist wso2-wsf-php-bin-1.0\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.0\wsf_c\modules\logging
 
--- php/clean.bat
+++ php/clean.bat
@@ -1,10 +1,4 @@
-@if exist wsf_c\axis2c\build\axis2c-bin-1.0.0-win32 rmdir /S /Q wsf_c\axis2c\build\axis2c-bin-1.0.0-win32
+@if exist wsf_c\wso2-wsf-c-bin-1.0 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.0
 
-@if exist wsf_c\sandesha2c\build\sandesha2 rmdir /S /Q wsf_c\sandesha2c\build\sandesha2
-
-@if exist wsf_c\rampartc\build\rampartc-0.9 rmdir /S /Q wsf_c\rampartc\build\rampartc-0.9
-
-@if exist wso2-wsf-php-bin-1.0alpha2-bin rmdir /S /Q wso2-wsf-php-bin-1.0alpha2
-
-
+@if exist wso2-wsf-php-bin-1.0 rmdir /S /Q wso2-wsf-php-bin-1.0
 
