be2a5fad5974c654b22b45f4e440be5d7271e779
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-03 08:07:28 +0000

    fixed a problem in flicker.mk
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@4686 a5903396-d722-0410-b921-86c7d4935375

--- c/examples/flicker/flicker.mk
+++ c/examples/flicker/flicker.mk
@@ -1,6 +1,6 @@
 
-flicker:
-	@cl.exe /nologo /D "WIN32" /D "_WINDOWS" /D "AXIS2_DECLARE_EXPORT" /D "_MBCS" *.C  \                      /I..\..\axis2c\build\axis2c-bin-1.1.0-win32\include /c
+echo:
+	@cl.exe /nologo /D "WIN32" /D "_WINDOWS" /D "AXIS2_DECLARE_EXPORT" /D "_MBCS" *.C  /I..\..\axis2c\build\axis2c-bin-1.1.0-win32\include /c
 	@link.exe /nologo *.obj /LIBPATH:..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib axiom.lib axutil.lib axis2_engine.lib axis2_parser.lib   /OUT:flicker.exe
 
 	
