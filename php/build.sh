4b91d41dd85900bfe9bc26b5bd748eb42ddb693c
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-07-13 05:29:57 +0000

    Fixed the revision to the latest WSF/C bransh https://wso2.org/repos/wso2/branches/wsf/c/130707/
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5330 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -2,48 +2,11 @@
 
 set -e
 
-#svn up;
-#cp build/axis2c/Makefile.am axis2c
 
-
-cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c
-cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
-cp build/wsf_c/sandesha2c/src/util/Makefile.am wsf_c/sandesha2c/src/util
-cp build/wsf_c/sandesha2c/src/wsrm/Makefile.am wsf_c/sandesha2c/src/wsrm
-cp build/wsf_c/sandesha2c/src/transport/Makefile.am wsf_c/sandesha2c/src/transport
-cp build/wsf_c/sandesha2c/src/handlers/Makefile.am wsf_c/sandesha2c/src/handlers
-cp build/wsf_c/sandesha2c/src/storage/Makefile.am wsf_c/sandesha2c/src/storage
-cp build/wsf_c/sandesha2c/src/storage/mysql/Makefile.am wsf_c/sandesha2c/src/storage/mysql
-cp build/wsf_c/sandesha2c/src/storage/sqlite/Makefile.am wsf_c/sandesha2c/src/storage/sqlite
-cp build/wsf_c/sandesha2c/src/storage/beans/Makefile.am wsf_c/sandesha2c/src/storage/beans
-cp build/wsf_c/sandesha2c/src/storage/inmemory/Makefile.am wsf_c/sandesha2c/src/storage/inmemory
-cp build/wsf_c/sandesha2c/src/client/Makefile.am wsf_c/sandesha2c/src/client
-cp build/wsf_c/sandesha2c/src/msgprocessors/Makefile.am wsf_c/sandesha2c/src/msgprocessors
-cp build/wsf_c/sandesha2c/src/polling/Makefile.am wsf_c/sandesha2c/src/polling
-cp build/wsf_c/sandesha2c/src/workers/Makefile.am wsf_c/sandesha2c/src/workers
-cp build/wsf_c/sandesha2c/autogen.sh wsf_c/sandesha2c
-cp build/wsf_c/axis2c/autogen.sh wsf_c/axis2c
-cp wsf_c/build/neethi/src/Makefile.am wsf_c/axis2c/neethi/src
-cp wsf_c/build/neethi/configure.ac wsf_c/axis2c/neethi
-
-
-
-#cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c
-cp build/wsf_c/axis2c/autogen.sh wsf_c/axis2c
-cp build/wsf_c/sandesha2c/src/core/Makefile.am wsf_c/sandesha2c/src/core
-cp build/wsf_c/sandesha2c/Makefile.am wsf_c/sandesha2c/
-cp build/wsf_c/rampartc/configure.ac wsf_c/rampartc/
-#cp build/wsf_c/axis2c/Makefile.am wsf_c/axis2c/
-cp wsf_c/sandesha2c/config/axis2.xml .
-cp wsf_c/sandesha2c/config/sqlite_schema.sh /tmp
-cp wsf_c/sandesha2c/config/mysql_schema.sh /tmp
-cp wsf_c/sandesha2c/config/init_mysql.sh /tmp
-
-
-for i in `find -name configure.ac`
+for i in `find . -name configure.ac`
 do
     sed  's/-Werror//' $i > configure.ac.back;
     mv configure.ac.back $i;
 done
-./autogen.sh && ./configure --with-wsf --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/wsf_c/axis2c/include --enable-tests=yes --enable-savan=no --with-mysql=yes --prefix=`php-config --extension-dir`/wsf_c && make -j 100 
+./autogen.sh && ./configure --with-wsf --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/wsf_c/axis2c/include --enable-tests=yes --enable-savan=no --with-mysql=no --prefix=`php-config --extension-dir`/wsf_c && make -j 100 
 
