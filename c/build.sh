4241c00742417a7ab3fd8bab8f048426af757cb0
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-07-13 02:43:11 +0000

    fixed build file to seperate file copying
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5317 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -2,24 +2,7 @@
 
 set -e 
 
-rm -rf axis2c/samples/server/listener
-cp -r build/xmpp/samples/listener axis2c/samples/server
-cp build/rampartc/configure.ac rampartc
-cp build/axis2c/configure.ac axis2c
-cp build/axis2c/Makefile.am axis2c
-cp build/rampartc/Makefile.am rampartc
-cp build/sandesha2c/configure.ac sandesha2c
-cp build/sandesha2c/Makefile.am sandesha2c
-cp build/axis2c/src/core/engine/Makefile.am axis2c/src/core/engine/
-cp build/axis2c/src/core/transport/Makefile.am axis2c/src/core/transport/Makefile.am
-cp build/neethi/configure.ac axis2c/neethi
-cp build/neethi/src/Makefile.am axis2c/neethi/src
-cp build/axis2c/tools/tcpmon/Makefile.am axis2c/tools/tcpmon
-cp build/axis2c/tools/tcpmon/configure.ac axis2c/tools/tcpmon
-cp build/savanc/configure.ac savanc
-cp build/savanc/Makefile.am savanc
-cp build/axis2c/xdocs/doxygenconf axis2c/xdocs/api
-cp build/axis2c/dist.sh axis2c
+./build/copy_build_files.sh
 
 ./autogen.sh
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-xmpp=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes
