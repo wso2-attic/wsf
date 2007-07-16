b90c11e1f58834b6a5d436f568acdffd712a462b
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-16 04:35:21 +0000

    build.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5447 a5903396-d722-0410-b921-86c7d4935375

--- php/build.bat
+++ php/build.bat
@@ -12,7 +12,7 @@
 
 @mkdir wso2-wsf-php-bin-1.0
 
-@xcopy /E /Q /I wsf_c\wso2-wsf-c-bin-1.0 wso2-wsf-php-bin-1.0\wsf_c
+@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.0.0 wso2-wsf-php-bin-1.0\wsf_c
 
 @if exist wso2-wsf-php-bin-1.0\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.0\wsf_c\services
 
@@ -20,11 +20,15 @@
 
 @if exist wso2-wsf-php-bin-1.0\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.0\wsf_c\modules\logging
 
-@if exist scripts xcopy /Y /I /Q scripts wso2-wsf-php-bin-1.0\scripts
+@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.0\scripts
 
-@if exist src\samples xcopy /Y /I /Q wso2-wsf-php-bin-1.0\samples
+@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.0\samples
 
-@if exist src\xdocs xcopy /Y /I /Q wso2-wsf-php-bin-1.0\docs
+@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.0\docs
 
+@cd src
 
+@nmake -f build.mk
+
+@cd ..
 
