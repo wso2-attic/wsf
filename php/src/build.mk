1f1826d35709b2e070d2a5978dabce6b89b4af16
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-16 04:45:57 +0000

    build.mk file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5448 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/src/build.mk
@@ -0,0 +1,39 @@
+AUTOCONF = .\..\configure.in
+!include $(AUTOCONF)
+
+CFLAGS = /nologo /w /D "WIN32" /D "_WINDOWS" /D "ZTS" /D "PHP_WIN32" /D "COMPILE_DL_WSF" \
+	 /D "_USE_32BIT_TIME_T=1" /D "USE_SANDESHA2" /D "ZEND_WIN32" /D "ZEND_DEBUG=0" \
+	 /D "_MBCS" 
+
+LDFLAGS = /nologo /LIBPATH:.\..\wso2-wsf-php-bin-1.0\wsf_c\lib /LIBPATH:$(PHP_BIN_DIR)\dev \
+	  /LIBPATH:$(LIBXML2_BIN_DIR)\lib
+
+LIBS = php5ts.lib axutil.lib axis2_engine.lib axis2_parser.lib \
+       axiom.lib libxml2.lib mod_rampart.lib axis2_http_sender.lib \
+       neethi_util.lib neethi.lib sandesha2.lib libxml2.lib wsock32.lib
+
+INCLUDE_PATH = /I$(PHP_SRC_DIR) /I$(PHP_SRC_DIR)\main /I$(PHP_SRC_DIR)\regex \
+	       /I$(PHP_SRC_DIR)\ext /I$(PHP_SRC_DIR)\sapi /I$(PHP_SRC_DIR)\Zend \
+	       /I$(PHP_SRC_DIR)\TSRM /I.\..\wso2-wsf-php-bin-1.0\wsf_c\include \
+	       /I$(LIBXML2_BIN_DIR)\include /I$(WIN32BUILD_DIR)\include \
+	       /I$(ICONV_BIN_DIR)\include 
+
+!if "$(DEBUG)" == "1"
+CFLAGS = $(CFLAGS) /D "_DEBUG" /Od /Z7 /MTd
+LDFLAGS = $(LDFLAGS) /DEBUG
+!else
+CFLAGS = $(CFLAGS) /D "NDEBUG" /O2 /MT
+LDFLAGS = $(LDFLAGS)
+!endif
+
+wsf.dll :
+	@if not exist intdir mkdir intdir
+	cl.exe $(CFLAGS) $(INCLUDE_PATH) *.c /Fointdir\ /c
+	link.exe $(LDFLAGS) intdir\*.obj $(LIBS) /DLL  /OUT:.\..\wso2-wsf-php-bin-1.0\wsf.dll
+
+clean: 
+	@if exist intdir rmdir intdir
+	@del *.obj
+	@del *.exp
+	
+	
