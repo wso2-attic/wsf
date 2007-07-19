96254e957bb7b1290d2527b56da5710843b6bd53
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-07-19 04:14:15 +0000

    Added package.sh script
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5634 a5903396-d722-0410-b921-86c7d4935375

---
+++ c/build/packaging/package.sh
@@ -0,0 +1,21 @@
+set -e
+make dist
+tar xf wso2-wsf-c-src-1.0.tar.gz
+cd wso2-wsf-c-src-1.0
+./configure
+make maintainer-clean
+
+cd axis2c/samples
+./configure
+make maintainer-clean
+cd ../../sandesha2c/samples
+./configure
+make maintainer-clean
+cd ../../rampartc/samples
+./configure
+make maintainer-clean
+cd ../../../
+rm -rf wso2-wsf-c-src-1.0.tar.gz
+tar -pczf wso2-wsf-c-src-1.0.tar.gz wso2-wsf-c-src-1.0
+zip -r wso2-wsf-c-src-1.0.zip wso2-wsf-c-src-1.0
+rm -rf wso2-wsf-c-src-1.0
