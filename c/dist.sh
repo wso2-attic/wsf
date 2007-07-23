edb70130d49a5d955913c48b384d9b6d3fbdb2ce
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-07-23 09:20:59 +0000

    Fixed a syntax error
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5795 a5903396-d722-0410-b921-86c7d4935375

--- c/dist.sh
+++ c/dist.sh
@@ -1,5 +1,5 @@
 set -e
-grep -rl WSFC_HOME *| xargs sed -i "s/WSFC_HOME/WSFC_HOME/g"
+grep -rl AXIS2C_HOME *| xargs sed -i "s/AXIS2C_HOME/WSFC_HOME/g"
 export WSFC_HOME=`pwd`/deploy
 find . -name "*configure.ac" | xargs sed -i "s/-Werror//g"
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-xmpp=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes
