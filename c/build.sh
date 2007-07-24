dac3b55a2e809c9a3f2cf5138e7c25099161bad7
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-07-24 10:26:08 +0000

    Changing externs for dependants
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5853 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -5,7 +5,7 @@ set -e
 ./build/copy_build_files.sh
 
 ./autogen.sh
-./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-xmpp=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes
+./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-xmpp=no --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes
 make
 make install
 make samples
