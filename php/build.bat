5a160c1c453b8d3a71e59388fe92f18bd13f0036
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-25 10:48:37 +0000

    build.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5880 a5903396-d722-0410-b921-86c7d4935375

--- php/build.bat
+++ php/build.bat
@@ -11,21 +11,21 @@
 
 @cd ..
 
-@mkdir wso2-wsf-php-bin-1.0.0beta-win32
+@mkdir wso2-wsf-php-bin-1.0.0-win32
 
-@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.0.1 wso2-wsf-php-bin-1.0.0beta-win32\wsf_c
+@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.0.1 wso2-wsf-php-bin-1.0.0-win32\wsf_c
 
-@if exist wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\services
+@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\services
 
-@if exist wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\modules\savan
+@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\savan
 
-@if exist wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.0.0beta-win32\wsf_c\modules\logging
+@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\logging
 
-@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.0.0beta-win32\scripts
+@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.0.0-win32\scripts
 
-@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.0.0beta-win32\samples
+@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.0.0-win32\samples
 
-@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.0.0beta-win32\docs
+@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.0.0-win32\docs
 
 @cd src
 
