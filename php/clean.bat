5f1498a3022f81572c2b03a6f85430d676f79c37
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-07-27 05:07:15 +0000

    clean.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@5944 a5903396-d722-0410-b921-86c7d4935375

--- php/clean.bat
+++ php/clean.bat
@@ -1,6 +1,6 @@
 @if exist wsf_c\wso2-wsf-c-bin-1.0.1 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.0.1
 
-@if exist wso2-wsf-php-bin-1.0.0 rmdir /S /Q wso2-wsf-php-bin-1.0.0
+@if exist wso2-wsf-php-bin-1.0.0-win32 rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32
 
 @if exist wsf_c\axis2c\build\axis2c-bin-1.1.0-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-1.1.0-win32
 
