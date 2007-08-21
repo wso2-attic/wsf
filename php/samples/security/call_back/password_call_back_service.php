92c74e0ca9e78ca863a583f26f23c567fb55eaa6
buddhika <buddhika@a5903396-d722-0410-b921-86c7d4935375>
2007-08-21 08:14:14 +0000

    added a sample for password callback in security
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6423 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/security/call_back/password_call_back_service.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function echoFunction($inMessage) {
+
+    $returnMessage = new WSMessage($inMessage->str);
+
+    return $returnMessage;
+}
+
+function get_my_password_function($username)
+{
+    //logic to get password from any source (ex: using mysql database
+    // etc)
+    $password = "RaigamaPW";
+    return $password;
+
+}
+
+$operations = array("echoString" => "echoFunction");
+$actions = array("http://php.axis2.org/samples/echoString" => "echoString");
+$sec_array = array("useUsernameToken" => TRUE);
+
+$policy = new WSPolicy(array("security"=>$sec_array));
+$sec_token = new WSSecurityToken(array("password_callback" => "get_my_password_function",
+                                       "passwordType" => "Digest"));
+
+$svr = new WSService(array("operations" => $operations,
+                           "actions" => $actions,
+                           "policy" => $policy,
+                           "securityToken" => $sec_token));
+        
+$svr->reply();
+
+?>
