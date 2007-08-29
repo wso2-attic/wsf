a39b4d2a3d894cc98c57f6dc63a07133e7829e21
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-08-29 04:22:24 +0000

    Fixed formatting and names
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6653 a5903396-d722-0410-b921-86c7d4935375

--- php/samples/security/username_token/username_token_client.php
+++ php/samples/security/username_token/username_token_client.php
@@ -15,26 +15,30 @@
  * limitations under the License.
  */
 
+// Request payload string 
 $reqPayloadString = <<<XML
 <ns1:echo xmlns:ns1="http://php.axis2.org/samples"><text>Hello World!</text></ns1:echo>
 XML;
 
 try {
-
+    // Create message with request payload and options
     $reqMessage = new WSMessage($reqPayloadString,
-                                array("to"=>"http://localhost/samples/security/username_token/username_token_service.php",
-                                      "action" => "http://php.axis2.org/samples/echoString"));
+                         array("to" => "http://localhost:9090/samples/security/username_token/username_token_service.php",
+                               "action" => "http://php.axis2.org/samples/echoString"));
+   
+    // Set up security options
+    $security_options = array("useUsernameToken" => TRUE );
+    $policy = new WSPolicy(array("security" => $security_options));
+    $security_token = new WSSecurityToken(array("user" => "Raigama",
+                                                "password" => "RaigamaPW",
+                                                "passwordType" => "Digest"));
     
-    $sec_array = array("useUsernameToken"=>TRUE );
-    $policy = new WSPolicy(array("security"=>$sec_array));
-    $sec_token = new WSSecurityToken(array("user" => "Raigama",
-                                           "password" => "RaigamaPW",
-                                           "passwordType" => "Digest"));
-    
+    // Create client with options
     $client = new WSClient(array("useWSA" => TRUE,
                                  "policy" => $policy,
-                                 "securityToken" => $sec_token));
-				
+                                 "securityToken" => $security_token));
+	
+    // Send request and capture response
     $resMessage = $client->request($reqMessage);
     
     printf("Response = %s \n", $resMessage->str);
@@ -46,6 +50,5 @@ try {
 	} else {
 		printf("Message = %s\n",$e->getMessage());
 	}
-
 }
 ?>
--- php/samples/security/username_token/username_token_service.php
+++ php/samples/security/username_token/username_token_service.php
@@ -15,27 +15,30 @@
  * limitations under the License.
  */
 
+// User defined function implementing service operation 
 function echoFunction($inMessage) {
-
     $returnMessage = new WSMessage($inMessage->str);
-
     return $returnMessage;
 }
 
+// Operations and associated options
 $operations = array("echoString" => "echoFunction");
 $actions = array("http://php.axis2.org/samples/echoString" => "echoString");
-$sec_array = array("useUsernameToken" => TRUE);
 
-$policy = new WSPolicy(array("security"=>$sec_array));
-$sec_token = new WSSecurityToken(array("user" => "Raigama",
-                                       "password" => "RaigamaPW",
-                                       "passwordType" => "Digest"));
+// Security options
+$security_options = array("useUsernameToken" => TRUE);
+$policy = new WSPolicy(array("security"=>$security_options));
+$security_token = new WSSecurityToken(array("user" => "Raigama",
+                                            "password" => "RaigamaPW",
+                                            "passwordType" => "Digest"));
 
-$svr = new WSService(array("operations" => $operations,
-                           "actions" => $actions,
-                           "policy" => $policy,
-                           "securityToken" => $sec_token));
-        
-$svr->reply();
+// Create service with options
+$service = new WSService(array("operations" => $operations,
+                               "actions" => $actions,
+                               "policy" => $policy,
+                               "securityToken" => $security_token));
+
+// Reply to requests
+$service->reply();
 
 ?>
