2dd99f24dfe6bfe20f0777e053b0204998fd0f4a
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2007-08-31 09:17:52 +0000

    New services to test the rest of the algorithmic suites
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6775 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/src/tests/samples/services/encrypt_service_Basic128Sha256.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function echoFunction($inMessage) {
+
+    $returnMessage = new WSMessage($inMessage->str);
+
+    return $returnMessage;
+}
+
+$pub_key = ws_get_cert_from_file("keys/alice_cert.cert");
+$pvt_key = ws_get_key_from_file("keys/bob_key.pem");
+
+$operations = array("echoString" => "echoFunction");
+$sec_array = array("encrypt" => TRUE,
+                    "algorithmSuite" => "Basic128Sha256",
+                    "securityTokenReference" => "EmbeddedToken");
+
+$actions = array("http://php.axis2.org/samples/echoString" => "echoString");
+
+$policy = new WSPolicy(array("security"=>$sec_array));
+$sec_token = new WSSecurityToken(array("privateKey" => $pvt_key,
+                                       "receiverCertificate" =>$pub_key));
+
+$svr = new WSService(array("actions" => $actions,
+                           "operations" => $operations,
+                           "policy" => $policy,
+                           "securityToken" => $sec_token));
+        
+$svr->reply();
+
+?>
---
+++ php/src/tests/samples/services/signing_service_Basic128Sha256Rsa15.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function echoFunction($inMessage) {
+
+    $returnMessage = new WSMessage($inMessage->str);
+
+    return $returnMessage;
+}
+
+$cert = ws_get_cert_from_file("keys/bob_cert.cert");
+$pvt_key = ws_get_key_from_file("keys/bob_key.pem");
+
+$operations = array("echoString" => "echoFunction");
+
+$sec_array = array("sign" => TRUE,
+                    "algorithmSuite" => "Basic128Sha256Rsa15",
+                    "securityTokenReference" => "EmbeddedToken");
+
+$policy = new WSPolicy(array("security"=>$sec_array));
+
+$sec_token = new WSSecurityToken(array("privateKey" => $pvt_key,
+                                       "certificate" => $cert));
+
+$actions = array("http://php.axis2.org/samples/echoString" => "echoString");
+
+$svr = new WSService(array("operations" => $operations,
+                           "actions" => $actions,
+                           "policy" => $policy,
+                           "securityToken" => $sec_token));
+        
+$svr->reply();
+
+?>
