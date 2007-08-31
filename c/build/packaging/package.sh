0e4a467427b3c2d07a63f6ab2faa85b62658b52a
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-08-31 09:24:42 +0000

    Added build stuff to package script
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6777 a5903396-d722-0410-b921-86c7d4935375

--- c/build/packaging/package.sh
+++ c/build/packaging/package.sh
@@ -1,4 +1,18 @@
-set -e
+#!/bin/bash
+
+set -e 
+
+export AXIS2C_HOME=`pwd`/deploy
+export WSFC_HOME=`pwd`/deploy
+
+./build/copy_build_files.sh
+
+./autogen.sh
+./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-openssl
+make -j 10
+make install
+make samples -j 10
+
 make dist
 tar xf wso2-wsf-c-src-1.1.0.tar.gz
 cd wso2-wsf-c-src-1.1.0
