e8c301708c0e22edb97bf7efdb3f43c7b13ae92f
sandakith <sandakith@a5903396-d722-0410-b921-86c7d4935375>
2007-09-04 07:09:13 +0000

    Fix the service loading of the dump aar tool, also updated the error handling when no IDE configured services available
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6899 a5903396-d722-0410-b921-86c7d4935375

--- java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/dumpaar/WSASDumpAARSelectionPage.java
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/dumpaar/WSASDumpAARSelectionPage.java
@@ -21,8 +21,8 @@ import java.util.List;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
@@ -33,6 +33,8 @@ import org.wso2.wsf.ide.wtp.ext.server.bean.WSASConfigurationBean;
 public class WSASDumpAARSelectionPage extends AbstractDumpAARWizardPage{
 	
     private Combo serviceSelectionCombo;
+    private String selectedService;
+    IPath wsasRepoPath;
    
     public WSASDumpAARSelectionPage(){
         super("page1");
@@ -56,27 +58,32 @@ public class WSASDumpAARSelectionPage extends AbstractDumpAARWizardPage{
         container.setLayout(layout);
         GridData gd = new GridData(GridData.FILL_HORIZONTAL);
 		
-		serviceSelectionCombo = new Combo(container,SWT.NONE);
+		serviceSelectionCombo = new Combo(container,SWT.NONE | SWT.READ_ONLY);
 		serviceSelectionCombo.setLayoutData(gd);
 		fillSrviceSelectionCombo();
 		serviceSelectionCombo.select(0);
-		serviceSelectionCombo.addModifyListener(new ModifyListener(){
-		    public void modifyText(ModifyEvent e){
-	        	handleModify();
-	    	}
+		setSelectedService(serviceSelectionCombo.getItem(0));
+		serviceSelectionCombo.addSelectionListener(new SelectionListener(){
+			public void widgetSelected(SelectionEvent e) {
+				handleModify(serviceSelectionCombo.getText());
+			}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
 		
 		setControl(container);
 		
 		//call the handle modify method if the settings are restored
 		if (restoredFromPreviousSettings){
-		    handleModify();
+		    handleModify(null);
 		}
     }
     
     
-    private void handleModify(){
-    	//Handle the modify event 
+    private void handleModify(String selectedService){
+    	if (selectedService != null) {
+			setSelectedService(selectedService);
+		}
     }
     
 	public boolean getWizardComplete() {
@@ -86,20 +93,32 @@ public class WSASDumpAARSelectionPage extends AbstractDumpAARWizardPage{
 	private void fillSrviceSelectionCombo(){
 		//Check whether there is IDE configured services in the WSAS services directory
 		WSASConfigurationBean.Init();
-		IPath wsasInstallationPath = new Path(WSASConfigurationBean.getWsasInstallationPath());
-		wsasInstallationPath = wsasInstallationPath.append("repository");
-		wsasInstallationPath = wsasInstallationPath.append("services");
-		List<IPath> ideConfiguredServices=FileUtils.getDirsOnly(wsasInstallationPath.toOSString());
+		wsasRepoPath = new Path(WSASConfigurationBean.getWsasInstallationPath());
+		wsasRepoPath = wsasRepoPath.append("repository");
+		wsasRepoPath = wsasRepoPath.append("services");
+		List<IPath> ideConfiguredServices=FileUtils.getDirsOnly(wsasRepoPath.toOSString());
 		//If services are there
 		if (ideConfiguredServices.size() != 0) {
+			updateStatus(null);
 			for (Iterator<IPath> iterator = ideConfiguredServices.iterator(); iterator.hasNext();){
 				IPath serviPath = (IPath) iterator.next();
 				serviceSelectionCombo.add(serviPath.lastSegment());
 			}
 		}else{
-			//TODO : invalidate the wizard by error that no services are available
-			//showErrorMessage(ServiceArchiver.getResourceString("wizard.codegen.unknown.error") + "  "+  e.getMessage());
+			updateStatus(WSASDumpAARPlugin.getResourceString("error.no.services"));
 		}
+	}
+
+	public String getSelectedService() {
+		return selectedService;
+	}
+
+	private void setSelectedService(String selectedService) {
+		this.selectedService = selectedService;
+	}
+	
+	public IPath getWSASRepoPath() {
+		return wsasRepoPath;
 	}
 
 }
