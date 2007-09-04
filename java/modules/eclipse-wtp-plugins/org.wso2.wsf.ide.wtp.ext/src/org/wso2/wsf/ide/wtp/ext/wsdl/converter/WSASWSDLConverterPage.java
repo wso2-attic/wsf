80bfecb452030bbd9c0a2954bc89024b6e45e463
sandakith <sandakith@a5903396-d722-0410-b921-86c7d4935375>
2007-09-04 04:53:32 +0000

    Added the controls to the WSDL Conveter Tool
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6888 a5903396-d722-0410-b921-86c7d4935375

--- java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/wsdl/converter/WSASWSDLConverterPage.java
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/wsdl/converter/WSASWSDLConverterPage.java
@@ -18,15 +18,20 @@ package org.wso2.wsf.ide.wtp.ext.wsdl.converter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
 
 public class WSASWSDLConverterPage extends AbstractWSDLConverterWizardPage{
 	
-    private Combo serviceSelectionCombo;
-   
+	Text wsdlPathText;
+	
     public WSASWSDLConverterPage(){
         super("page1");
     }
@@ -41,24 +46,48 @@ public class WSASWSDLConverterPage extends AbstractWSDLConverterWizardPage{
      * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
      */
     public void createControl(Composite parent) {
-        Composite container = new Composite(parent, SWT.NULL);
-        GridLayout layout = new GridLayout();
-        layout.numColumns=3;
-        layout.verticalSpacing = 9;
+        final Composite container = new Composite(parent, SWT.NULL);
+        GridLayout lo = new GridLayout(1,true);
+        container.setLayout(lo);
         
-        container.setLayout(layout);
-        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		
-		serviceSelectionCombo = new Combo(container,SWT.NONE);
-		serviceSelectionCombo.setLayoutData(gd);
-		fillSrviceSelectionCombo();
-		serviceSelectionCombo.select(0);
-		serviceSelectionCombo.addModifyListener(new ModifyListener(){
-		    public void modifyText(ModifyEvent e){
-	        	handleModify();
-	    	}
-		});
-		
+        Group wsdlGroup = new Group(container, SWT.NONE);
+        wsdlGroup.setText("Please upload your WSDL 1.1");
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 14;
+        wsdlGroup.setLayout(layout);
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL);
+        wsdlGroup.setLayoutData(gd);
+        
+        wsdlPathText = new Text(wsdlGroup,SWT.NONE);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalSpan=13;
+        wsdlPathText.setLayoutData(gd);
+        wsdlPathText.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent e) {
+			}
+        });
+        
+        Button wsdlBrowseButton = new Button(wsdlGroup,SWT.NONE);
+        wsdlBrowseButton.setText("Upload");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        wsdlBrowseButton.setLayoutData(gd);
+        wsdlBrowseButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				handleBrowse(container);
+			}     
+		}); 
+       
+        Button convertButton = new Button(wsdlGroup,SWT.NONE);
+        convertButton.setText("Convert to WSDL 2.0");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        convertButton.setLayoutData(gd);
+        convertButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				//TODO Call convert Mechanism
+			}     
+		}); 		
 		setControl(container);
 		
 		//call the handle modify method if the settings are restored
@@ -76,7 +105,15 @@ public class WSASWSDLConverterPage extends AbstractWSDLConverterWizardPage{
 		return false;
 	}
 	
-	private void fillSrviceSelectionCombo(){
+	/**
+	 * Pops up the file browse dialog box
+	 */
+	private void handleBrowse(Composite parent) {
+		DirectoryDialog fileDialog = new DirectoryDialog(parent.getShell());
+		String fileName = fileDialog.open();
+		if (fileName != null) {
+			wsdlPathText.setText(fileName);
+		}
 	}
 
 }
