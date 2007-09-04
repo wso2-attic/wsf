f82c06c6aae702b2d2d40f3edd2211cce60c64f2
sandakith <sandakith@a5903396-d722-0410-b921-86c7d4935375>
2007-09-04 04:22:17 +0000

    Adding the controls to the Archive Validator wizard
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@6883 a5903396-d722-0410-b921-86c7d4935375

--- java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/validate/service/WSASArchiveValidatePage.java
+++ java/modules/eclipse-wtp-plugins/org.wso2.wsf.ide.wtp.ext/src/org/wso2/wsf/ide/wtp/ext/validate/service/WSASArchiveValidatePage.java
@@ -18,15 +18,21 @@ package org.wso2.wsf.ide.wtp.ext.validate.service;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class WSASArchiveValidatePage extends AbstractArchiveValidateWizardPage{
 	
-    private Combo serviceSelectionCombo;
-   
+	Text servicesXMlPathText;
+	Text aarPathText;
     public WSASArchiveValidatePage(){
         super("page1");
     }
@@ -41,24 +47,96 @@ public class WSASArchiveValidatePage extends AbstractArchiveValidateWizardPage{
      * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
      */
     public void createControl(Composite parent) {
-        Composite container = new Composite(parent, SWT.NULL);
-        GridLayout layout = new GridLayout();
-        layout.numColumns=3;
-        layout.verticalSpacing = 9;
+        final Composite container = new Composite(parent, SWT.NULL);
+        GridLayout lo = new GridLayout(1,true);
+        container.setLayout(lo);
         
-        container.setLayout(layout);
-        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		
-		serviceSelectionCombo = new Combo(container,SWT.NONE);
-		serviceSelectionCombo.setLayoutData(gd);
-		fillSrviceSelectionCombo();
-		serviceSelectionCombo.select(0);
-		serviceSelectionCombo.addModifyListener(new ModifyListener(){
-		    public void modifyText(ModifyEvent e){
-	        	handleModify();
-	    	}
-		});
-		
+        Group aarGroup = new Group(container, SWT.NONE);
+        aarGroup.setText("Select AAR (Service Archive)");
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 14;
+        aarGroup.setLayout(layout);
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL);
+        aarGroup.setLayoutData(gd);
+        
+        aarPathText = new Text(aarGroup,SWT.NONE);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalSpan=13;
+        aarPathText.setLayoutData(gd);
+        aarPathText.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent e) {
+			}
+        });
+        
+        Button validateAARBrowseButton = new Button(aarGroup,SWT.NONE);
+        validateAARBrowseButton.setText("Browse");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        validateAARBrowseButton.setLayoutData(gd);
+        validateAARBrowseButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				handleBrowse(container, aarPathText);
+			}     
+		}); 
+       
+        Button validateAARButton = new Button(aarGroup,SWT.NONE);
+        validateAARButton.setText("Validate");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        validateAARButton.setLayoutData(gd);
+        validateAARButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				//TODO Call validate Mechanism
+			}     
+		}); 
+        
+        /////////////////////////////////////////////////////////////////////////////////
+        
+	    Label separator = new Label( container, SWT.NONE);  // Leave some vertical space.
+	    gd = new GridData(GridData.FILL_HORIZONTAL);
+	    gd.horizontalSpan = 1;
+	    separator.setText( " " );
+	    separator.setLayoutData( gd );
+	    
+        Group serviceXMLGroup = new Group(container, SWT.NONE);
+        serviceXMLGroup.setText("Select Services.xml");
+        GridLayout layout1 = new GridLayout();
+        layout1.numColumns = 14;
+        serviceXMLGroup.setLayout(layout);
+        gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL);
+        serviceXMLGroup.setLayoutData(gd);
+        
+        final Text servicesXMlPathText = new Text(serviceXMLGroup,SWT.NONE);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalSpan=13;
+        servicesXMlPathText.setLayoutData(gd);
+        servicesXMlPathText.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent e) {
+			}
+        });
+        
+        Button validateServicesXmlBrowseButton = new Button(serviceXMLGroup,SWT.NONE);
+        validateServicesXmlBrowseButton.setText("Browse");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        validateServicesXmlBrowseButton.setLayoutData(gd);
+        validateServicesXmlBrowseButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				handleBrowse(container, servicesXMlPathText);
+			}     
+		}); 
+        
+        Button validateServicesXmlButton = new Button(serviceXMLGroup,SWT.NONE);
+        validateServicesXmlButton.setText("Validate");
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        validateServicesXmlButton.setLayoutData(gd);
+        validateServicesXmlButton.addSelectionListener( new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e){
+				//TODO Call validate Mechanism
+			}     
+		}); 
+        
 		setControl(container);
 		
 		//call the handle modify method if the settings are restored
@@ -76,7 +154,15 @@ public class WSASArchiveValidatePage extends AbstractArchiveValidateWizardPage{
 		return false;
 	}
 	
-	private void fillSrviceSelectionCombo(){
+	/**
+	 * Pops up the file browse dialog box
+	 */
+	private void handleBrowse(Composite parent, Text pathText) {
+		DirectoryDialog fileDialog = new DirectoryDialog(parent.getShell());
+		String fileName = fileDialog.open();
+		if (fileName != null) {
+			pathText.setText(fileName);
+		}
 	}
 
 }
