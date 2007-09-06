86524ffca9e8460d5fbb63a48ef686417d39ee20
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-09-06 10:51:37 +0000

    build files updated for the new release
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@7055 a5903396-d722-0410-b921-86c7d4935375

--- php/build.bat
+++ php/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@call clean.bat
+rem @call clean.bat
 
 @copy configure.in wsf_c\configure.in
 @copy wsf_c_build.bat wsf_c\build.bat
@@ -11,29 +11,31 @@
 
 @cd ..
 
-@mkdir wso2-wsf-php-bin-1.0.0-win32
+@mkdir wso2-wsf-php-bin-1.1.0-win32
 
-@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.0.1 wso2-wsf-php-bin-1.0.0-win32\wsf_c
+@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.1.0 wso2-wsf-php-bin-1.1.0-win32\wsf_c
 
-@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\services
+@if exist wso2-wsf-php-bin-1.1.0-win32\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.1.0-win32\wsf_c\services
 
-@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\savan
+@if exist wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\savan
 
-@if exist wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32\wsf_c\modules\logging
+@if exist wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\sandesha2 rmdir /S /Q wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\sandesha2
 
-@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.0.0-win32\scripts
+@if exist wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.1.0-win32\wsf_c\modules\logging
 
-@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.0.0-win32\samples
+@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.1.0-win32\scripts
 
-@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.0.0-win32\docs
+@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.1.0-win32\samples
 
-@if exist NOTICE copy /Y NOTICE wso2-wsf-php-bin-1.0.0-win32
+@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.1.0-win32\docs
 
-@if exist README.INSTALL_WIN32 copy /Y README.INSTALL_WIN32 wso2-wsf-php-bin-1.0.0-win32
+@if exist NOTICE copy /Y NOTICE wso2-wsf-php-bin-1.1.0-win32
 
-@if exist README.INSTALL copy /Y README.INSTALL wso2-wsf-php-bin-1.0.0-win32
+@if exist README.INSTALL_WIN32 copy /Y README.INSTALL_WIN32 wso2-wsf-php-bin-1.1.0-win32
 
-@if exist LICENSE copy /Y LICENSE wso2-wsf-php-bin-1.0.0-win32
+@if exist README.INSTALL copy /Y README.INSTALL wso2-wsf-php-bin-1.1.0-win32
+
+@if exist LICENSE copy /Y LICENSE wso2-wsf-php-bin-1.1.0-win32
 
 @cd src
 
--- php/clean.bat
+++ php/clean.bat
@@ -1,6 +1,6 @@
-@if exist wsf_c\wso2-wsf-c-bin-1.0.1 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.0.1
+@if exist wsf_c\wso2-wsf-c-bin-1.1.0 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.1.0
 
-@if exist wso2-wsf-php-bin-1.0.0-win32 rmdir /S /Q wso2-wsf-php-bin-1.0.0-win32
+@if exist wso2-wsf-php-bin-1.1.0-win32 rmdir /S /Q wso2-wsf-php-bin-1.1.0-win32
 
 @if exist wsf_c\axis2c\build\axis2c-bin-1.1.0-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-1.1.0-win32
 
@@ -8,7 +8,7 @@
 
 @if exist wsf_c\axis2c\build\win32\int.msvc rmdir /s /q wsf_c\axis2c\build\win32\int.msvc
 
-@if exist wsf_c\rampartc\build\rampartc-0.90 rmdir /s /q wsf_c\rampartc\build\rampartc-0.90
+@if exist wsf_c\rampartc\build\rampartc-1.0 rmdir /s /q wsf_c\rampartc\build\rampartc-1.0
 
 @if exist wsf_c\rampartc\build\win32\int.msvc rmdir /s /q wsf_c\rampartc\build\win32\int.msvc
 
