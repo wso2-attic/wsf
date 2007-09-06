c8d8991508e6ad3e0385af04d040c8157c3f68e4
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-09-06 10:49:14 +0000

    .rc script and build file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@7054 a5903396-d722-0410-b921-86c7d4935375

--- php/src/build.mk
+++ php/src/build.mk
@@ -5,7 +5,7 @@ CFLAGS = /nologo /w /D "WIN32" /D "_WINDOWS" /D "ZTS" /D "PHP_WIN32" /D "COMPILE
 	 /D "_USE_32BIT_TIME_T=1" /D "USE_SANDESHA2" /D "ZEND_WIN32" /D "ZEND_DEBUG=0" \
 	 /D "_MBCS" 
 
-LDFLAGS = /nologo /LIBPATH:.\..\wso2-wsf-php-bin-1.0.0-win32\wsf_c\lib /LIBPATH:$(PHP_BIN_DIR)\dev \
+LDFLAGS = /nologo /LIBPATH:.\..\wso2-wsf-php-bin-1.1.0-win32\wsf_c\lib /LIBPATH:$(PHP_BIN_DIR)\dev \
 	  /LIBPATH:$(LIBXML2_BIN_DIR)\lib
 
 LIBS = php5ts.lib axutil.lib axis2_engine.lib axis2_parser.lib \
@@ -14,7 +14,7 @@ LIBS = php5ts.lib axutil.lib axis2_engine.lib axis2_parser.lib \
 
 INCLUDE_PATH = /I$(PHP_SRC_DIR) /I$(PHP_SRC_DIR)\main /I$(PHP_SRC_DIR)\regex \
 	       /I$(PHP_SRC_DIR)\ext /I$(PHP_SRC_DIR)\sapi /I$(PHP_SRC_DIR)\Zend \
-	       /I$(PHP_SRC_DIR)\TSRM /I.\..\wso2-wsf-php-bin-1.0.0-win32\wsf_c\include \
+	       /I$(PHP_SRC_DIR)\TSRM /I.\..\wso2-wsf-php-bin-1.1.0-win32\wsf_c\include \
 	       /I$(LIBXML2_BIN_DIR)\include /I$(WIN32BUILD_DIR)\include \
 	       /I$(ICONV_BIN_DIR)\include 
 
@@ -30,7 +30,7 @@ wsf.dll :
 	@if not exist intdir mkdir intdir
 	@cl.exe $(CFLAGS) $(INCLUDE_PATH) *.c /Fointdir\ /c
 	@rc.exe /r /fo "intdir/wsf.res" wsf.rc
-	@link.exe $(LDFLAGS) intdir\*.obj intdir\wsf.res $(LIBS) /DLL  /OUT:.\..\wso2-wsf-php-bin-1.0.0-win32\wsf.dll
+	@link.exe $(LDFLAGS) intdir\*.obj intdir\wsf.res $(LIBS) /DLL  /OUT:.\..\wso2-wsf-php-bin-1.1.0-win32\wsf.dll
 
 wsfphp: wsf.dll
 
@@ -38,8 +38,8 @@ clean:
 	@if exist intdir rmdir /s /q intdir
 		
 copy_sqlite:
-	@if exist $(SQLITE_BIN_DIR)\sqlite3.dll copy /Y  $(SQLITE_BIN_DIR)\sqlite3.dll .\..\wso2-wsf-php-bin-1.0.0-win32\wsf_c\lib
-	@if exist $(SQLITE_BIN_DIR)\sqlite3.exe copy /Y  $(SQLITE_BIN_DIR)\sqlite3.exe .\..\wso2-wsf-php-bin-1.0.0-win32\wsf_c\lib
+	@if exist $(SQLITE_BIN_DIR)\sqlite3.dll copy /Y  $(SQLITE_BIN_DIR)\sqlite3.dll .\..\wso2-wsf-php-bin-1.1.0-win32\wsf_c\lib
+	@if exist $(SQLITE_BIN_DIR)\sqlite3.exe copy /Y  $(SQLITE_BIN_DIR)\sqlite3.exe .\..\wso2-wsf-php-bin-1.1.0-win32\wsf_c\lib
 
 dist: clean copy_sqlite wsfphp
 
