f8e8bd0fd38587935519a46d313f68175b56e9a9
samisa <samisa@a5903396-d722-0410-b921-86c7d4935375>
2007-09-26 10:45:32 +0000

    Added version changes
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@8024 a5903396-d722-0410-b921-86c7d4935375

--- c/build.bat
+++ c/build.bat
@@ -25,14 +25,14 @@ rem Build Apache Rampart/C
 @if not %ERRORLEVEL% EQU 0 goto rampart_error
 
 @cd ..
-@xcopy /E /Q /I /Y rampartc-1.0\modules\rampart .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\modules\rampart
-@copy /Y rampartc-1.0\include\*.h .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\include\
-@copy /Y rampartc-1.0\modules\rampart\mod_rampart.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y rampartc-1.0\modules\rampart\mod_rampart.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y rampartc-1.0\bin\samples\services\sec_echo\server_axis2.xml .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\axis2.xml
-@xcopy /E /I /Q /Y rampartc-1.0\bin\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\rampart
+@xcopy /E /Q /I /Y rampartc-1.0.0\modules\rampart .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\modules\rampart
+@copy /Y rampartc-1.0.0\include\*.h .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\include\
+@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
+@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
+@copy /Y rampartc-1.0.0\bin\samples\services\sec_echo\server_axis2.xml .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\axis2.xml
+@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\rampart
 @xcopy /E /I /Q /Y .\..\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\rampart
-@xcopy /E /I /Q /Y rampartc-1.0\bin\samples\services\sec_echo .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\sec_echo
+@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples\services\sec_echo .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\sec_echo
 @goto build_sandesha2
 
 :rampart_error
@@ -53,10 +53,8 @@ rem Build Apache Sandesha2/C
 @copy /Y sandesha2\bin\samples\sandesha2\*.exe .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
 @xcopy /E /Q /I /Y sandesha2\services\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\RMSampleService
 @xcopy /E /Q /I /Y sandesha2\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\sandesha2
-@copy /Y sandesha2\axis2.xml .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\axis2.xml
 @copy /Y sandesha2\include\*.h  .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\include\
-@copy /Y sandesha2\sqlite_schema.bat  .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\sqlite_schema.bat
-@copy /Y  sandesha2\bin\samples\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
+@xcopy /E /Q /I /Y  sandesha2\bin\samples\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
 @mkdir .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
 @copy /Y ..\config\*.txt .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
 @copy /Y ..\config\*.bat .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
@@ -90,8 +88,8 @@ rem Build Apache Savan/C
 @xcopy /E /Q /I /Y savanc\services\listener .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\listener
 @xcopy /E /Q /I /Y savanc\services\subscription .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\subscription
 @xcopy /E /Q /I /Y savanc\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\savan
-@copy /Y savanc\modules\savan\savan_client.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y savanc\modules\savan\savan_client.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
+@copy /Y savanc\modules\savan\mod_savan.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
+@copy /Y savanc\modules\savan\mod_savan.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
 @mkdir .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\savan
 @copy /Y savanc\bin\samples\savan\*.exe .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\savan
 @goto pack_wsfc
