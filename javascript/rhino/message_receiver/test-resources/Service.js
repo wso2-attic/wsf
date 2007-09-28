2f3c2492fc0a6e74895846581664d8a249f3d345
keith <keith@a5903396-d722-0410-b921-86c7d4935375>
2007-09-28 06:38:47 +0000

    Adding missing license headers
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@8120 a5903396-d722-0410-b921-86c7d4935375

--- javascript/rhino/message_receiver/test-resources/Service.js
+++ javascript/rhino/message_receiver/test-resources/Service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
+ * Copyright 2006 WSO2, Inc. http://www.wso2.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
