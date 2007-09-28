b680743e05fcdb4cbce79947e1d708daf20fd21f
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-09-28 12:01:12 +0000

    ssl_client.php added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@8164 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/ssl_client.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+$reqPayloadString = <<<XML
+	<ns1:echoString xmlns:ns1="http://php.axis2.org/samples">
+		<text>Hello World!</text>
+	</ns1:echoString>
+XML;
+
+try {
+
+    $client = new WSClient(array("to"=>"https://localhost/samples/echo_service.php",
+					   "CACert"=>"E:\\cacert.pem"));
+    $msg = $client->request($reqPayloadString);
+    echo $msg->str;
+        
+} catch (Exception $e) {
+	if ($e instanceof WSFault) {
+		printf("Soap Fault: %s\n", $e->Code);
+	} else {
+		printf("Message = %s\n",$e->getMessage());
+	}
+}
+?>
