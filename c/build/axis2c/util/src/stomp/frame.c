c0f52321588f5ca3ceb7cc6776721a28fdd5d755
dinesh <dinesh@a5903396-d722-0410-b921-86c7d4935375>
2007-10-04 12:51:35 +0000

    made modification to code indentation
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@8526 a5903396-d722-0410-b921-86c7d4935375

--- c/build/axis2c/util/src/stomp/frame.c
+++ c/build/axis2c/util/src/stomp/frame.c
@@ -5,177 +5,222 @@
 
 #define BUFF_SIZE 1024
 
-static axis2_char_t *
-read_stomp_buffer (
-    const axutil_env_t *env,
-    axutil_stream_t *stream);
+#define MEM_ZERO(s) memset((void *)s, 0, sizeof (s))
 
+static axis2_char_t *read_stomp_buffer(
+    const axutil_env_t * env,
+    axutil_stream_t * stream);
 
 axutil_stomp_frame_t *
-axutil_stomp_frame_create (const axutil_env_t *env)
+axutil_stomp_frame_create(
+    const axutil_env_t * env)
 {
-    AXIS2_ENV_CHECK (env, NULL);
     axutil_stomp_frame_t *frame = NULL;
-    frame = AXIS2_MALLOC (env->allocator, sizeof (axutil_stomp_frame_t));
-    frame->command = NULL;
-    frame->headers = axutil_array_list_create (env, 3);
-    frame->body = NULL;
+    frame = AXIS2_MALLOC(env->allocator, sizeof(axutil_stomp_frame_t));
+    MEM_ZERO(frame);
+    frame->headers = axutil_array_list_create(env, 3);
     return frame;
 }
 
-
 void
-axutil_stomp_frame_free (axutil_stomp_frame_t *frame,
-    const axutil_env_t *env)
+axutil_stomp_frame_free(
+    axutil_stomp_frame_t * frame,
+    const axutil_env_t * env)
 {
-    if (!frame)
-        return;
-
-    int size = 0;
-    int i = 0;
     void *headers;
+    int i;
+    int size;
+    i = 0;
+    size = 0;
+
+    if (!frame)
+    {
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI, "[stomp]Frame is NULL");
+        return;
+    }
+
     if (frame->command)
-        AXIS2_FREE (env->allocator, frame->command);
+    {
+        AXIS2_FREE(env->allocator, frame->command);
+    }
 
     if (frame->body)
-        AXIS2_FREE (env->allocator, frame->body);
+    {
+        AXIS2_FREE(env->allocator, frame->body);
+    }
 
-    size = axutil_array_list_size (frame->headers, env);
+    size = axutil_array_list_size(frame->headers, env);
 
     if (size > 0)
     {
         for (i = 0; i < size; i++)
         {
-            headers = axutil_array_list_get (frame->headers, env, i);
-            AXIS2_FREE (env->allocator, headers);
+            headers = axutil_array_list_get(frame->headers, env, i);
+            AXIS2_FREE(env->allocator, headers);
         }
     }
-    axutil_array_list_free (frame->headers, env);
-
-    AXIS2_FREE (env->allocator, frame);
+    axutil_array_list_free(frame->headers, env);
+    AXIS2_FREE(env->allocator, frame);
 }
 
-
 axis2_status_t
-axutil_stomp_frame_set_command (axutil_stomp_frame_t *frame,
-    const axutil_env_t *env,
-    axis2_char_t *command)
+axutil_stomp_frame_set_command(
+    axutil_stomp_frame_t * frame,
+    const axutil_env_t * env,
+    axis2_char_t * command)
 {
-    AXIS2_ENV_CHECK (env, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, frame, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, command, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, frame, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, command, AXIS2_FAILURE);
+    if (!frame->command)
+    {
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI,
+                        "[stomp]stomp command is not found");
+        return AXIS2_FAILURE;
+    }
+
     frame->command = command;
-    AXIS2_LOG_DEBUG (env->log, AXIS2_LOG_SI, "%s stomp command is set", command);
+    AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "[stomp]%s stomp command is set",
+                    command);
     return AXIS2_SUCCESS;
 }
-
 
 axis2_status_t
-axutil_stomp_frame_set_header (axutil_stomp_frame_t *frame,
-    const axutil_env_t *env,
-    axis2_char_t *header,
-    axis2_char_t *value)
+axutil_stomp_frame_set_header(
+    axutil_stomp_frame_t * frame,
+    const axutil_env_t * env,
+    axis2_char_t * header,
+    axis2_char_t * value)
 {
-    AXIS2_ENV_CHECK (env, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, frame, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, header, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, value, AXIS2_FAILURE);
-
     axis2_char_t *stomp_header = NULL;
-    stomp_header = axutil_strcat (env, header, ":", value, NULL);
-    axutil_array_list_add (frame->headers, env, (void *)stomp_header);
-    AXIS2_LOG_DEBUG (env->log, AXIS2_LOG_SI, 
-        "%s stomp header is set, value is %s", header, value);
+
+    AXIS2_PARAM_CHECK(env->error, frame, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, header, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, value, AXIS2_FAILURE);
+
+    stomp_header = axutil_strcat(env, header, ":", value, NULL);
+    axutil_array_list_add(frame->headers, env, (void *) stomp_header);
+    AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI,
+                    "[stomp] %s stomp header is set, value is %s", header,
+                    value);
+    AXIS2_FREE(env->allocator, header);
+    AXIS2_FREE(env->allocator, value);
     return AXIS2_SUCCESS;
 }
 
-
-axis2_status_t 
-axutil_stomp_frame_set_body (axutil_stomp_frame_t *frame,
-    const axutil_env_t *env,
-    axis2_char_t *body)
+axis2_status_t
+axutil_stomp_frame_set_body(
+    axutil_stomp_frame_t * frame,
+    const axutil_env_t * env,
+    axis2_char_t * body)
 {
-    AXIS2_ENV_CHECK (env, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, frame, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, body, AXIS2_FAILURE);
+    AXIS2_ENV_CHECK(env, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, frame, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, body, AXIS2_FAILURE);
+    if (!frame->body)
+    {
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI, "[stomp]stomp body is not set");
+        return AXIS2_FAILURE;
+    }
     frame->body = body;
-    AXIS2_LOG_DEBUG (env->log, AXIS2_LOG_SI, "%s stomp body is set", body);
+    AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI, "%s stomp body is set", body);
     return AXIS2_SUCCESS;
 }
 
 int
-axutil_stomp_frame_write (axutil_stomp_frame_t *frame,
-    const axutil_env_t *env,
-    axutil_stream_t *stream)
+axutil_stomp_frame_write(
+    axutil_stomp_frame_t * frame,
+    const axutil_env_t * env,
+    axutil_stream_t * stream)
 {
-    AXIS2_ENV_CHECK (env, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, frame, AXIS2_FAILURE);
     int total = 0;
-    /* command */
-    total += axutil_stream_write (stream, env, frame->command, strlen (frame->command));
-    total += axutil_stream_write (stream, env, "\n", 1);
 
     /* headers */
     int size = 0;
     int i = 0;
     axis2_char_t *header = NULL;
-    size = axutil_array_list_size (frame->headers, env);
+
+    AXIS2_ENV_CHECK(env, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, frame, AXIS2_FAILURE);
+
+    /* command */
+    total +=
+        axutil_stream_write(stream, env, frame->command,
+                            strlen(frame->command));
+    total += axutil_stream_write(stream, env, "\n", 1);
+
+    size = axutil_array_list_size(frame->headers, env);
     if (size > 0)
     {
         for (i = 0; i < size; i++)
         {
-            header = (axis2_char_t *) axutil_array_list_get (frame->headers, env, i);
-                total += axutil_stream_write (stream, env, header, strlen (header));
-                total += axutil_stream_write (stream, env, "\n", 1);
+            header =
+                (axis2_char_t *) axutil_array_list_get(frame->headers, env, i);
+            total += axutil_stream_write(stream, env, header, strlen(header));
+            total += axutil_stream_write(stream, env, "\n", 1);
         }
     }
-    total += axutil_stream_write (stream, env, "\n", 1);
+    total += axutil_stream_write(stream, env, "\n", 1);
 
     /* body */
     if (frame->body)
     {
-        total += axutil_stream_write (stream, env, frame->body, strlen (frame->body));
+        total +=
+            axutil_stream_write(stream, env, frame->body, strlen(frame->body));
     }
-    total += axutil_stream_write (stream, env, "\0\n", 2);
-    AXIS2_LOG_DEBUG (env->log, AXIS2_LOG_SI, 
-                     "stomp frame is write to stream %d of total bytes", total);
+    total += axutil_stream_write(stream, env, "\0\n", 2);
+    AXIS2_LOG_DEBUG(env->log, AXIS2_LOG_SI,
+                    "[stomp]stomp frame is write to stream %d of total bytes",
+                    total);
     return total;
 }
 
-
 axis2_char_t *
-read_stomp_buffer (const axutil_env_t *env,
-    axutil_stream_t *stream)
+read_stomp_buffer(
+    const axutil_env_t * env,
+    axutil_stream_t * stream)
 {
     int size = BUFF_SIZE;
-    axis2_char_t *tmp_buffer = (axis2_char_t *)AXIS2_MALLOC (env->allocator, size);
-    axis2_char_t buff [1];
+    axis2_char_t *tmp_buffer =
+        (axis2_char_t *) AXIS2_MALLOC(env->allocator, (size_t) size);
+    axis2_char_t buff[1];
     axis2_char_t end[1];
     int i = 0;
     int len = 0;
 
-    while (1 && stream)
+    memset((void *) tmp_buffer, 0, (size_t) size);
+    if (!stream)
     {
-        len = axutil_stream_read (stream, env, &buff, 1);
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI, "[stomp]stream is NULL");
+        return tmp_buffer;
+    }
+
+    while (1)
+    {
+        len = axutil_stream_read(stream, env, &buff, 1);
         if (len > 0)
         {
             if (i >= size)
             {
-                size <<= 2;
-                axis2_char_t *new_buffer = (axis2_char_t *)AXIS2_MALLOC (env->allocator, size);
-                memcpy (new_buffer, tmp_buffer, size);
-                AXIS2_FREE (env->allocator, tmp_buffer);
+                axis2_char_t *new_buffer =
+                    (axis2_char_t *) AXIS2_MALLOC(env->allocator,
+                                                  (size_t) size);
+                if (size > 0)
+                {
+                    size <<= 2;
+                }
+                memcpy(new_buffer, tmp_buffer, (size_t) size);
+                AXIS2_FREE(env->allocator, tmp_buffer);
                 tmp_buffer = new_buffer;
             }
-            memcpy (tmp_buffer + i, buff, 1);
+            memcpy(tmp_buffer + i, buff, 1);
             i++;
 
-            if (tmp_buffer[i-1] == 0)
+            if (tmp_buffer[i - 1] == 0)
             {
-                len = axutil_stream_read (stream, env, &end, 1);
-                if (end [0] != '\n')
+                len = axutil_stream_read(stream, env, &end, 1);
+                if (end[0] != '\n')
                 {
-                    return AXIS2_FAILURE;
+                    return NULL;
                 }
                 break;
             }
@@ -184,25 +229,30 @@ read_stomp_buffer (const axutil_env_t *env,
             return NULL;
 
     }
-
     return tmp_buffer;
 }
 
 axutil_stomp_frame_t *
-axutil_stomp_frame_read (const axutil_env_t *env,
-    axutil_stream_t *stream)
+axutil_stomp_frame_read(
+    const axutil_env_t * env,
+    axutil_stream_t * stream)
 {
     axutil_stomp_frame_t *frame = NULL;
     axis2_char_t *buffer = NULL;
     axis2_char_t *p = NULL;
     int h;
 
-    frame = axutil_stomp_frame_create (env);
-    buffer = read_stomp_buffer (env, stream);
-    p = strstr (buffer, "\n");
+    frame = axutil_stomp_frame_create(env);
+    buffer = read_stomp_buffer(env, stream);
+    p = strstr(buffer, "\n");
     h = p - buffer;
     if (!p)
     {
+        axutil_stomp_frame_free(frame, env);
+        if (buffer)
+        {
+            AXIS2_FREE(env->allocator, buffer);
+        }
         return NULL;
     }
     *p = 0;
@@ -211,10 +261,10 @@ axutil_stomp_frame_read (const axutil_env_t *env,
     buffer = p + 1;
     while (h)
     {
-        p = strstr (buffer, "\n");
+        p = strstr(buffer, "\n");
         h = p - buffer;
         *p = 0;
-        axutil_array_list_add (frame->headers, env, (void *)buffer);
+        axutil_array_list_add(frame->headers, env, (void *) buffer);
         buffer = p + 1;
     }
 
--- c/build/axis2c/util/src/stomp/stomp.c
+++ c/build/axis2c/util/src/stomp/stomp.c
@@ -2,106 +2,106 @@
 #include <strings.h>
 #include <axutil_stomp.h>
 
-
 axutil_stomp_t *
-axutil_stomp_create (const axutil_env_t *env,
+axutil_stomp_create(
+    const axutil_env_t * env,
     char *host,
     int port)
 {
-    AXIS2_ENV_CHECK (env->error, NULL);
     axutil_stomp_t *stomp;
-    stomp = AXIS2_MALLOC (env->allocator, sizeof (axutil_stomp_t));
-    stomp->socket = axutil_network_handler_open_socket (env, host, port);
+
+    AXIS2_ENV_CHECK(env->error, NULL);
+    stomp = AXIS2_MALLOC(env->allocator, sizeof(axutil_stomp_t));
+    stomp->socket = axutil_network_handler_open_socket(env, host, port);
 
     if (stomp->socket == -1)
     {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI, 
-                     "stomp client is unable to open socket for %s host and %d port", 
-                     host, port);
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI,
+                        "[stomp]stomp client is unable to open socket for",
+                        "%s host and %d port",
+                        host, port);
         return stomp;
     }
 
-    stomp->stream = axutil_stream_create_socket (env, stomp->socket);
+    stomp->stream = axutil_stream_create_socket(env, stomp->socket);
 
     if (!stomp->stream)
     {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI, 
-                         "stomp client is unable to create stream for",
-                         "socket with %s host and %d port", 
-                         host, port);
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI,
+                        "[stomp]stomp client is unable to create stream for",
+                        "socket with %s host and %d port", host, port);
 
         return stomp;
     }
 
-    stomp->frame = axutil_stomp_frame_create (env);
+    stomp->frame = axutil_stomp_frame_create(env);
 
     if (!stomp->frame)
     {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI, 
-                         "stomp client is unable to create frame for",
-                         "socket with %s host and %d port", 
-                         host, port);
+        AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI,
+                        "[stomp]stomp client is unable to create frame for",
+                        "socket with %s host and %d port", host, port);
         return stomp;
     }
 
     return stomp;
 }
 
-
 axis2_status_t
-axutil_stomp_set_command (axutil_stomp_t *stomp,
-    const axutil_env_t *env,
-    axis2_char_t *command)
+axutil_stomp_set_command(
+    axutil_stomp_t * stomp,
+    const axutil_env_t * env,
+    axis2_char_t * command)
 {
-    AXIS2_PARAM_CHECK (env->error, stomp, AXIS2_FAILURE);
-    axutil_stomp_frame_set_command (stomp->frame, env, command);
+    AXIS2_PARAM_CHECK(env->error, stomp, AXIS2_FAILURE);
+    axutil_stomp_frame_set_command(stomp->frame, env, command);
     return AXIS2_SUCCESS;
 }
 
-
 axis2_status_t
-axutil_stomp_set_body (axutil_stomp_t *stomp,
-    const axutil_env_t *env,
-    axis2_char_t *body)
+axutil_stomp_set_body(
+    axutil_stomp_t * stomp,
+    const axutil_env_t * env,
+    axis2_char_t * body)
 {
-    AXIS2_PARAM_CHECK (env->error, stomp, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, stomp->frame, AXIS2_FAILURE);
-    axutil_stomp_frame_set_body (stomp->frame, env, body);
+    AXIS2_PARAM_CHECK(env->error, stomp, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, stomp->frame, AXIS2_FAILURE);
+    axutil_stomp_frame_set_body(stomp->frame, env, body);
     return AXIS2_SUCCESS;
 }
 
-
 axis2_status_t
-axutil_stomp_set_header (axutil_stomp_t *stomp,
-    const axutil_env_t *env,
-    axis2_char_t *header,
-    axis2_char_t *value)
+axutil_stomp_set_header(
+    axutil_stomp_t * stomp,
+    const axutil_env_t * env,
+    axis2_char_t * header,
+    axis2_char_t * value)
 {
-    AXIS2_PARAM_CHECK (env->error, stomp, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, stomp->frame, AXIS2_FAILURE);
-    axutil_stomp_frame_set_header (stomp->frame, env, header, value);
+    AXIS2_PARAM_CHECK(env->error, stomp, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, stomp->frame, AXIS2_FAILURE);
+    axutil_stomp_frame_set_header(stomp->frame, env, header, value);
     return AXIS2_SUCCESS;
 }
 
-
-int 
-axutil_stomp_write (axutil_stomp_t *stomp,
-    const axutil_env_t *env)
+int
+axutil_stomp_write(
+    axutil_stomp_t * stomp,
+    const axutil_env_t * env)
 {
-    AXIS2_PARAM_CHECK (env->error, stomp, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, stomp->frame, AXIS2_FAILURE);
-    AXIS2_PARAM_CHECK (env->error, stomp->stream, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, stomp, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, stomp->frame, AXIS2_FAILURE);
+    AXIS2_PARAM_CHECK(env->error, stomp->stream, AXIS2_FAILURE);
 
-    return axutil_stomp_frame_write (stomp->frame, env, stomp->stream);
+    return axutil_stomp_frame_write(stomp->frame, env, stomp->stream);
 }
-
 
 axutil_stomp_frame_t *
-axutil_stomp_read (axutil_stomp_t *stomp,
-    const axutil_env_t *env)
+axutil_stomp_read(
+    axutil_stomp_t * stomp,
+    const axutil_env_t * env)
 {
     axutil_stomp_frame_t *frame = NULL;
-    frame = axutil_stomp_frame_read (env, stomp->stream);
+    frame = axutil_stomp_frame_read(env, stomp->stream);
     if (frame)
     {
 /*         axutil_stomp_frame_free (stomp->frame, env); */
@@ -109,10 +109,3 @@ axutil_stomp_read (axutil_stomp_t *stomp,
     }
     return stomp->frame;
 }
-
-
-
-
-
-
-
