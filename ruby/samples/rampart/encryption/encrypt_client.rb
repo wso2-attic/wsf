62039b88497e2282212c132b4f4047510a437577
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-11-07 09:06:14 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@9523 a5903396-d722-0410-b921-86c7d4935375

---
+++ ruby/samples/rampart/encryption/encrypt_client.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+# Copyright 2005,2006,2007 WSO2, Inc. http://wso2.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'wsf'
+require 'rexml/document'
+
+include REXML
+
+req_payload = <<XML
+<ns1:echo xmlns:ns1="http://php.axis2.org/samples"><text>Hello World!</text></ns1:echo>
+XML
+
+def load_policy_from_file(filename)
+   #returns REXML::Element
+   input = File.new(filename)
+   doc = Document.new(input)
+   root_element = doc.root
+   return root_element
+end
+
+begin
+   rec_cert = WSUtil::ws_get_cert_from_file("../keys/bob_cert.cert")
+   pvt_key = WSUtil::ws_get_key_from_file("../keys/alice_key.pem")
+  
+ 
+   message_properties = {"to" => "http://localhost:9090/axis2/services/sec_echo/echoString",
+                         "action" =>  "http://php.axis2.org/samples/echoString"}
+   payload = WSMessage.new(req_payload, 
+                           nil, 
+                           message_properties)
+
+   policy_content = {"encrypt"=> true,
+                     "algorithm_suite" => "Basic256Rsa15",
+                     "security_token_reference" => "IssuerSerial"}
+
+   policy = WSPolicy.new({"security" => policy_content})
+
+   security_options = {"private_key" => pvt_key,
+                       "receiver_certificate" => rec_cert}
+ 
+   security_token = WSSecurityToken.new(security_options)
+
+   options = {"use_wsa" => true,
+              "policy" => policy,
+              "security_token" => security_token}
+   
+   client = WSClient.new(options, "/home/janapriya/deploy/wsfc/", "/home/janapriya/dev/wsf_ext/ruby/samples/rampart/sec_echo.log")
+
+   res_message = client.request(payload)
+
+   if not res_message.nil? then
+      puts "Received OM: "<< "\n" << res_message.payload_to_s << "\n\n"
+      puts "Client invocation SUCCESSFUL !!!"
+   else
+      puts "Client invocation FAILED !!!"
+   end
+
+rescue WSFault => wsfault
+   puts "Client invocation FAILED !!!\n"
+   puts "WSFault : "
+   puts wsfault.xml
+   puts "----------"
+   puts wsfault.code
+   puts "----------"
+   puts wsfault.reason
+   puts "----------"
+   puts wsfault.role
+   puts "----------"
+   puts wsfault.detail
+   puts "----------"
+
+rescue => exception
+   puts "Client invocation FAILED !!!\n"
+   puts "Exception : " << exception
+end
