487a337016a2d648ae8f8b7ded2626f999a1c961
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-11-19 06:34:58 +0000

    sample added for classes support
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@9878 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/echo_service1.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+class Bar
+{
+    private $value = "";
+
+    function Bar($str){
+        $this->value = $str;
+    }
+    function echoValue($inMessage){
+
+$reqPayloadString = <<<XML
+<ns1:echoString xmlns:ns1="http://wso2.org/projects/wsf/php">
+     <value>$this->value</value></ns1:echoString>
+XML;
+        return new WSMessage($reqPayloadString);
+    }
+}
+
+
+$ops = array("echoString" => "echoValue");
+
+$svr = new WSService(array("classes"=>array("Bar"=>array("operations"=>$ops,"args"=>array("Hello")))));
+        
+$svr->reply();
+
+?>
