9a76b0dcbda4dd8f18f3c63203aaa910e84c1b08
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-11-20 11:41:52 +0000

    Changed the version number
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@9961 a5903396-d722-0410-b921-86c7d4935375

--- c/dist.sh
+++ c/dist.sh
@@ -44,7 +44,7 @@ cd ..
 
 cp /tmp/dist.sh /tmp/dist_hook.sh .
 
-rm -rf wso2-wsf-c-src-1.1.0.tar.gz
-tar -pczf wso2-wsf-c-src-1.1.0.tar.gz wso2-wsf-c-src-1.1.0
-zip -r wso2-wsf-c-src-1.1.0.zip wso2-wsf-c-src-1.1.0
-rm -rf wso2-wsf-c-src-1.1.0
+rm -rf wso2-wsf-c-src-1.2.0.tar.gz
+tar -pczf wso2-wsf-c-src-1.2.0.tar.gz wso2-wsf-c-src-1.2.0
+zip -r wso2-wsf-c-src-1.2.0.zip wso2-wsf-c-src-1.2.0
+rm -rf wso2-wsf-c-src-1.2.0
