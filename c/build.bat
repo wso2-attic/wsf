c5e258eab48b73cf7ab82b5d06e46207c5b83f9f
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-11-20 09:04:34 +0000

    build.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@9942 a5903396-d722-0410-b921-86c7d4935375

--- c/build.bat
+++ c/build.bat
@@ -4,7 +4,7 @@
 rem Build Apache Axis2/C
 
 @cd axis2c\build
-@if exist axis2c-bin-1.1.0-win32 rmdir /s /q axis2c-bin-1.1.0-win32
+@if exist axis2c-bin-1.2.0-win32 rmdir /s /q axis2c-bin-1.2.0-win32
 @cd win32
 @nmake dist AUTOCONF=..\..\..\configure.in
 @if not %ERRORLEVEL% EQU 0 goto end
@@ -12,7 +12,7 @@ rem Build Apache Axis2/C
 rem Build Flicker sample
 @cd ..\..\..\examples\flicker
 @nmake -f flicker.mk
-@copy /Y flicker.exe  ..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples
+@copy /Y flicker.exe  ..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples
 
 rem Build Trader sample
 @cd ..\trader
@@ -20,19 +20,19 @@ rem Build Trader sample
 
 rem Build Apache Rampart/C
 @cd ..\..\rampartc\build\win32
-@nmake dist AUTOCONF=..\..\..\configure.in AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.1.0-win32
+@nmake dist AUTOCONF=..\..\..\configure.in AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.2.0-win32
 
 @if not %ERRORLEVEL% EQU 0 goto rampart_error
 
 @cd ..
-@xcopy /E /Q /I /Y rampartc-1.0.0\modules\rampart .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\modules\rampart
-@copy /Y rampartc-1.0.0\include\*.h .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\include\
-@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y rampartc-1.0.0\bin\samples\services\sec_echo\server_axis2.xml .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\axis2.xml
-@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\rampart
-@xcopy /E /I /Q /Y .\..\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\rampart
-@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples\services\sec_echo .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\sec_echo
+@xcopy /E /Q /I /Y rampartc-1.0.0\modules\rampart .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\modules\rampart
+@copy /Y rampartc-1.0.0\include\*.h .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\include\
+@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.lib .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@copy /Y rampartc-1.0.0\modules\rampart\mod_rampart.dll .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@copy /Y rampartc-1.0.0\bin\samples\services\sec_echo\server_axis2.xml .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\axis2.xml
+@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\rampart
+@xcopy /E /I /Q /Y .\..\samples .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\samples\rampart
+@xcopy /E /I /Q /Y rampartc-1.0.0\bin\samples\services\sec_echo .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\services\sec_echo
 @goto build_sandesha2
 
 :rampart_error
@@ -41,24 +41,24 @@ rem Build Apache Rampart/C
 rem Build Apache Sandesha2/C
 :build_sandesha2
 @cd ..\..\sandesha2c\build\win32
-@nmake dist AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.1.0-win32 AUTOCONF=..\..\..\configure.in
+@nmake dist AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.2.0-win32 AUTOCONF=..\..\..\configure.in
 
 @if not %ERRORLEVEL% EQU 0 goto sandesha2_error
 
 @cd ..
-@xcopy /E /Q /I /Y sandesha2\modules\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\modules\sandesha2
-@xcopy /E /Q /I /Y sandesha2\modules\sandesha2\sandesha2.lib  .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@xcopy /E /Q /I /Y sandesha2\modules\sandesha2\sandesha2.dll  .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@mkdir .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
-@copy /Y sandesha2\bin\samples\sandesha2\*.exe .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
-@xcopy /E /Q /I /Y sandesha2\services\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\RMSampleService
-@xcopy /E /Q /I /Y sandesha2\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\sandesha2
-@copy /Y sandesha2\include\*.h  .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\include\
-@xcopy /E /Q /I /Y  sandesha2\bin\samples\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\sandesha2
-@mkdir .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
-@copy /Y ..\config\*.txt .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
-@copy /Y ..\config\*.bat .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
-@copy /Y ..\config\*.sh .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\sandesha2
+@xcopy /E /Q /I /Y sandesha2\modules\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\modules\sandesha2
+@xcopy /E /Q /I /Y sandesha2\modules\sandesha2\sandesha2.lib  .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@xcopy /E /Q /I /Y sandesha2\modules\sandesha2\sandesha2.dll  .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@mkdir .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\sandesha2
+@copy /Y sandesha2\bin\samples\sandesha2\*.exe .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\sandesha2
+@xcopy /E /Q /I /Y sandesha2\services\RMSampleService .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\services\RMSampleService
+@xcopy /E /Q /I /Y sandesha2\samples .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\samples\sandesha2
+@copy /Y sandesha2\include\*.h  .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\include\
+@xcopy /E /Q /I /Y  sandesha2\bin\samples\sandesha2 .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\sandesha2
+@mkdir .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\sandesha2
+@copy /Y ..\config\*.txt .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\sandesha2
+@copy /Y ..\config\*.bat .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\sandesha2
+@copy /Y ..\config\*.sh .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\sandesha2
 @goto build_neethi_util
 
 :sandesha2_error
@@ -68,30 +68,30 @@ rem Build Neethi util
 :build_neethi_util
 @cd ..\..\
 @cd axis2c\neethi\src\util
-@nmake -f neethi_util.mk AXIS2_BIN_DIR=..\..\..\build\axis2c-bin-1.1.0-win32 AUTOCONF=..\..\..\..\configure.in
+@nmake -f neethi_util.mk AXIS2_BIN_DIR=..\..\..\build\axis2c-bin-1.2.0-win32 AUTOCONF=..\..\..\..\configure.in
 @cd ..\..\..\..\
 
 rem Build wsclient
 @cd wsclient
-@nmake -f wsclient.mk AXIS2_BIN_DIR=..\axis2c\build\axis2c-bin-1.1.0-win32 AUTOCONF=..\configure.in
+@nmake -f wsclient.mk AXIS2_BIN_DIR=..\axis2c\build\axis2c-bin-1.2.0-win32 AUTOCONF=..\configure.in
 @cd ..
 
 rem Build Apache Savan/C
 @cd savanc\build\win32
-@nmake dist AUTOCONF=..\..\..\configure.in AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.1.0-win32
+@nmake dist AUTOCONF=..\..\..\configure.in AXIS2_BIN_DIR=.\..\..\..\axis2c\build\axis2c-bin-1.2.0-win32
 
 @if not %ERRORLEVEL% EQU 0 goto savan_error
 
 @cd ..
-@xcopy /E /Q /I /Y savanc\modules\savan .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\modules\savan
-@xcopy /E /Q /I /Y savanc\services\publisher .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\publisher
-@xcopy /E /Q /I /Y savanc\services\listener .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\listener
-@xcopy /E /Q /I /Y savanc\services\subscription .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\services\subscription
-@xcopy /E /Q /I /Y savanc\samples .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\samples\savan
-@copy /Y savanc\modules\savan\mod_savan.dll .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@copy /Y savanc\modules\savan\mod_savan.lib .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\lib\
-@mkdir .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\savan
-@copy /Y savanc\bin\samples\savan\*.exe .\..\..\axis2c\build\axis2c-bin-1.1.0-win32\bin\samples\savan
+@xcopy /E /Q /I /Y savanc\modules\savan .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\modules\savan
+@xcopy /E /Q /I /Y savanc\services\publisher .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\services\publisher
+@xcopy /E /Q /I /Y savanc\services\listener .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\services\listener
+@xcopy /E /Q /I /Y savanc\services\subscription .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\services\subscription
+@xcopy /E /Q /I /Y savanc\samples .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\samples\savan
+@copy /Y savanc\modules\savan\mod_savan.dll .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@copy /Y savanc\modules\savan\mod_savan.lib .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\lib\
+@mkdir .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\savan
+@copy /Y savanc\bin\samples\savan\*.exe .\..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples\savan
 @goto pack_wsfc
 
 :savan_error
@@ -99,15 +99,15 @@ rem Build Apache Savan/C
 
 :pack_wsfc
 @cd ..\..
-@if not exist wso2-wsf-c-bin-1.1.0 mkdir wso2-wsf-c-bin-1.1.0
-@xcopy /E /Q /I /Y axis2c\build\axis2c-bin-1.1.0-win32 wso2-wsf-c-bin-1.1.0
-@copy /Y config\axis2.xml wso2-wsf-c-bin-1.1.0\axis2.xml
-@copy /Y INSTALL wso2-wsf-c-bin-1.1.0
-@copy /Y README wso2-wsf-c-bin-1.1.0
-@copy /Y NEWS wso2-wsf-c-bin-1.1.0
-@copy /Y COPYING wso2-wsf-c-bin-1.1.0
-@copy /Y AUTHORS wso2-wsf-c-bin-1.1.0
-@copy /Y NOTICE wso2-wsf-c-bin-1.1.0
-@xcopy /E /I /Q /Y docs  wso2-wsf-c-bin-1.1.0\docs
+@if not exist wso2-wsf-c-bin-1.2.0 mkdir wso2-wsf-c-bin-1.2.0
+@xcopy /E /Q /I /Y axis2c\build\axis2c-bin-1.2.0-win32 wso2-wsf-c-bin-1.2.0
+@copy /Y config\axis2.xml wso2-wsf-c-bin-1.2.0\axis2.xml
+@copy /Y INSTALL wso2-wsf-c-bin-1.2.0
+@copy /Y README wso2-wsf-c-bin-1.2.0
+@copy /Y NEWS wso2-wsf-c-bin-1.2.0
+@copy /Y COPYING wso2-wsf-c-bin-1.2.0
+@copy /Y AUTHORS wso2-wsf-c-bin-1.2.0
+@copy /Y NOTICE wso2-wsf-c-bin-1.2.0
+@xcopy /E /I /Q /Y docs  wso2-wsf-c-bin-1.2.0\docs
 
 :end
