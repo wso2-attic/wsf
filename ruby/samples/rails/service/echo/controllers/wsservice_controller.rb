4438bd6c85bceffd9e776cd243c17b963970ac73
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2007-11-21 12:24:02 +0000

    Adding the sample wsservice_controller
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10030 a5903396-d722-0410-b921-86c7d4935375

--- ruby/samples/rails/service/echo/controllers/wsservice_controller.rb
+++ ruby/samples/rails/service/echo/controllers/wsservice_controller.rb
@@ -1,14 +1,25 @@
-# The function which defines the logic of the service operation
+#the global function to expose 
 def echoFunction(in_msg)
     return in_msg
 end
 
+#the class to expose
+class WSFApi
+    def echoStringFunc in_msg
+        return in_msg
+    end
+
+    def echoIntFunc in_msg
+        return in_msg
+    end
+end
+
 class WsserviceController < ApplicationController
 
+  
   def echo
       require "wsf"
-
-      # The map to operation name => function defines the buisness logic
+      #operation to ruby function map
       operations = {"echoString" => "echoFunction",
                     "echoInt" => "echoFunction"}
 
@@ -21,16 +32,31 @@ class WsserviceController < ApplicationController
 
   def echo_addr
       require "wsf"
-
-      # The map to service operation name => function defines the buisness logic
+      #operation to ruby function map
       operations = {"echoString" => "echoFunction",
                     "echoInt" => "echoFunction"}
-      
-      # The map to wsa action => service operation name
+
+      #action to operation map
       actions = {"http://ruby.axis2.org/samples/echoString" => "echoString",
                 "http://ruby.axis2.org/samples/echoInt" => "echoInt"}
 
       wss = WSO2::WSF::WSService.new({:operations => operations, :actions => actions});
+
+      res = wss.reply(request, response);
+
+      render :xml => res
+  end
+
+  def echo_classmap
+      require "wsf"
+      #operation to ruby function map
+      class_operation_map = {"echoString" => "echoStringFunc",
+                    "echoInt" => "echoIntFunc"}
+      #action to operation map
+      actions = {"http://ruby.axis2.org/samples/echoString" => "echoString",
+                "http://ruby.axis2.org/samples/echoInt" => "echoInt"}
+
+      wss = WSO2::WSF::WSService.new({:classes => [:WSFApi => class_operation_map], :actions => actions});
 
       res = wss.reply(request, response);
 
