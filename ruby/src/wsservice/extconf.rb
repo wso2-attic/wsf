1da838dddb26219a9463c48ce479eda4065897f7
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-11-21 04:54:05 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@9987 a5903396-d722-0410-b921-86c7d4935375

---
+++ ruby/src/wsservice/extconf.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'mkmf'
+require 'rbconfig'
+
+
+# Check the configurations
+WSFC_HOME = Config::CONFIG['WSFC_HOME']
+if(WSFC_HOME == nil)
+  puts "Error in retrieving the WSFC_HOME from configuration, Add the WSFC_HOME in rbconfig.rb\n"
+  exit(-1)
+end
+
+dir_config("wsservice", WSFC_HOME + '/include/axis2-1.1', WSFC_HOME + '/lib')
+
+have_library('axutil')
+have_library('axis2_minizip')
+have_library('axis2_parser')
+have_library('axis2_libxml2')
+have_library('axis2_axiom')
+have_library('axis2_engine')
+have_library('axis2_http_common')
+have_library('axis2_http_sender')
+have_library('axis2_http_receiver')
+have_library('axis2_engine')
+
+
+create_makefile("wsservice")
