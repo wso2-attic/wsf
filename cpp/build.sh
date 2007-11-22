d8b4374ca611aa242d361246675c5f850e0fb947
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-11-22 07:07:49 +0000

    Fixed build errors
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10063 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-cp build/wsf_c/rampartc/configure.ac build/wsf_c/rampartc/configure.ac
-cp build/wsf_c/rampartc/src/Makefile.am  build/wsf_c/rampartc/src/Makefile.am
-cp build/wsf_c/rampartc/src/core/Makefile.am  build/wsf_c/rampartc/src/core/Makefile.am
+cp build/wsf_c/rampartc/configure.ac wsf_c/rampartc/configure.ac
+cp build/wsf_c/rampartc/src/Makefile.am  wsf_c/rampartc/src/Makefile.am
+cp build/wsf_c/rampartc/src/core/Makefile.am  wsf_c/rampartc/src/core/Makefile.am
+cp build/wsf_c/axis2c/neethi/configure.ac wsf_c/axis2c/neethi/configure.ac
+cp build/wsf_c/axis2c/neethi/src/Makefile.am wsf_c/axis2c/neethi/src/Makefile.am
+
 
 sh autogen.sh
 
