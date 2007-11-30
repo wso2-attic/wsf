4f8cfb6eafeb858220acd8188258b9cd476db343
buddhika <buddhika@a5903396-d722-0410-b921-86c7d4935375>
2007-11-30 09:27:27 +0000

    Adding secure MTOM sample for signing
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10326 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/security/secure_mtom/encryption/mtom_security_service.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function getAttachment($inMessage) {
+
+	$cid2stringMap = $inMessage->attachments;
+	$cid2contentMap = $inMessage->cid2contentType;
+	$imageName;
+	$file_saved = 0;
+
+	foreach($cid2stringMap as $i=>$value){
+		$f = $cid2stringMap[$i];
+		$contentType = $cid2contentMap[$i];
+		if(strcmp($contentType,"image/jpeg") ==0){
+			$imageName = $i."."."jpg";
+			$file_saved = file_put_contents("/tmp/".$imageName, $f);
+		}
+	}
+
+	if($file_saved){
+		$resPayload = <<<XML
+<ns1:response xmlns:ns1="http://php.axis2.org/samples/mtom">Image Saved</ns1:response>
+XML;
+	}
+	else{
+		$resPayload = <<<XML
+<ns1:response xmlns:ns1="http://php.axis2.org/samples/mtom">Image Saving Failed</ns1:response>
+XML;
+	}
+
+	$returnMessage = new WSMessage($resPayload);
+
+	return $returnMessage;
+}
+$pub_key = ws_get_cert_from_file("../../keys/alice_cert.cert");
+
+$pub_key = ws_get_cert_from_file("../../keys/alice_cert.cert");
+$pvt_key = ws_get_key_from_file("../../keys/bob_key.pem");
+
+$policy_xml = file_get_contents("policySignOnlyMtom.xml");
+$policy = new WSPolicy($policy_xml);
+
+$sec_token = new WSSecurityToken(array("privateKey" => $pvt_key,
+                                       "receiverCertificate" =>$pub_key));
+
+$actions = array("http://wso2.org/upload" => "upload");
+$operations = array("upload" => "getAttachment");
+
+$server = new WSService(array(
+                "operations" => $operations,
+				"actions" => $actions, 
+                "requestXOP"=>TRUE,
+				"policy" => $policy,
+				"securityToken" => $sec_token));
+
+$server->reply();
+
+?>
