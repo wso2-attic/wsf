f3ccf38186f2206637a25b040a750d469b38cdfc
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-11-30 06:27:06 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10311 a5903396-d722-0410-b921-86c7d4935375

--- ruby/samples/rampart/service/rampart_controller.rb
+++ ruby/samples/rampart/service/rampart_controller.rb
@@ -73,6 +73,28 @@ class RampartController < ApplicationController
 		end
   	end
 
+    def timestamp_file
+        begin
+            operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
+            actions = {"http://php.axis2.org/samples/echoString" => "echoString"}
+
+            policy_content = load_policy_from_file("./app/controllers/policies/policy_timestamp.xml")
+
+            policy = WSO2::WSF::WSPolicy.new(policy_content)
+
+            sec_token = WSO2::WSF::WSSecurityToken.new({"ttl" => 100})
+
+            wss = WSO2::WSF::WSService.new({"operations" => operations,
+                                          "actions" => actions,
+                                          "policy" => policy,
+                                          "securityToken" => sec_token})
+
+            res = wss.reply(request, response);
+
+            render :xml => res
+        end
+    end
+
 	def usernametoken
         begin
             operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
@@ -95,6 +117,30 @@ class RampartController < ApplicationController
 			render :xml => res
 		end
 	end
+
+    def usernametoken_file
+        begin
+            operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
+            actions = {"http://php.axis2.org/samples/echoString" => "echoString"}
+
+            policy_content = load_policy_from_file("./app/controllers/policies/policy_usernametoken.xml")
+
+            policy = WSO2::WSF::WSPolicy.new(policy_content)
+
+            sec_token = WSO2::WSF::WSSecurityToken.new({"user" => "Raigama",
+                                                        "password" => "RaigamaPW",
+                                                        "password_type" => "Digest"})
+
+            wss = WSO2::WSF::WSService.new({"operations" => operations,
+                                          "actions" => actions,
+                                          "policy" => policy,
+                                          "securityToken" => sec_token})
+
+            res = wss.reply(request, response);
+
+            render :xml => res
+        end
+    end
 
 	def signing
 		begin
@@ -124,6 +170,33 @@ class RampartController < ApplicationController
 		end
 	end
 
+    def signing_file
+        begin
+            operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
+            actions = {"http://php.axis2.org/samples/echoString" => "echoString"}
+
+            cert = WSO2::Util::WSUtil::ws_get_cert_from_file("./app/controllers/keys/bob_cert.cert");
+            pvt_key = WSO2::Util::WSUtil::ws_get_key_from_file("./app/controllers/keys/bob_key.pem");
+
+            policy_content = load_policy_from_file("./app/controllers/policies/policy_signing.xml")
+
+            policy = WSO2::WSF::WSPolicy.new(policy_content)
+
+            sec_token = WSO2::WSF::WSSecurityToken.new({"private_key" => pvt_key,
+            policy = WSO2::WSF::WSPolicy.new({"security" => policy_content})
+                                                        "certificate" => cert})
+
+            wss = WSO2::WSF::WSService.new({"operations" => operations,
+                                          "actions" => actions,
+                                          "policy" => policy,
+                                          "securityToken" => sec_token})
+
+            res = wss.reply(request, response);
+
+            render :xml => res
+        end
+    end
+
 	def encryption
 		begin
             operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
@@ -151,6 +224,33 @@ class RampartController < ApplicationController
 			render :xml => res
 		end
 	end
+
+    def encryption_file
+        begin
+            operations = {"echoString" => "echoFunction", "echoInt" => "echoFunction"}
+            actions = {"http://php.axis2.org/samples/echoString" => "echoString"}
+
+            pub_key = WSO2::Util::WSUtil::ws_get_cert_from_file("./app/controllers/keys/alice_cert.cert");
+            pvt_key = WSO2::Util::WSUtil::ws_get_key_from_file("./app/controllers/keys/bob_key.pem");
+
+            policy_content = load_policy_from_file("./app/controllers/policies/policy_encryption.xml")
+
+            policy = WSO2::WSF::WSPolicy.new(policy_content)
+
+            sec_token = WSO2::WSF::WSSecurityToken.new({"private_key" => pvt_key,
+                                                        "receiver_certificate" =>pub_key})
+
+            wss = WSO2::WSF::WSService.new({"operations" => operations,
+                                          "actions" => actions,
+                                          "policy" => policy,
+                                          "securityToken" => sec_token})
+
+            res = wss.reply(request, response);
+
+            render :xml => res
+        end
+    end
+
 
 	def complete
 		begin
