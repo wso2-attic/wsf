b28b5f75c6c93d72b24deb2c9034231f2114ad9e
danushka <danushka@a5903396-d722-0410-b921-86c7d4935375>
2007-12-06 06:10:09 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10591 a5903396-d722-0410-b921-86c7d4935375

---
+++ ruby/samples/provider/mtom/mtom_download_service_controller.rb
@@ -0,0 +1,30 @@
+#the global function to expose 
+def downloadFunction(in_msg)
+  res_payload_string = "<ns1:download xmlns:ns1='http://php.axis2.org/samples/mtom'><ns1:fileName>test.jpg</ns1:fileName><ns1:image xmlmime:contentType='image/jpeg' xmlns:xmlmime='http://www.w3.org/2004/06/xmlmime'><xop:Include xmlns:xop='http://www.w3.org/2004/08/xop/include' href='cid:myid1'></xop:Include></ns1:image></ns1:download>"
+  
+  content = IO.read("resources/axis2.jpg")
+
+  msg = WSO2::WSF::WSMessage.new(res_payload_string,
+							     nil,
+								 {"attachments" => {"myid1" => content}})
+ 
+  return msg
+end
+
+class MtomDownloadServiceController < ApplicationController
+
+  require "wsf"
+  
+  def mtom_download
+      #operation to ruby function map
+      operations = {"download" => "downloadFunction"}
+
+      wss = WSO2::WSF::WSService.new({:operations => operations,
+	  								  :use_mtom => true});
+
+      res = wss.reply(request, response);
+
+      render :text => res
+  end
+
+end
---
+++ ruby/samples/provider/mtom/mtom_upload_service_controller.rb
@@ -0,0 +1,45 @@
+#the global function to expose 
+def uploadFunction(in_msg)
+	attachments = in_msg.property "attachments"
+	cid_2_content_type = in_msg.property "cid_2_content_type"
+
+	if attachments and cid_2_content_type then
+	
+		cid_2_content_type.each_pair {|cid, content_type|
+			content = attachments[cid]
+			if content then
+			
+				file_name = cid
+				file_name = file_name + ".jpg" if content_type == "image/jpeg"
+
+				f = File.new(file_name, "w")
+				f.write(content)
+				f.close
+
+			end
+		}
+
+	end
+
+	res_payload_string = "<ns1:response xmlns:ns1='http://ws.apache.org/axis2/c/samples'>Image Saved</ns1:response>"
+
+    return res_payload_string
+end
+
+class MtomUploadServiceController < ApplicationController
+
+  require "wsf"
+  
+  def mtom_upload
+      #operation to ruby function map
+      operations = {"upload" => "uploadFunction"}
+
+      wss = WSO2::WSF::WSService.new({:operations => operations,
+	  								  :request_xop => true});
+
+      res = wss.reply(request, response);
+
+      render :text => res
+  end
+
+end
