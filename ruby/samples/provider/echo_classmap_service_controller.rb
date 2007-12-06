0d428752a3c2f3e284a4fd1942cc853e2a6576fd
danushka <danushka@a5903396-d722-0410-b921-86c7d4935375>
2007-12-06 12:31:06 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10639 a5903396-d722-0410-b921-86c7d4935375

--- ruby/samples/provider/echo_classmap_service_controller.rb
+++ ruby/samples/provider/echo_classmap_service_controller.rb
@@ -13,14 +13,14 @@ class EchoClassmapServiceController < ApplicationController
 
   require "wsf"
   
-  def echo_classmap
+  def echo
 	  #operation to ruby function map
       class_operation_map = {"echoString" => "echoStringFunc",
                     		 "echoInt" => "echoIntFunc"}
       
 	  #action to operation map
-      actions = {"wso2:wsf:ruby:echo_string" => "echoString",
-                 "wso2:wsf:ruby:echo_int" => "echoInt"}
+      actions = {"http://ruby.wsf.wso2/samples/echoString" => "echoString",
+                 "http://ruby.wsf.wso2/samples/echoInt" => "echoInt"}
 
       wss = WSO2::WSF::WSService.new({:classes => [:WSFApi => class_operation_map],
 									  :actions => actions});
