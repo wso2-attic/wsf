5bc6c2e18bdd30d7542a7ac5784d184e3a601cc5
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-12-07 14:55:14 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10708 a5903396-d722-0410-b921-86c7d4935375

--- ruby/build/build.bat
+++ ruby/build/build.bat
@@ -1,11 +1,15 @@
-echo Setting Visual Studio environment variables...
-vcvars32.bat
+@echo off
+echo.
 echo Building WSF Client...
+echo.
 ruby extconf-win32.rb
 nmake 
 nmake install
+echo. 
 echo Building WSF Service...
+echo.
 cd wsservice
 ruby extconf-win32.rb
 nmake
 nmake install
+cd ..
