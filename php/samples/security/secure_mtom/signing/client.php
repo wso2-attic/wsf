c4cc5f84c40389d7d2574a5fdc2bb3c5fdb4d62b
buddhika <buddhika@a5903396-d722-0410-b921-86c7d4935375>
2007-12-10 09:59:11 +0000

    modified MTOM-signing client
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@10813 a5903396-d722-0410-b921-86c7d4935375

--- php/samples/security/secure_mtom/signing/client.php
+++ php/samples/security/secure_mtom/signing/client.php
@@ -28,7 +28,7 @@ try {
 	$f = file_get_contents("../../../resources/axis2.jpg");
 
 	$reqMessage = new WSMessage($reqPayloadString,
-	array("to" => "http://localhost/samples/security/secure_mtom/encryption/mtom_security_service.php",
+	array("to" => "http://localhost/samples/security/secure_mtom/signing/service.php",
               "action" => "http://wso2.org/upload",
               "attachments" => array("myid1" => $f)));
 
