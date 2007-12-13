a236a3d33ae1b0d8a00fd6f48f7f47b9c1ebfebe
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2007-12-13 09:22:33 +0000

    Getting meta info from wsf_meta, a global variable.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11065 a5903396-d722-0410-b921-86c7d4935375

--- ruby/src/wsdl/ws_wsdl_operations.rb
+++ ruby/src/wsdl/ws_wsdl_operations.rb
@@ -7,48 +7,49 @@ class WS_WSDL_Operations
   WS_OPERATION_RET_TAG = 'return';
   WS_OPERATION_NAME_TAG = 'name';
 
-  mappingTable = [ "string"         =>  "string",
-                   "boolean"        =>  "boolean",
-                   "double"         =>  "float",
-                   "float"          =>  "float",
-                   "int"            =>  "int",
-                   "integer"        =>  "int",
-                   "byte"           =>  "int",
-                   "decimal"        =>  "string",
-                   "base64Binary"   =>  "string",
-                   "hexBinary"      =>  "string",
-                   "anyType"        =>  "soap Var Object",
-                   "any"            =>  "soap Var Object",
-                   "QName"          =>  "string",
-                   "dateTime"       =>  "string",
-                   "date"           =>  "string",
-                   "time"           =>  "string",
-                   "unsignedLong"   =>  "int",
-                   "unsignedInt"    =>  "int",
-                   "unsignedShort"  =>  "int",
-                   "unsignedByte"   =>  "int",
-                   "positiveInteger"=>  "int",
-                   "negativeInteger"=>  "int",
-                   "nonNegativeInteger"=>"int",
-                   "gYearMonth"     =>  "string",
-                   "gMonthDate"     =>  "string",
-                   "gYear"          =>  "string",
-                   "gMonth"         =>  "string",
-                   "gDay"           =>  "string",
-                   "duration"       =>  "string",
-                   "Name"           =>  "string",
-                   "NCName"         =>  "string",
-                   "NMTOKEN"        =>  "string",
-                   "NOTATION"       =>  "string",
-                   "NMTOKENS"       =>  "string",
-                   "ENTITY"         =>  "string",
-                   "ENTITIES"       =>  "string",
-                   "IDREF"          =>  "string",
-                   "IDREFS"         =>  "string",
-                   "anyURI"         =>  "string",
-                   "language"       =>  "string",
-                   "normalizedString"=> "string",
-                   "token"          => "string" ]
+  mapping_table = { 
+    "string"         =>  "string",
+    "boolean"        =>  "boolean",
+    "double"         =>  "float",
+    "float"          =>  "float",
+    "int"            =>  "int",
+    "integer"        =>  "int",
+    "byte"           =>  "int",
+    "decimal"        =>  "string",
+    "base64Binary"   =>  "string",
+    "hexBinary"      =>  "string",
+    "anyType"        =>  "soap Var Object",
+    "any"            =>  "soap Var Object",
+    "QName"          =>  "string",
+    "dateTime"       =>  "string",
+    "date"           =>  "string",
+    "time"           =>  "string",
+    "unsignedLong"   =>  "int",
+    "unsignedInt"    =>  "int",
+    "unsignedShort"  =>  "int",
+    "unsignedByte"   =>  "int",
+    "positiveInteger"=>  "int",
+    "negativeInteger"=>  "int",
+    "nonNegativeInteger"=>"int",
+    "gYearMonth"     =>  "string",
+    "gMonthDate"     =>  "string",
+    "gYear"          =>  "string",
+    "gMonth"         =>  "string",
+    "gDay"           =>  "string",
+    "duration"       =>  "string",
+    "Name"           =>  "string",
+    "NCName"         =>  "string",
+    "NMTOKEN"        =>  "string",
+    "NOTATION"       =>  "string",
+    "NMTOKENS"       =>  "string",
+    "ENTITY"         =>  "string",
+    "ENTITIES"       =>  "string",
+    "IDREF"          =>  "string",
+    "IDREFS"         =>  "string",
+    "anyURI"         =>  "string",
+    "language"       =>  "string",
+    "normalizedString"=> "string",
+    "token"          => "string" }
 
   def initialize(operations)
     operations.each_pair do |i, value|
@@ -59,16 +60,29 @@ class WS_WSDL_Operations
   end
 
   def set_operations(operation_name)
-    @operations[@operation_name][WS_OPERATION_INPUT_TAG] = []
-    @operations[@operation_name][WS_OPERATION_OUTPUT_TAG] = []
+    @operations[operation_name][WS_OPERATION_INPUT_TAG] = []
+    @operations[operation_name][WS_OPERATION_OUTPUT_TAG] = []
 
-    # get doc comments here, figure out a way to get a la doc commenting thing
+    @xsd_types[operation_name]["In"] = []
 
-    # ideally this logic executes when there's no doc comments
-    # going ahead and executing them since we don't have them, yet.
+    $wsf_meta[operation_name.intern][:param_names].each do |param|
+      match = []
+      match[2] = param
+      match[1] = "anyType"
+      @xsd_types[operation_name]["In"][match[2]] = "anyType"
+             
+      @created_types[match[1]] = 2
+      @operations[operation_name][WS_OPERATION_INPUT_TAG] =
+        { WS_OPERATION_NAME_TAG => match[2], WS_OPERATION_TYPE_TAG => match[1] }
 
-    xsd_types[operation_name]["In"] = []
-    operation.
+    end
+
+    @xsd_types[operation_name]["Out"]["returnVal"] = "anyType"
+    
+    @created_types[match[1]] = 2
+    @operations[operation_name][WS_OPERATION_OUTPUT_TAG] =
+      { WS_OPERATION_NAME_TAG => WS_OPERATION_RET_TAG, WS_OPERATION_TYPE_TAG => match[1] }
+
   end
 
 end
