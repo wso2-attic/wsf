edbad4f44aa755071755ed989fce2591cfa06f75
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2007-12-15 19:47:40 +0000

    Changing WSFC_wrap.c to WSFC_wrapper.c
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11169 a5903396-d722-0410-b921-86c7d4935375

--- ruby/build/sync_src.rb
+++ ruby/build/sync_src.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Generate WSFC Wrapper
-system('swig -ruby -I../swig/ -outdir ./ -Wall WSFC.i') unless File.exists?('WSFC_wrap.c')
+system('swig -ruby -I../swig/ -o WSFC_wrapper.c -outdir ./ -Wall WSFC.i') unless File.exists?('WSFC_wrapper.c')
 
 # Create "lib" Directory
 Dir.mkdir('lib') unless (FileTest.exist?('lib') && FileTest.directory?('lib'))
