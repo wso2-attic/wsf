51107b659db1214b89dde539b230f1abdcf0a835
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2007-12-17 06:03:09 +0000

    Correting sync
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11196 a5903396-d722-0410-b921-86c7d4935375

--- ruby/sync_src.rb
+++ ruby/sync_src.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 # Generate WSFC Wrapper
-system('swig -ruby -I../swig/ -o WSFC_wrapper.c -outdir ./ -Wall WSFC.i') unless File.exists?('WSFC_wrapper.c')
+system('swig -ruby -Iswig/ -o WSFC_wrapper.c -outdir ./ -Wall WSFC.i') unless File.exists?('WSFC_wrapper.c')
