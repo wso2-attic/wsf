e984b29aec6dbf5a7eb79740bd8f2631e6115a6e
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-12-18 11:39:50 +0000

    Fixing jira issue 212
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11402 a5903396-d722-0410-b921-86c7d4935375

--- c/link_samples.sh
+++ c/link_samples.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-if [ -w /etc/opt ]
-then
-	if [ ! -d /etc/opt/wso2/wsf_c/services ]
-	then
-		mkdir /etc/opt/wso2/wsf_c/services
-	fi
-
-	if [ ! -d /etc/opt/wso2/wsf_c/services/ ]
-	then
-		ln -s $1/services /etc/opt/wso2/wsf_c/services
-	fi
-
-	if [ ! -d /etc/opt/wso2/wsf_c/modules ]
-	then
-		ln -s $1/modules /etc/opt/wso2/wsf_c/modules
-	fi
-else
-	exit 0
-fi
+mkdir -p /etc/opt/wso2/wsf_c/services
+mkdir -p /etc/opt/wso2/wsf_c/modules
+ln -f -s $1/services /etc/opt/wso2/wsf_c/services
+ln -f -s $1/modules /etc/opt/wso2/wsf_c/modules
 
 
 
