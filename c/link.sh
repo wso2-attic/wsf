73b483e68e27a8929a7c9ed3cb6abf42138bd8e4
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2007-12-18 11:39:31 +0000

    Fixing jira issue 212
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11401 a5903396-d722-0410-b921-86c7d4935375

--- c/link.sh
+++ c/link.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-if [ -w /etc/opt ]
-then
-	if [ ! -d /etc/opt/wso2 ]
-	then
-		mkdir /etc/opt/wso2
-	fi
-
-	if [ ! -d /etc/opt/wso2/wsf_c ]
-	then
-		mkdir /etc/opt/wso2/wsf_c
-	fi
-
-	if [ ! -f /etc/opt/wso2/wsf_c/axis2.xml ]
-	then
-		ln -s $1/axis2.xml /etc/opt/wso2/wsf_c/axis2.xml
-	fi
-else
-	exit 0
-fi
+mkdir -p mkdir /etc/opt/wso2/wsf_c
+ln -f -s $1/axis2.xml /etc/opt/wso2/wsf_c/axis2.xml
 
 
 
