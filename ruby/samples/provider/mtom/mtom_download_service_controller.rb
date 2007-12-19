28f58a47da8d0b031909c997a5aaea322bf4a456
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-12-19 12:32:53 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11570 a5903396-d722-0410-b921-86c7d4935375

--- ruby/samples/provider/mtom/mtom_download_service_controller.rb
+++ ruby/samples/provider/mtom/mtom_download_service_controller.rb
@@ -2,7 +2,10 @@
 def downloadFunction(in_msg)
   res_payload_string = "<ns1:download xmlns:ns1='http://php.axis2.org/samples/mtom'><ns1:fileName>test.jpg</ns1:fileName><ns1:image xmlmime:contentType='image/jpeg' xmlns:xmlmime='http://www.w3.org/2004/06/xmlmime'><xop:Include xmlns:xop='http://www.w3.org/2004/08/xop/include' href='cid:myid1'></xop:Include></ns1:image></ns1:download>"
   
-  content = IO.read("resources/axis2.jpg")
+  resource = File.new("resources/axis2.jpg", "rb")
+  
+  content = resource.read
+
 
   msg = WSO2::WSF::WSMessage.new(res_payload_string,
 							     nil,
