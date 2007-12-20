700385ec013dd65c40555b1ddd903edb67d6ba26
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2007-12-20 13:18:10 +0000

    with-mysql option removed
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11610 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -11,5 +11,5 @@ do
     sed  's/-Werror//' $i > configure.ac.back;
     mv configure.ac.back $i;
 done
-./autogen.sh && ./configure --with-wsf --enable-openssl --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/wsf_c/axis2c/include --enable-tests=no --enable-wsclient=no --with-mysql  --enable-savan=no --prefix=`php-config --extension-dir`/wsf_c && make -j 100 
+./autogen.sh && ./configure --with-wsf --enable-openssl --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/wsf_c/axis2c/include --enable-tests=no --enable-wsclient=no  --enable-savan=no --prefix=`php-config --extension-dir`/wsf_c && make -j 100 
 
