8fb9500176be9b9de2910db7b023d00b17a78fd1
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2007-12-21 07:33:18 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11638 a5903396-d722-0410-b921-86c7d4935375

--- ruby/build_dist.bat
+++ ruby/build_dist.bat
@@ -53,6 +53,8 @@ rem @if exist xcopy /Y /I /Q /E scripts wso2-wsf-ruby-bin-1.0.0-win32\scripts
 
 @if exist NEWS copy /Y NEWS wso2-wsf-ruby-bin-1.0.0-win32
 
+@if exist README.INSTALL_WIN32 copy /Y README.INSTALL_WIN32  wso2-wsf-ruby-bin-1.0.0-win3
+
 @if exist install.rb copy /Y install.rb wso2-wsf-ruby-bin-1.0.0-win32
 
 @if exist find_rbconfig.rb copy /Y find_rbconfig.rb wso2-wsf-ruby-bin-1.0.0-win32
