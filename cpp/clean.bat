21e92e4ec2054ee6862fc5727dfaf80fcdfa9d40
senaka <senaka@a5903396-d722-0410-b921-86c7d4935375>
2007-12-27 19:17:07 +0000

    Added clean.bat and configure.in files for WSF/C++ to provide MS Windows support.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11703 a5903396-d722-0410-b921-86c7d4935375

---
+++ cpp/clean.bat
@@ -0,0 +1,21 @@
+@if exist wsf_c\axis2c\build\axis2c-bin-1.2.0-win32 rmdir /S /Q wsf_c\axis2c\build\axis2c-bin-1.2.0-win32
+
+@if exist wsf_c\sandesha2c\build\sandesha2 rmdir /S /Q wsf_c\sandesha2c\build\sandesha2
+
+@if exist wsf_c\rampartc\build\rampartc-1.0 rmdir /S /Q wsf_c\rampartc\build\rampartc-1.0
+
+@if exist wsf_c\savanc\build\savanc rmdir /S /Q wsf_c\savanc\build\savanc
+
+@if exist wsf_c\wso2-wsf-c-bin-1.2.0-win32 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.2.0-win32
+
+@if exist wso2-wsf-cpp-bin-1.0.0-win32 rmdir /S /Q wso2-wsf-cpp-bin-1.0.0-win32
+
+@if exist wsf_c\axis2c\build\win32\int.msvc rmdir /S /Q wsf_c\axis2c\build\win32\int.msvc
+
+@if exist wsf_c\rampartc\build\win32\int.msvc rmdir /S /Q wsf_c\rampartc\build\win32\int.msvc
+
+@if exist wsf_c\sandesha2c\build\win32\int.msvc rmdir /S /Q wsf_c\sandesha2c\build\win32\int.msvc
+
+@if exist wsf_c\savanc\build\win32\int.msvc rmdir /S /Q wsf_c\savanc\build\win32\int.msvc
+
+@if exist wsf_c\axis2c\build\deploy rmdir /S /Q wsf_c\axis2c\build\deploy
