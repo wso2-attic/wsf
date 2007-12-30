fa6dedb53e668f2d74e03d9b5d4bcbd07030fe54
senaka <senaka@a5903396-d722-0410-b921-86c7d4935375>
2007-12-30 20:52:11 +0000

    Added secpolicy scenario deploy batch file and modified clean batch file.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@11710 a5903396-d722-0410-b921-86c7d4935375

--- cpp/clean.bat
+++ cpp/clean.bat
@@ -10,12 +10,6 @@
 
 @if exist wso2-wsf-cpp-bin-1.0.0-win32 rmdir /S /Q wso2-wsf-cpp-bin-1.0.0-win32
 
-@if exist wsf_c\axis2c\build\win32\int.msvc rmdir /S /Q wsf_c\axis2c\build\win32\int.msvc
-
-@if exist wsf_c\rampartc\build\win32\int.msvc rmdir /S /Q wsf_c\rampartc\build\win32\int.msvc
-
-@if exist wsf_c\sandesha2c\build\win32\int.msvc rmdir /S /Q wsf_c\sandesha2c\build\win32\int.msvc
-
-@if exist wsf_c\savanc\build\win32\int.msvc rmdir /S /Q wsf_c\savanc\build\win32\int.msvc
+@for /F "tokens=*" %%G in ('dir /B /AD /S *int.msvc*') do rmdir /S /Q %%G
 
 @if exist wsf_c\axis2c\build\deploy rmdir /S /Q wsf_c\axis2c\build\deploy
