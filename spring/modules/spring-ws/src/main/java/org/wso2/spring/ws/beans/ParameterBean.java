1f850a9a07c27bc5e787bd2fa2e1a45065a112a3
tharindu <tharindu@a5903396-d722-0410-b921-86c7d4935375>
2008-01-25 06:27:37 +0000

    license notes added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@12864 a5903396-d722-0410-b921-86c7d4935375

--- spring/modules/spring-ws/src/main/java/org/wso2/spring/ws/beans/ParameterBean.java
+++ spring/modules/spring-ws/src/main/java/org/wso2/spring/ws/beans/ParameterBean.java
@@ -1,13 +1,26 @@
+/*
+ * Copyright 2005-2008 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.spring.ws.beans;
 
 import org.apache.axis2.AxisFault;
-import org.apache.axis2.deployment.DeploymentErrorMsgs;
 import org.apache.axis2.deployment.DeploymentException;
-import org.apache.axis2.description.AxisDescription;
 import org.apache.axis2.description.AxisService;
 import org.apache.axis2.description.Parameter;
 import org.apache.axis2.engine.AxisConfiguration;
-import org.apache.axis2.i18n.Messages;
 
 // Replaces the Parameter property of the configuration .xml files
 
