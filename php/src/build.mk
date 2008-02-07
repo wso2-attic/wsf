fe9f03d000cf85a356b05b40c68c5fe7e080089b
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-02-07 05:51:58 +0000

    makefile updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@13364 a5903396-d722-0410-b921-86c7d4935375

--- php/src/build.mk
+++ php/src/build.mk
@@ -2,7 +2,7 @@ AUTOCONF = .\..\configure.in
 !include $(AUTOCONF)
 
 CFLAGS = /nologo /w /D "WIN32" /D "_WINDOWS" /D "PHP_WIN32" /D "COMPILE_DL_WSF" \
-	 /D "_USE_32BIT_TIME_T=1" /D "USE_SANDESHA2" /D "ZEND_WIN32" /D "ZEND_DEBUG=0" \
+	 /D "_USE_32BIT_TIME_T=1" /D "ZEND_WIN32" /D "ZEND_DEBUG=0" \
 	 /D "_MBCS" 
 
 LDFLAGS = /nologo /LIBPATH:.\..\wso2-wsf-php-bin-1.2.1-win32\wsf_c\lib /LIBPATH:$(PHP_BIN_DIR)\dev \
@@ -10,7 +10,7 @@ LDFLAGS = /nologo /LIBPATH:.\..\wso2-wsf-php-bin-1.2.1-win32\wsf_c\lib /LIBPATH:
 
 LIBS = axutil.lib axis2_engine.lib axis2_parser.lib \
        axiom.lib libxml2.lib mod_rampart.lib axis2_http_sender.lib \
-       neethi_util.lib neethi.lib sandesha2.lib libxml2.lib wsock32.lib
+       neethi_util.lib neethi.lib sandesha2.lib sandesha2_client.lib libxml2.lib wsock32.lib
 
 INCLUDE_PATH = /I$(PHP_SRC_DIR) /I$(PHP_SRC_DIR)\main /I$(PHP_SRC_DIR)\regex \
 	       /I$(PHP_SRC_DIR)\ext /I$(PHP_SRC_DIR)\sapi /I$(PHP_SRC_DIR)\Zend \
