d243fb21a68b77d302bbab5246448e7327806117
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-02-07 06:09:18 +0000

    build_dev.bat bat file added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@13369 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/build_dev.bat
@@ -0,0 +1,63 @@
+@echo off
+
+rem @call clean.bat
+
+@copy configure.in wsf_c\configure.in
+@copy wsf_c_build.bat wsf_c\build.bat
+
+@cd wsf_c
+
+@call build.bat
+
+@cd ..
+
+@mkdir wso2-wsf-php-bin-1.2.1-win32
+
+@xcopy /E /Q /Y /I wsf_c\wso2-wsf-c-bin-1.2.0 wso2-wsf-php-bin-1.2.1-win32\wsf_c
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\services rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\services
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\docs rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\docs
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\modules\savan rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\modules\savan
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\samples rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\samples
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\ides rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\ides
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\samples rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\samples
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\tools rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\tools
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\docs rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\bin\docs
+
+@if exist wso2-wsf-php-bin-1.2.1-win32\wsf_c\modules\logging rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32\wsf_c\modules\logging
+
+@if exist scripts xcopy /Y /I /Q /E scripts wso2-wsf-php-bin-1.2.1-win32\scripts
+
+@if exist samples xcopy /Y /I /Q /E samples wso2-wsf-php-bin-1.2.1-win32\samples
+
+@if exist docs xcopy /Y /I /Q /E docs wso2-wsf-php-bin-1.2.1-win32\docs
+
+@if exist NOTICE copy /Y NOTICE wso2-wsf-php-bin-1.2.1-win32
+
+@if exist README.INSTALL_WIN32 copy /Y README.INSTALL_WIN32 wso2-wsf-php-bin-1.2.1-win32
+
+@if exist README.INSTALL copy /Y README.INSTALL wso2-wsf-php-bin-1.2.1-win32
+
+@if exist LICENSE copy /Y LICENSE wso2-wsf-php-bin-1.2.1-win32
+
+@if exist NEWS copy /Y NEWS wso2-wsf-php-bin-1.2.1-win32
+
+@if exist AUTHORS copy /Y AUTHORS wso2-wsf-php-bin-1.2.1-win32
+
+@if exist COPYING copy /Y COPYING wso2-wsf-php-bin-1.2.1-win32
+
+@if exist ChangeLog copy /Y ChangeLog wso2-wsf-php-bin-1.2.1-win32
+
+@cd src
+
+@nmake /nologo -f build.mk dist
+
+@cd ..
+
