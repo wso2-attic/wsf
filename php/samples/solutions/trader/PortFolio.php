63af43b84fb84a3bebfa519e13393a2498fa1448
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-02-11 06:18:54 +0000

    trader sample added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@13545 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/solutions/trader/PortFolio.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+$getResult = $_POST["getResult"];
+$symbol = $_POST["symbol"];
+$amount = $_POST["amount"];
+		    
+?>
+
+<html>
+<head>
+<title> Trader Service </title>
+</head>
+<body>
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<table cols="2" border="1" cellpadding="10" cellspacing="0" align="center" width="100%" bgcolor="#eeeeee">
+<tr>
+<td>
+<h4>Create portFolio</h4>
+
+<br/>
+
+<h4>Parameters</h4>
+Symbol:<input type="text" size="6" maxlength="15" name="symbol" value="" color="red">
+Amount in millions:<input type="text" size="6" maxlength="15" name="amount" value=""><br/>
+<br/>
+
+<input type="submit" value="Create" name="getResult">
+<input type="submit" value="Reset" name="reset">
+
+
+</td>
+</tr>
+</table>
+
+</form>
+<?php
+if (isset($_POST['getResult']))
+{
+      if (isset($_GET['symbol']) && isset($_GET['amount']))
+      {
+	
+	$symbol = $_GET['symbol'];
+	$amount = $_GET['amount'];
+	
+      }
+    
+     
+if (isset($symbol) &&  isset($amount))
+{
+  $fp;
+  if (stristr(PHP_OS, 'WIN')) {
+	$fp = @fopen('portfolio.xml','r+');
+ }else{
+ 	$fp = @fopen('/tmp/portfolio.xml','r+');
+ }
+ 
+ if(!$fp) {
+   
+
+ 
+  $items = array();
+  $items [] = array(
+  'Symbol' => $symbol,
+  'Amount' => $amount,
+  );
+
+  $doc = new DOMDocument("1.0");
+  $doc->formatOutput = true;
+  $r = $doc->createElement( "items" );
+  $doc->appendChild( $r );
+
+
+foreach( $items as $item )
+  {
+  $b = $doc->createElement( "item" );
+  
+  $Symbol = $doc->createElement( "Symbol" );
+  $Symbol->appendChild(
+  $doc->createTextNode( $item['Symbol'] )
+  );
+  $b->appendChild( $Symbol );
+  
+  $Amount = $doc->createElement( "Amount" );
+  $Amount->appendChild(
+  $doc->createTextNode( $item['Amount'] )
+  );
+  $b->appendChild( $Amount );
+  
+  $r->appendChild( $b );
+
+  }
+ if (stristr(PHP_OS, 'WIN')) {
+ 	print $doc->save("portfolio.xml");
+ }else{
+ 	print $doc->save("/tmp/portfolio.xml");
+ }
+    echo "created";
+
+ }
+ else
+{
+  $items = array();
+  $items [] = array(
+  'Symbol' => $symbol,
+  'Amount' => $amount,
+  );
+  
+ if (stristr(PHP_OS, 'WIN')) {
+	 $doc =DOMDocument::load("portfolio.xml");
+ }else{
+  $doc =DOMDocument::load("/tmp/portfolio.xml");
+ }
+ $doc->formatOutput = true;
+ $r = $doc->firstChild;
+
+ 
+  foreach( $items as $item )
+  {
+  $b = $doc->createElement( "item" );
+  
+  $Symbol = $doc->createElement( "Symbol" );
+  $Symbol->appendChild(
+  $doc->createTextNode( $item['Symbol'] )
+  );
+  $b->appendChild( $Symbol );
+  
+  $Amount = $doc->createElement( "Amount" );
+  $Amount->appendChild(
+  $doc->createTextNode( $item['Amount'] )
+  );
+  $b->appendChild( $Amount );
+  
+  $r->appendChild( $b );
+
+  }
+  fwrite($fp,$doc->savexml());
+  fclose($fp);
+  echo "created";
+ }
+}
+}
+
+?>
+
+
+
+
+</body>
+</html>
---
+++ php/samples/solutions/trader/Trader.php
@@ -0,0 +1,231 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+$getResult = $_POST["getResult"];
+$operation = $_POST["operation"];
+$epr = $_POST["epr"];
+$userid = $_POST["userid"];
+$password = $_POST["password"];
+$symbol = $_POST['symbol'];
+$amount = $_POST["amount"];		    
+?>
+
+<html>
+<head>
+<title> Trader Service </title>
+</head>
+<body>
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<table cols="4" border="1" cellpadding="10" cellspacing="0" align="center" width="100%" style="background-image: url(/images/cse.jpg);background-repeat: no-repeat;background-position: center right" bgcolor="#9999FF">
+<tr>
+<td>
+<h4>Trader</h4>
+<h4>Operations</h4>
+
+Create Account : <input type="radio" value="create-account" name="operation" checked ></br></br>
+
+Buy: <input type="radio" value="buy" name="operation"></br></br>
+
+Sell: <input type="radio" value="sell" name="operation"></br></br>
+
+Get Portfolio: <input type="radio" value="getPortfolio" name="operation">
+<br/><br/>
+<h4>Target Endpoint</h4>
+Endpoint:<input type="text" size="60" maxlength="60" name="epr" 
+          value="http://localhost/samples/solutions/trader/TraderService.php"><br />
+
+<h4>Parameters</h4>
+User ID:<input type="text" size="6" maxlength="15" name="userid" value="">
+Password:<input type="password" size="6" maxlength="15" name="password" value="">
+Symbol:<input type="text" size="6" maxlength="15" name="symbol" value="">
+Amount:<input type="text" size="6" maxlength="15" name="amount" value=""><br/>
+<br/>
+
+<input type="submit" value="submit" name="getResult">
+<input type="submit" value="Reset" name="reset">
+
+</td>
+</tr>
+</table>
+
+</form>
+
+
+<?php
+    if (isset($_POST['getResult']))
+       {
+        if (isset($_GET['userid']) && isset($_GET['password']))
+           {
+	    $epr = $_GET['epr'];
+	    $userid = $_GET['userid'];
+	    $password = $_GET['password'];
+	    $operation  = $_GET['operation'];
+	    $symbol = $_GET['symbol'];
+	    $amount = $_GET['amount'];
+           }
+    
+     
+        if (isset($userid) &&  isset($password))
+           {
+            $reqPayloadString = <<<XML
+            <ns1:$operation xmlns:ns1="http://ws.apache.org/axis2/php/trader">
+            <param1>$userid</param1>
+            <param2>$password</param2>
+	    <param3>$symbol</param3>
+	    <param4>$amount</param4>
+</ns1:$operation>
+XML;
+
+            try
+              {
+	       $client = new WSClient(array(
+				      "to"=>$epr));
+	       $response = $client->request($reqPayloadString);
+	 
+	       if ($response)
+	          {
+	   // output according to the operation  
+          	  if ($operation=="create-account")
+                     {
+         	      call_user_func ('resultcreateaccount', $response);
+         	     }
+  
+          	  if ($operation=="getPortfolio")
+                     {		 
+         	      call_user_func ('resultgetPortfolio', $response,"","");
+         	     }
+		
+		  if ($operation=="buy")
+                     {
+        	      call_user_func ('resultbuy', $response,$symbol);
+       	   	     }
+
+		  if ($operation=="sell")
+                     {
+       	              call_user_func ('resultsell', $response,$symbol);
+       	   	     }	   
+		  }
+    	      } 
+  
+            catch (Exception $e)
+              {
+	       if ($e instanceof WSFault)
+	          {
+	           printf("Soap Fault: %s\n", $e->Reason);
+	          }
+	       else
+	          {
+	           printf("Message = %s\n",$e->getMessage());
+	          }
+              }
+           }
+       }
+
+
+
+    function resultcreateaccount($response)
+      {
+       echo  "$response->str";
+      }
+
+
+    function resultbuy($response,$symbol)
+      {
+       $op = "resultbuy";  
+       call_user_func ('resultgetPortfolio', $response,$symbol,$op);
+      
+      }
+
+
+    function resultsell($response,$symbol)
+      {
+       $op = "resultsell";
+       call_user_func ('resultgetPortfolio', $response,$symbol,$op);
+      }
+
+
+    function resultgetPortfolio($response,$symbol1,$op)
+      { 
+       $strresponse = $response->str;
+
+// Making the $strresponse to normal string type thus $output
+       $dom = new DOMDocument;
+       $dom =DOMDocument::loadXML($strresponse);
+       $result = $dom->getElementsByTagName( "result" );
+       $output = $result->item(0)->nodeValue;
+//
+       if (strcmp($output,"Match") == 0)
+          {
+		  $doc = new DOMDocument();
+	   if (stristr(PHP_OS, 'WIN')) {
+	    	   $doc =DOMDocument::load("portfolio.xml");
+	   }else{
+	    	   $doc =DOMDocument::load("portfolio.xml");
+	   }
+
+   	   $items = $doc->getElementsByTagName( "item" );
+?>
+<form >
+<table cols="4" border="1" cellpadding="10" cellspacing="0" align="left" width="50%" bgcolor="#eeeeee">
+<tr>
+<td>
+           <?php   
+           echo "Symbol ";
+           ?>
+           <td>
+           <?php   
+           echo "Amount";
+
+           foreach( $items as $item )
+             {
+              $Symbols = $item->getElementsByTagName( "Symbol" );
+              $Symbol = $Symbols->item(0)->nodeValue;
+
+              $amounts = $item->getElementsByTagName( "Amount" );
+              $amount = $amounts->item(0)->nodeValue;
+           ?>
+<tr>
+<td>
+           <?php   
+           echo "$Symbol";
+           ?>
+</td>
+<td>
+           <?php
+           echo "$amount";
+           echo "million";
+           ?>
+</td>
+</tr>
+           <?php
+             }
+
+           if($op=="resultbuy" or $op=="resultsell")
+             echo "Amount of $symbol1 changed";
+                    
+          }
+       else
+          echo "$strresponse";
+
+           ?>
+</table>
+</form>
+<?php
+      }
+
+?>
+</body>
+</html>
