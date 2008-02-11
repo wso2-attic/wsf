7121567eb7710c2c0b94e7adf0386d246e35506a
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-02-11 05:32:42 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@13542 a5903396-d722-0410-b921-86c7d4935375

--- php/clean.bat
+++ php/clean.bat
@@ -1,14 +1,14 @@
 @if exist wsf_c\wso2-wsf-c-bin-1.2.0 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.2.0
 
-@if exist wso2-wsf-php-1.2.1-win32 rmdir /S /Q wso2-wsf-php-1.2.1-win32
+@if exist wso2-wsf-php-bin-1.2.1-win32 rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32
 
-@if exist wsf_c\axis2c\build\axis2c-bin-1.2.0-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-1.3.0-win32
+@if exist wsf_c\axis2c\build\axis2c-bin-1.3.0-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-1.3.0-win32
 
 @if exist wsf_c\axis2c\build\deploy rmdir /s /q wsf_c\axis2c\build\deploy
 
 @if exist wsf_c\axis2c\build\win32\int.msvc rmdir /s /q wsf_c\axis2c\build\win32\int.msvc
 
-@if exist wsf_c\rampartc\build\rampartc-1.0.0 rmdir /s /q wsf_c\rampartc\build\rampartc-1.0.0
+@if exist wsf_c\rampartc\build\rampartc-1.1.0 rmdir /s /q wsf_c\rampartc\build\rampartc-1.1.0
 
 @if exist wsf_c\rampartc\build\win32\int.msvc rmdir /s /q wsf_c\rampartc\build\win32\int.msvc
 
