72e739b13e981f7df5d427b277a33f66f17741ad
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2008-02-13 06:21:21 +0000

    Moved wsclient directory from wsf/c to wsf/wsclient
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@13666 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/samples/echo_rest.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-"%WSFC_HOME%\bin\wsclient" http://localhost:9090/axis2/services/echo < "%WSFC_HOME%\bin\samples\wsclient\data\echo.xml"
-
--- c/wsclient/samples/echo_rest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if test -z $WSFC_HOME; then 
-    WSFC_HOME=$PWD/../../..
-fi
-$WSFC_HOME/bin/wsclient http://localhost:9090/axis2/services/echo <$WSFC_HOME/bin/samples/wsclient/data/echo.xml
-
