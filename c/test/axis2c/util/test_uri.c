6b10bbca6676b1ed9f7397cd9d59034ed739f2bb
senaka <senaka@a5903396-d722-0410-b921-86c7d4935375>
2008-02-28 10:41:42 +0000

    Improving test_uri.c, added tests for parse_hostinfo
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@14309 a5903396-d722-0410-b921-86c7d4935375

--- c/test/axis2c/util/test_uri.c
+++ c/test/axis2c/util/test_uri.c
@@ -22,6 +22,10 @@ static void test_axutil_uri_create(
     wsf_unit_test_case_t *tc,
     void *data);
 
+static void test_axutil_uri_parse_hostinfo(    
+    wsf_unit_test_case_t *tc,
+    void *data);
+
 wsf_unit_status_t WSF_UNIT_CALL test_uri(wsf_unit_suite_t * suite)
 {
     wsf_unit_status_t status = WSF_UNIT_FAILURE;
@@ -42,6 +46,7 @@ wsf_unit_status_t WSF_UNIT_CALL test_uri(wsf_unit_suite_t * suite)
     }
 
     wsf_unit_run_test(suite, test_axutil_uri_create, data);
+    wsf_unit_run_test(suite, test_axutil_uri_parse_hostinfo, data);
 
     if (data)
     {
@@ -84,4 +89,144 @@ static void WSF_UNIT_CALL test_axutil_uri_create(wsf_unit_test_case_t *tc, void 
         test_data->test_data = uri;
     }
     WSF_UNIT_FAIL(tc, "Add axutil_uri_create to Header and Remove this");
+}
+
+static void WSF_UNIT_CALL test_axutil_uri_parse_hostinfo(wsf_unit_test_case_t *tc, void *data)
+{
+    axutil_env_t *env = NULL;
+    axutil_test_data_t *test_data = (axutil_test_data_t *)data;
+    axutil_uri_t *uri = NULL;
+
+    WSF_UNIT_ASSERT_NOT_NULL(tc, "Test Data Unavailable", test_data);
+    WSF_UNIT_ASSERT_NOT_NULL(tc, "Test Environment Unavailable", test_data->test_env);
+
+    if (!test_data || !test_data->test_env)
+    {
+        return;
+    }
+
+    env = test_data->test_env;
+    
+    uri = axutil_uri_parse_hostinfo(env, "home.netscape.com:443");
+
+    WSF_UNIT_ASSERT_NOT_NULL(tc, "CONNECT Parsing Failed", uri);
+
+    WSF_UNIT_ASSERT_EQUALS_INT(tc, "CONNECT Parsing Identified Invalid Port",
+        443, (int)axutil_uri_get_port(uri, env));
+
+    WSF_UNIT_ASSERT_EQUALS_STRING(tc, "CONNECT Parsing Identified Invalid Server",
+        "home.netscape.com:443", axutil_uri_get_server(uri, env));
+
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "[sample.ipvsix.com]:443");
+
+    WSF_UNIT_ASSERT_NOT_NULL(tc, "CONNECT Parsing Failed", uri);
+
+    WSF_UNIT_ASSERT_EQUALS_INT(tc, "CONNECT Parsing Identified Invalid Port",
+        443, (int)axutil_uri_get_port(uri, env));
+
+    WSF_UNIT_ASSERT_EQUALS_STRING(tc, "CONNECT Parsing Identified Invalid Server",
+        "[sample.ipvsix.com]:443", axutil_uri_get_server(uri, env));
+
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "192.168.0.1:443");
+
+    WSF_UNIT_ASSERT_NOT_NULL(tc, "CONNECT Parsing Failed", uri);
+
+    WSF_UNIT_ASSERT_EQUALS_INT(tc, "CONNECT Parsing Identified Invalid Port",
+        443, (int)axutil_uri_get_port(uri, env));
+
+    WSF_UNIT_ASSERT_EQUALS_STRING(tc, "CONNECT Parsing Identified Invalid Server",
+        "192.168.0.1:443", axutil_uri_get_server(uri, env));
+
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    /* Error Tests */
+    uri = axutil_uri_parse_hostinfo(env, "home.netscape.com");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "home.netscape.com:443/extra");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "home.netscape.com:443?extra");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "home.netscape.com:443#extra");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "http://home.netscape.com:443");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "//home.netscape.com:443");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, ".home.netscape.com:443");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+
+    uri = axutil_uri_parse_hostinfo(env, "[home.netscape.com]:443");
+
+    WSF_UNIT_ASSERT_NULL(tc, "CONNECT Parsing Must Fail But, Didn't", uri);
+    if (uri)
+    {
+        axutil_uri_free((axutil_uri_t *)uri, env);
+        uri = NULL;
+    }
+    /* End of Tests */
+    WSF_UNIT_FAIL(tc, "No Method to get Hostname");
+    WSF_UNIT_FAIL(tc, "axutil_uri_get_server Returns Reference but NO Documentation");
 }
