d46fa981e8eb600dae227dc869d4abc8bbef4e49
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2008-03-10 08:08:04 +0000

    Adding new files for testing security
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@14640 a5903396-d722-0410-b921-86c7d4935375

---
+++ c/wsclient/samples/security_sign.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if test -z $WSFC_HOME; then 
+    WSFC_HOME=$PWD/../../..
+fi
+$WSFC_HOME/bin/wsclient --soap --no-mtom --timestamp --sign-body --key $WSFC_HOME/bin/samples/rampart/keys/ahome/alice_key.pem --certificate $WSFC_HOME/bin/samples/rampart/keys/ahome/alice_cert.cert --recipient-certificate $WSFC_HOME/bin/samples/rampart/keys/ahome/bob_cert.cert  http://localhost:9090/axis2/services/sec_echo <$WSFC_HOME/bin/samples/wsclient/data/echo.xml
