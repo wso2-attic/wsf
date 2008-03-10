ac9b3b695f046a17555baf240ba41cf9edb91ea6
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2008-03-10 06:53:41 +0000

    Changed the version number for the release of 1.2.1
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@14630 a5903396-d722-0410-b921-86c7d4935375

--- c/build.bat
+++ c/build.bat
@@ -3,7 +3,7 @@
 
 rem Build Apache Axis2/C
 @cd axis2c\build
-@if exist axis2c-bin-1.2.0-win32 rmdir /s /q axis2c-bin-1.2.0-win32
+@if exist axis2c-bin-1.2.1-win32 rmdir /s /q axis2c-bin-1.2.1-win32
 @cd win32
 @nmake dist AUTOCONF=..\..\..\configure.in
 @if not %ERRORLEVEL% EQU 0 goto end
@@ -11,7 +11,7 @@ rem Build Apache Axis2/C
 rem Build Flickr sample
 @cd ..\..\..\examples\flickr
 @nmake -f flickr.mk
-@copy /Y flickr.exe  ..\..\axis2c\build\axis2c-bin-1.2.0-win32\bin\samples
+@copy /Y flickr.exe  ..\..\axis2c\build\axis2c-bin-1.2.1-win32\bin\samples
 
 rem Build Trader sample
 @cd ..\trader
@@ -26,12 +26,12 @@ rem Build Optional Modules
 rem Build Neethi util
 :build_neethi_util
 @cd axis2c\neethi\src\util
-@nmake -f neethi_util.mk AXIS2_BIN_DIR=..\..\..\build\axis2c-bin-1.2.0-win32 AUTOCONF=..\..\..\..\configure.in
+@nmake -f neethi_util.mk AXIS2_BIN_DIR=..\..\..\build\axis2c-bin-1.2.1-win32 AUTOCONF=..\..\..\..\configure.in
 @cd ..\..\..\..\
 
 rem Build wsclient
 @cd wsclient
-@nmake -f wsclient.mk AXIS2_BIN_DIR=..\axis2c\build\axis2c-bin-1.2.0-win32 AUTOCONF=..\configure.in
+@nmake -f wsclient.mk AXIS2_BIN_DIR=..\axis2c\build\axis2c-bin-1.2.1-win32 AUTOCONF=..\configure.in
 @cd ..
 
 rem Build Unit Test Framework
@@ -42,15 +42,15 @@ rem Build Unit Test Framework
 rem Build Tests
 
 :pack_wsfc
-@if not exist wso2-wsf-c-bin-1.2.0-win32 mkdir wso2-wsf-c-bin-1.2.0-win32
-@xcopy /E /Q /I /Y axis2c\build\axis2c-bin-1.2.0-win32 wso2-wsf-c-bin-1.2.0-win32
-@copy /Y config\axis2.xml wso2-wsf-c-bin-1.2.0-win32\axis2.xml
-@copy /Y INSTALL wso2-wsf-c-bin-1.2.0-win32
-@copy /Y README wso2-wsf-c-bin-1.2.0-win32
-@copy /Y NEWS wso2-wsf-c-bin-1.2.0-win32
-@copy /Y COPYING wso2-wsf-c-bin-1.2.0-win32
-@copy /Y AUTHORS wso2-wsf-c-bin-1.2.0-win32
-@copy /Y NOTICE wso2-wsf-c-bin-1.2.0-win32
-@xcopy /E /I /Q /Y docs  wso2-wsf-c-bin-1.2.0-win32\docs
+@if not exist wso2-wsf-c-bin-1.2.1-win32 mkdir wso2-wsf-c-bin-1.2.1-win32
+@xcopy /E /Q /I /Y axis2c\build\axis2c-bin-1.2.1-win32 wso2-wsf-c-bin-1.2.1-win32
+@copy /Y config\axis2.xml wso2-wsf-c-bin-1.2.1-win32\axis2.xml
+@copy /Y INSTALL wso2-wsf-c-bin-1.2.1-win32
+@copy /Y README wso2-wsf-c-bin-1.2.1-win32
+@copy /Y NEWS wso2-wsf-c-bin-1.2.1-win32
+@copy /Y COPYING wso2-wsf-c-bin-1.2.1-win32
+@copy /Y AUTHORS wso2-wsf-c-bin-1.2.1-win32
+@copy /Y NOTICE wso2-wsf-c-bin-1.2.1-win32
+@xcopy /E /I /Q /Y docs  wso2-wsf-c-bin-1.2.1-win32\docs
 
 :end
