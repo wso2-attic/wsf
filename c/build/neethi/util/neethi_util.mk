2277ad4e11f67fa89f6866b13e14f9baf8047365
dumindu <dumindu@a5903396-d722-0410-b921-86c7d4935375>
2008-03-26 22:30:20 +0000

    Fixed the build system to use AXIS2C_VERSION, WSFC_VERSION variables instead of having the versions hard coded.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@15136 a5903396-d722-0410-b921-86c7d4935375

--- c/build/neethi/util/neethi_util.mk
+++ c/build/neethi/util/neethi_util.mk
@@ -1,4 +1,4 @@
-AXIS2_BIN_DIR= ..\..\..\build\axis2c-bin-1.1.0-win32
+AXIS2_BIN_DIR= ..\..\..\build\axis2c-bin-$(AXIS2C_VERSION)-win32
 
 CFLAGS = /nologo /D "WIN32" /D "AXIS2_DECLARE_EXPORT" /D "_WINDOWS" /D "_MBCS"
 LDFLAGS = /nologo
