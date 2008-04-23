ab3aa27757a77f2ffde4cd6556988c53f9c8b83e
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-04-23 08:03:04 +0000

    clean.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@16019 a5903396-d722-0410-b921-86c7d4935375

--- php/clean.bat
+++ php/clean.bat
@@ -1,14 +1,17 @@
-@if exist wsf_c\wso2-wsf-c-bin-1.2.0 rmdir /S /Q wsf_c\wso2-wsf-c-bin-1.2.0
+@call wsf_c\build\init.bat
+set WSFPHP_VERSION=1.2.2
 
-@if exist wso2-wsf-php-bin-1.2.1-win32 rmdir /S /Q wso2-wsf-php-bin-1.2.1-win32
+@if exist wsf_c\wso2-wsf-c-bin-%WSFC_VERSION% rmdir /S /Q wsf_c\wso2-wsf-c-bin-%WSFC_VERSION%
 
-@if exist wsf_c\axis2c\build\axis2c-bin-1.3.0-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-1.3.0-win32
+@if exist wso2-wsf-php-bin-%WSFPHP_VERSION%-win32 rmdir /S /Q wso2-wsf-php-bin-%WSFPHP_VERSION%-win32
+
+@if exist wsf_c\axis2c\build\axis2c-bin-%AXIS2C_VERSION%-win32 rmdir /s /q wsf_c\axis2c\build\axis2c-bin-%AXIS2C_VERSION%-win32
 
 @if exist wsf_c\axis2c\build\deploy rmdir /s /q wsf_c\axis2c\build\deploy
 
 @if exist wsf_c\axis2c\build\win32\int.msvc rmdir /s /q wsf_c\axis2c\build\win32\int.msvc
 
-@if exist wsf_c\rampartc\build\rampartc-1.1.0 rmdir /s /q wsf_c\rampartc\build\rampartc-1.1.0
+@if exist wsf_c\rampartc\build\rampartc-%RAMPART_VERSION% rmdir /s /q wsf_c\rampartc\build\rampartc-%RAMPART_VERSION%
 
 @if exist wsf_c\rampartc\build\win32\int.msvc rmdir /s /q wsf_c\rampartc\build\win32\int.msvc
 
