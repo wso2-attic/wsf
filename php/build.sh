e02c410dceabe56667a3bc234e9876121d1ede60
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2008-04-27 09:06:29 +0000

    Copying wsf_c/axis2/axiom/src/parser/Makefile.am
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@16211 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -8,6 +8,7 @@ build/copy_build_files.sh
 cd ..
 cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c/
 cp build/wsf_c/axis2c/axiom/configure.ac wsf_c/axis2c/axiom
+cp build/wsf_c/axis2c/axiom/src/parser/Makefile.am wsf_c/axis2c/axiom/src/parser/
 cp build/wsf_c/axis2c/tools/tcpmon/configure.ac wsf_c/axis2c/tools/tcpmon
 
 for i in `find . -name configure.ac`
