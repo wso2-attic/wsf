519ab067a8dfa5e814fb6055448f45fa1ea5b210
janapriya <janapriya@a5903396-d722-0410-b921-86c7d4935375>
2008-04-28 18:02:58 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@16294 a5903396-d722-0410-b921-86c7d4935375

--- ruby/lib/wsdl/ws_wsdl_proxy.rb
+++ ruby/lib/wsdl/ws_wsdl_proxy.rb
@@ -18,11 +18,10 @@ module WSO2
       end
       
       def method_missing(name, *args)
-        if args.length == 1 then #only 1 argument supported.
+        if args.length == 1 then 
           arg = args[0]
           if arg.kind_of? Hash then
-            #TODO: if there is no "/" or "\" in the wsdl path, this is a relative path. Add the current location of the script. 	handle paths with ".." too.
-            result = WSFC::wsf_wsdl_request_function(@env, 
+            esult = WSFC::wsf_wsdl_request_function(@env, 
                                                                       @svc_client, 
                                                                       @options, 
                                                                       @wsdl, 
@@ -43,3 +42,53 @@ module WSO2
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      #~ def method_missing(name, *args)
+        #~ if args.length == 1 then #only 1 argument supported.
+          #~ arg = args[0]
+          #~ if arg.kind_of? Hash then
+            #~ #TODO: if there is no "/" or "\" in the wsdl path, this is a relative path. Add the current location of the script. 	handle paths with ".." too.
+            #~ result = WSFC::wsf_wsdl_request_function(@env, 
+                                                                      #~ @svc_client, 
+                                                                      #~ @options, 
+                                                                      #~ @wsdl, 
+                                                                      #~ name.to_s, arg,
+                                                                      #~ @type_map,
+                                                                      #~ @xslt_location)
+            #~ return result
+          #~ else
+            #~ puts "only Hash is supported"
+          #~ end
+        #~ else
+          #~ puts "dynamic function only support one argument"
+        #~ end
+        #~ return nil
+      #~ end    
