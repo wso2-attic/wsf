9ee9a35f8a83c8e67c4ce44f234a666a4f2062c4
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2008-05-09 09:15:45 +0000

    Changed the version number
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@16732 a5903396-d722-0410-b921-86c7d4935375

--- c/build/packaging/package.sh
+++ c/build/packaging/package.sh
@@ -14,8 +14,8 @@ make install
 make samples -j 10
 
 make dist
-tar xf wso2-wsf-c-src-1.1.0.tar.gz
-cd wso2-wsf-c-src-1.1.0
+tar xf wso2-wsf-c-src-1.2.0.tar.gz
+cd wso2-wsf-c-src-1.2.0
 ./configure
 make maintainer-clean
 
@@ -29,7 +29,7 @@ cd ../../rampartc/samples
 ./configure
 make maintainer-clean
 cd ../../../
-rm -rf wso2-wsf-c-src-1.1.0.tar.gz
-tar -pczf wso2-wsf-c-src-1.1.0.tar.gz wso2-wsf-c-src-1.1.0
-zip -r wso2-wsf-c-src-1.1.0.zip wso2-wsf-c-src-1.1.0
-rm -rf wso2-wsf-c-src-1.1.0
+rm -rf wso2-wsf-c-src-1.2.0.tar.gz
+tar -pczf wso2-wsf-c-src-1.2.0.tar.gz wso2-wsf-c-src-1.2.0
+zip -r wso2-wsf-c-src-1.2.0.zip wso2-wsf-c-src-1.2.0
+rm -rf wso2-wsf-c-src-1.2.0
