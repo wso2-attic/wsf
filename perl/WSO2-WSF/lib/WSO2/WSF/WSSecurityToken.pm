45ec08d442deb5514ee0ebe853bf5266d0298b05
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-05-11 04:27:58 +0000

    Removing certificate delimiters.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@16789 a5903396-d722-0410-b921-86c7d4935375

--- perl/WSO2-WSF/lib/WSO2/WSF/WSSecurityToken.pm
+++ perl/WSO2-WSF/lib/WSO2/WSF/WSSecurityToken.pm
@@ -12,15 +12,25 @@ sub new {
 
     # $self = {} unless( defined( $self ) );
 
-    if( (defined $self->{policy}) and ( scalar( $self->{policy} ) =~ /HASH/ ) ) {
+    if ( (defined $self->{policy}) and ( scalar( $self->{policy} ) =~ /HASH/ ) ) {
 	die "ERROR:  Policy should be a hash";
     }
 
-    if( defined( $self ) ) {
+    if ( defined( $self ) ) {
 	foreach my $k (keys (%{$self})) {
+	    # removing certificate delimiters, until this is handled in
+	    # Rampart/C itself
+	    if ( $k eq "privateKey" ) {
+		$self->{$k} =~ s/-----BEGIN RSA PRIVATE KEY-----//sim if defined $self->{$k};
+		$self->{$k} =~ s/-----END RSA PRIVATE KEY-----//sim if defined $self->{$k};
+	    } elsif ( ($k eq "receiverCertificate") or ($k eq "certificate") ) {
+		$self->{$k} =~ s/-----BEGIN CERTIFICATE-----//sim if defined $self->{$k};
+		$self->{$k} =~ s/-----END CERTIFICATE-----//sim if defined $self->{$k};
+	    }
 	    $this->{$k} = $self->{$k};
 	}
     }
+
 
     bless $self, $this;
     return $self;
