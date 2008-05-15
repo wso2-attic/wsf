42e79dbd95fb63e19524b04bd31855db5fab4833
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-05-15 08:40:37 +0000

    Adding copyright notice.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@17034 a5903396-d722-0410-b921-86c7d4935375

--- perl/samples/reliable_messaging/ping_client.pl
+++ perl/samples/reliable_messaging/ping_client.pl
@@ -1,5 +1,18 @@
 #!/usr/bin/env perl
 
+# Copyright 2005,2006 WSO2, Inc. http://wso2.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
 use WSO2::WSF;
 
 # This assumes WSF/C is installed and axis2_http_server is listening on port 8585
