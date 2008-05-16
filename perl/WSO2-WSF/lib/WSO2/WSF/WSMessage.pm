7470b528a83748e6563649afc4ad10c401e949eb
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-05-16 05:45:18 +0000

    Adding POD
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@17076 a5903396-d722-0410-b921-86c7d4935375

--- perl/WSO2-WSF/lib/WSO2/WSF/WSMessage.pm
+++ perl/WSO2-WSF/lib/WSO2/WSF/WSMessage.pm
@@ -1,5 +1,19 @@
 package WSO2::WSF::WSMessage;
 
+# Copyright 2005,2006 WSO2, Inc. http://wso2.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 use 5.008008;
 # use strict;
 use warnings;
@@ -55,10 +69,10 @@ create a WSClient object.
 
 =over 4
 
-=item axis2c_home
+=item wsfc_home
 
-This is a mandatory options that contain the absolute folder of your Axis2/C
-or WSF/C installation.
+This is a mandatory options that contain the absolute folder of your
+WSF/C installation.
 
 =item payload
 
@@ -89,6 +103,18 @@ URI specifying where the response should be sent to.  WS-A ReplyTo.
 URI specifying where the SOAP fault should be sent to in case of an error.
 WS-A FaultTo.
 
+=item inputHeaders
+
+This accept an array of WSHeader objects.  Even if it's one header element
+you have to give it in an array.
+
+my $header1 = new WSO2::WSF::WSHeader( { ... } );
+
+my $client = new WSO2::WSF::WSMessage( { ...
+                                         'inputHeaders' => [ $header1 ],
+                                         ...
+                                       } );
+
 =head1 SEE ALSO
 
 Look at WSO2::WSF::WSClient to see how to pass a WSMessage and consume an
@@ -99,22 +125,22 @@ http://wso2.org/projects/wsf/perl
 
 =head1 AUTHOR
 
-Chintana Wilamuna, E<lt>chintana@wso2.comE<gt>
+WSO2 WSF/Perl Team
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2007 by WSO2
 
-Licensed under the Apache License, Version 2.0 (the "License");             
-you may not use this file except in compliance with the License.            
-You may obtain a copy of the License at                                     
-                                                                            
-      http://www.apache.org/licenses/LICENSE-2.0                             
-                                                                            
-Unless required by applicable law or agreed to in writing, software         
-distributed under the License is distributed on an "AS IS" BASIS,           
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    
-See the License for the specific language governing permissions and         
-limitations under the License.     
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =cut
