f70b9a3b8860f274956e5ffb06ee7e7de81e05f8
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-05-16 05:44:02 +0000

    Adding POD
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@17073 a5903396-d722-0410-b921-86c7d4935375

--- perl/WSO2-WSF/lib/WSO2/WSF/WSHeader.pm
+++ perl/WSO2-WSF/lib/WSO2/WSF/WSHeader.pm
@@ -1,5 +1,19 @@
 package WSO2::WSF::WSHeader;
 
+# Copyright 2005,2006 WSO2, Inc. http://wso2.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 use 5.008008;
 # use strict;
 use warnings;
@@ -20,4 +34,103 @@ sub new {
     bless $self, $this;
     return $self;
 }
+
 1;
+__END__
+
+=head1 NAME
+
+WSO2::WSF::WSHeader - Create a WSHeader object.
+
+=head1 SYNOPSYS
+
+  my $header = new WSO2::WSF::WSHeader( { WSHEADER_OPTIONS } );
+
+  my $header = new WSO2::WSF::WSHeader( { 'name' => 'person',
+                                          'data' => 'Frodo',
+                                          ...
+                                        } );
+
+  my $client = new WSO2::WSF::WSClient( { ...
+                                          'inputHeaders' => [ $header ],
+                                          ...
+                                        } );
+
+=head1 DESCRIPTION
+
+A WSMessage object can be used to store all the info in order to successfully
+consume a Web service.  This could be passed to the request method after you
+create a WSClient object.
+
+A WSHeader object can be used to hold a custom header element which could
+then be passed as an array to 'inputHeaders' option when creating a WSClient
+object.
+
+=head1 WSHEADER_OPTIONS
+
+=over 4
+
+=item ns
+
+Specify SOAP header's namespace URI.
+
+=item nsprefix
+
+Soap header's namespace prefix.
+
+=item name
+
+Soap header element's localname
+
+=item data
+
+If it's a single value give it as a string, if nested elements are need to
+be generated give an array of WSHeader objects.
+
+# a single header element
+
+  my $h1 = new WSO2::WSF::WSHeader( { 'name' => 'person',
+                                      'data' => 'Frodo'
+                                    } );
+
+# header with child elements
+
+  my $name = new WSO2::WSF::WSHeader( { 'name' => 'fullname',
+                                        'data' => 'Frodo Baggins'
+                                      } );
+
+  my $age = new WSO2::WSF::WSHeader( { 'name' => 'age',
+                                       'data' => 'Eleventy-one'
+                                     } );
+
+  my $h3 = new WSO2::WSF::WSHeader( { 'name' => 'person',
+                                      'data' => [ $name, $age ]
+                                    } );
+
+=head1 SEE ALSO
+
+Look at WSO2::WSF::WSClient to see how to pass a WSHeader and create a custom
+header element.
+
+=head1 AUTHOR
+
+WSO2 WSF/Perl Team
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by WSO2
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
