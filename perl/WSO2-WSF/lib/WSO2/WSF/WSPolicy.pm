31f6358f36a3d482dbe08f42ac588422f7db2e05
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-05-16 05:45:44 +0000

    Adding POD
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@17077 a5903396-d722-0410-b921-86c7d4935375

--- perl/WSO2-WSF/lib/WSO2/WSF/WSPolicy.pm
+++ perl/WSO2-WSF/lib/WSO2/WSF/WSPolicy.pm
@@ -1,5 +1,19 @@
 package WSO2::WSF::WSPolicy;
 
+# Copyright 2005,2006 WSO2, Inc. http://wso2.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 use 5.008008;
 # use strict;
 use warnings;
@@ -167,5 +181,88 @@ sub create_policy_from_string {
     return WSO2::WSF::C::wsf_str_to_axiom_node($env, $this->{security}, length($this->{security}));
 }
 
-
 1;
+__END__
+
+=head1 NAME
+
+WSO2::WSF::WSPolicy - Create a WSPolicy object
+
+=head1 SYNOPSYS
+
+  my $policy = new WSO2::WSF::WSPolicy( { WSPOLICY_OPTIONS } );
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEC_TOKEN_OPTIONS
+
+=over 4
+
+=item user
+
+Holds the username.
+
+=item certificate
+
+User's certificate to be used for signing.  Load the entire certificate
+into a string and give that as a value.  You can use the usual slurp
+mode to do this.
+
+my CERT, "< my_certificate.cert";
+undef $/;
+my $cert = <CERT>;
+
+=item password
+
+User's password used for UsernameToken
+
+=item passwordType
+
+Password type used in UsernameToken.  Valid vaules are 'PlainText' and
+'Digest'.
+
+=item privateKey
+
+User's private key, loaded from the privake key file, same as loading
+the certificate.
+
+=item receiverCertificate
+
+Receiver's certificate to be used for encryption, same as loading the
+certificate.
+
+=item ttl
+
+The time period within which the message would be valid in seconds.
+
+=head1 SEE ALSO
+
+Look at WSO2::WSF::WSClient to see how to pass a WSMessage and consume an
+existing Web service.
+
+Mailing list, bug tracker, svn info can be found on the project web site at
+http://wso2.org/projects/wsf/perl
+
+=head1 AUTHOR
+
+WSO2 WSF/Perl Team
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by WSO2
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
