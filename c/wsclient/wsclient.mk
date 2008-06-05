8ebb121d00483a00d6305b60ea40485042068f7b
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-06-05 06:59:28 +0000

    makefile updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@17957 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/wsclient.mk
+++ c/wsclient/wsclient.mk
@@ -5,6 +5,17 @@ AUTOCONF = ..\configure.in
 
 AXIS2_BIN_DIR=.\..\axis2c\build\axis2c-bin-$(AXIS2C_VERSION)-win32
 
+CFLAGS = /nologo /D "WIN32" /w /D "_WINDOWS" /D "AXIS2_DECLARE_EXPORT" /D "_MBCS" \
+	/I$(AXIS2_BIN_DIR)\include /I.\..\axis2c\neethi\include /I.\..\axis2c\neethi\src\util
+
+!if "$(DEBUG)" == "1"
+CFLAGS = $(CFLAGS) /D "_DEBUG" /Od /Z7 $(CRUNTIME)d
+LDFLAGS = $(LDFLAGS) /DEBUG
+!else
+CFLAGS = $(CFLAGS) /D "NDEBUG" /O2 $(CRUNTIME)
+LDFLAGS = $(LDFLAGS)
+!endif
+
 !if "$(ENABLE_RAMPARTC)" == "1"
 OPFLAGS = mod_rampart.lib
 !else
@@ -15,11 +26,12 @@ wsclient:
 	echo off
 
 	if not exist int.msvc mkdir int.msvc
-	cl.exe /nologo /D "WIN32" /w /D "_WINDOWS" /D "AXIS2_DECLARE_EXPORT" /D "_MBCS" src\*.c \
-	/I$(AXIS2_BIN_DIR)\include /I.\..\axis2c\neethi\include /I.\..\axis2c\neethi\src\util /Foint.msvc\ /c
+	cl.exe $(CFLAGS) src\*.c /Foint.msvc\ /c
 
-	link.exe /LIBPATH:$(AXIS2_BIN_DIR)\lib int.msvc\*.obj axutil.lib  axis2_engine.lib \
-	axis2_parser.lib axiom.lib neethi_util.lib axis2_http_sender.lib $(OPFLAGS) /OUT:$(AXIS2_BIN_DIR)\bin\wsclient.exe
+	link.exe $(LDFLAGS) /LIBPATH:$(AXIS2_BIN_DIR)\lib int.msvc\*.obj axutil.lib  axis2_engine.lib \
+	axis2_parser.lib axiom.lib neethi_util.lib axis2_http_sender.lib $(OPFLAGS) \
+	/OUT:$(AXIS2_BIN_DIR)\bin\wsclient.exe
+
 	if exist int.msvc rmdir /s /q int.msvc
 	if not exist $(AXIS2_BIN_DIR)\bin\samples\wsclient mkdir $(AXIS2_BIN_DIR)\bin\samples\wsclient
 	xcopy /S /Y samples\* $(AXIS2_BIN_DIR)\bin\samples\wsclient
