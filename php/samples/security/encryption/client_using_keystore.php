0d2331cef754d5a43c1dee55a1a8da30196cc2b3
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-06-11 12:11:41 +0000

    client with key store added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18324 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/security/encryption/client_using_keystore.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Copyright 2005,2008 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+$reqPayloadString = <<<XML
+<ns1:echo xmlns:ns1="http://php.axis2.org/samples"><text>Hello World!</text></ns1:echo>
+XML;
+
+try {
+    
+    $reqMessage = new WSMessage($reqPayloadString,
+                                array("to" => "http://localhost:8080/samples/security/encryption/service.php",
+                                      "action" => "http://php.axis2.org/samples/echoString"));
+    
+    $sec_array = array("encrypt" => TRUE,
+                       "algorithmSuite" => "Basic256Rsa15",
+                       "securityTokenReference" => "IssuerSerial");
+    
+    
+    $keystore = file_get_contents("../keys/alice_kstore.p12");
+
+    $policy = new WSPolicy(array("security" => $sec_array));
+    $sec_token = new WSSecurityToken(array("PKCS12KeyStore"=>$keystore,
+	    				   "user"=>"a",
+				   	   "password"=>"a12345"));
+    
+    $client = new WSClient(array("useWSA" => TRUE,
+                                 "policy" => $policy,
+				 "securityToken" => $sec_token));
+				
+    $resMessage = $client->request($reqMessage);
+    
+    printf("Response = %s \n", $resMessage->str);
+
+} catch (Exception $e) {
+
+	if ($e instanceof WSFault) {
+		printf("Soap Fault: %s\n", $e->Reason);
+	} else {
+		printf("Message = %s\n",$e->getMessage());
+	}
+
+}
+?>
