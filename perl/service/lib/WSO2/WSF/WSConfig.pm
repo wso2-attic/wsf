1aafd02d37a39cc0a04071b6ae5e4bf551c2c77a
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-06-26 14:40:19 +0000

    Getting WSFC_HOME from the standard location.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18659 a5903396-d722-0410-b921-86c7d4935375

--- perl/service/lib/WSO2/WSF/WSConfig.pm
+++ perl/service/lib/WSO2/WSF/WSConfig.pm
@@ -1,6 +1,6 @@
 package WSO2::WSF::WSConfig;
 
-use constant WSFC_HOME => "\/home\/dinesh\/wsf_c\/deploy";
+use constant WSFC_HOME => "\/opt\/wso2\/wsf_c";
 use constant LOG_PATH  => "\/tmp\/wsf_per_service.log";
 use constant LOG_LEVEL => "debug";
 
