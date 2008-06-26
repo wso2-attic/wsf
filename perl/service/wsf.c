2c31a719a8035304ff097e229b7940a492e6cbc6
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-06-26 14:54:09 +0000

    Removing all the functions, not used anymore, now we're initing per request.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18661 a5903396-d722-0410-b921-86c7d4935375

--- perl/service/wsf.c
+++ perl/service/wsf.c
@@ -17,74 +17,8 @@
 #include "wsf.h"
 #include "wsf_worker.h"
 #include "wsf_util.h"
+#include "wsf_xml_msg_recv.h"
 #include <EXTERN.h>
 #include <perl.h>
 
-static axutil_env_t *env;
-static int ws_is_svr;
-axis2_msg_recv_t *wsf_msg_recv;
-wsf_worker_t *worker;
-static PerlInterpreter *my_perl;
 
-void
-wsf_init (const axis2_char_t *path_to_log,
-          int log_level)
-{
-    axis2_char_t *repo_path = "/home/dinesh/wsf_c/deploy";
-    
-    env = wsf_env_create (path_to_log);
-    if (!env)
-    {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI, 
-                         "unable to create environment, given log path is %s \
-and log level is %d", path_to_log, log_level);
-        return;
-    }
-	env->log->level = log_level;
-
-    wsf_msg_recv = (axis2_msg_recv_t* )wsf_xml_msg_recv_create (env);
-
-    if (!wsf_msg_recv)
-    {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI,
-                         "wsf perl msg receiver creation failed \n");
-        return;
-    }
-    axiom_xml_reader_init ();
-
-    /* repo path need to be given for worker creation */
-    worker = wsf_worker_create (env, 
-                                repo_path);
-    if (!worker)
-    {
-        AXIS2_LOG_ERROR (env->log, AXIS2_LOG_SI,
-                         "wsf perl worker creation failed for repo path %s", 
-                         repo_path);
-        return;
-    }
-    AXIS2_LOG_INFO (env->log, "wsf perl successfully initialized");
-}
-
-wsf_worker_t *
-wsf_get_worker ()
-{
-    if (!worker)
-        return NULL;
-    return worker;
-}
-
-axutil_env_t *
-wsf_get_env ()
-{
-    if (!env)
-        return NULL;
-    return env;
-}
-
-axis2_msg_recv_t *
-wsf_get_msg_recv ()
-{
-    if (!wsf_msg_recv)
-        return NULL;
-    return wsf_msg_recv;
-}
