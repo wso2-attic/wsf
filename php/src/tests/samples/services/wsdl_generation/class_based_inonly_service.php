9ceb6c81b3fece81c992608ba07fed102fb349eb
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2008-06-27 08:47:42 +0000

    adding test service for in only operation
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18686 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/src/tests/samples/services/wsdl_generation/class_based_inonly_service.php
@@ -0,0 +1,141 @@
+<?php
+/*
+ * Copyright 2005,2006 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+class Bar
+{
+    private $value = "Test";
+    private $end = "Test";
+
+    
+    function Bar($start, $end){
+        $this->value = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * echoValue function
+     * @param array of object Foo $param1 param1 description
+     * @param string $param2
+     */
+    function echoValue($param1, $param2){
+
+        $boo1 = new Boo();
+        $boo1->boo1 = array("test1", "test2");
+        $boo1->boo2 = array("test3", "test4");
+
+        $boo2 = new Boo();
+        $boo2->boo1 = array("test1", "test2");
+        $boo2->boo2 = array("test3", "test4");
+
+        //either following or the other
+        return array("ret_value"=>array($boo1, $boo2));
+
+        return  new echoStringResponse(array($boo1, $boo2));
+    }
+}
+/**
+ * classname Foo
+ * @namespace http://mytest/xsd1 namespace of the class Foo
+ */
+class Foo
+{
+    /**
+     * @var string $foo1
+     */
+    public $foo1;
+    /**
+     * @var string $foo2
+     * maps to xs:QName
+     */
+    public $foo2;
+
+    /**
+     * @var array of string $foo3
+     * maps to xs:dateTime
+     */
+    public $foo3;
+
+    /**
+     * @var object Bay $foo4
+     */
+    public $foo4;
+}
+
+/**
+ * classname Bay
+ * @namespace http://mytest/xsd2 namespace of the class Bay
+ */
+class Bay
+{
+    /**
+     * @var array of string $bar1
+     */
+    public $bar1;
+
+    /**
+     * @var array of object Boo $bar2
+     */
+    public $bar2;
+}
+
+/**
+ * classname Boo
+ * @namespace http://mytest/xsd1
+ */
+class Boo
+{
+    /**
+     * @var array of string $boo1
+     */
+    public $boo1;
+
+    /**
+     * @var array of string $boo2
+     * maps to xs:duration
+     */
+    public $boo2;
+}
+
+class echoStringResponse
+{
+    public $ret_value;
+
+    public function __construct($param_value)
+    {
+        $this->ret_value = $param_value;
+    }
+}
+
+
+$operations = array("echoString" => "echoValue");
+$opParams = array("echoValue" => "MIXED");
+
+$classmap = array("Foo" => "Foo", "Bay" => "Bay");
+
+$service = new WSService(array("classes" => 
+                            array("Bar" => 
+                                array("operations" => $operations,
+                                      "args" => array("Welcome", "Bye!"))),
+                               "bindingStyle"=> "doclit",
+                               /* "bindingStyle"=> "rpc-enc",
+                               "classmap" => $classmap, */
+                               "opParams" => $opParams));
+        
+$service->reply();
+
+?>
