57903d6fd1e9e04368dca41713e1b155caa929a6
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2008-07-04 05:31:58 +0000

    * Ditching ModPerl getting request info from CGI
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18848 a5903396-d722-0410-b921-86c7d4935375

--- perl/service/lib/WSO2/WSF/WSService.pm
+++ perl/service/lib/WSO2/WSF/WSService.pm
@@ -66,12 +66,13 @@ sub fill_svc_info {
     # generating svc_name replacing / with |
     # for example /axis2/services/echo_services.pl becomes
     # |axis2|services|echo_services.pl
-    my $svc_name = $request->uri();
+    my $svc_name = $request->script_name();
     $svc_name =~ s#/#|#g;
     WSO2::WSF::Serverc::wsf_svc_info_t_svc_name_set($self->{-svc_info}, $svc_name);
     WSO2::WSF::Serverc::wsf_svc_info_t_msg_recv_set($self->{-svc_info}, $self->{-msg_recv});
 
-    WSO2::WSF::Serverc::wsf_svc_info_t_script_filename_set($self->{-svc_info}, $request->filename());
+    # WSO2::WSF::Serverc::wsf_svc_info_t_script_filename_set($self->{-svc_info}, $request->filename());
+    WSO2::WSF::Serverc::wsf_svc_info_t_script_filename_set($self->{-svc_info}, "/var/www" . $request->url(-absolute => 1));
 
     # setting perl worker for svc_info structure because that is essential
     # for creating svc from svc_info.
@@ -108,41 +109,76 @@ sub reply {
 
     # getting a type error from swig saying the 2nd argument should be a char *
     # so concaternating with an empty string
-    my $content_type = "" . $request->headers_in->{'content-type'};
+#    my $content_type = "" . $request->headers_in->{'content-type'};
+    my $content_type = "" . $request->content_type();
 
-    WSO2::WSF::Serverc::wsf_req_info_t_content_encoding_set($req_info, $request->content_encoding());
+#    WSO2::WSF::Serverc::wsf_req_info_t_content_encoding_set($req_info, $request->content_encoding());
+    WSO2::WSF::Serverc::wsf_req_info_t_content_encoding_set($req_info, $content_type);
+
+#    WSO2::WSF::Serverc::wsf_req_info_t_content_type_set($req_info, $content_type);
     WSO2::WSF::Serverc::wsf_req_info_t_content_type_set($req_info, $content_type);
-    WSO2::WSF::Serverc::wsf_req_info_t_content_length_set($req_info, $request->headers_in->{'content-length'});
-    WSO2::WSF::Serverc::wsf_req_info_t_svr_name_set($req_info, $request->hostname());
 
-    my @svrinfo = split(/:/, $request->headers_in->{'host'}); # This will have servername and the port number
-    WSO2::WSF::Serverc::wsf_req_info_t_svr_port_set($req_info, $svrinfo[1]); # getting the port number
+#    WSO2::WSF::Serverc::wsf_req_info_t_content_length_set($req_info, $request->headers_in->{'content-length'});
+    WSO2::WSF::Serverc::wsf_req_info_t_content_length_set($req_info, $ENV{'CONTENT_LENGTH'});
 
-    WSO2::WSF::Serverc::wsf_req_info_t_http_protocol_set($req_info, $request->protocol());
+#    WSO2::WSF::Serverc::wsf_req_info_t_svr_name_set($req_info, $request->hostname());
+    WSO2::WSF::Serverc::wsf_req_info_t_svr_name_set($req_info, $request->server_name());
 
-    if (defined $request->headers_in->{'soapaction'}) {
-        WSO2::WSF::Serverc::wsf_req_info_t_soap_action_set($req_info, $request->headers_in->{'soapaction'});
+#    my @svrinfo = split(/:/, $request->headers_in->{'host'}); # This will have servername and the port number
+#    WSO2::WSF::Serverc::wsf_req_info_t_svr_port_set($req_info, $svrinfo[1]); # getting the port number
+    WSO2::WSF::Serverc::wsf_req_info_t_svr_port_set($req_info, $request->server_port()); # getting the port number
+
+#    WSO2::WSF::Serverc::wsf_req_info_t_http_protocol_set($req_info, $request->protocol());
+    WSO2::WSF::Serverc::wsf_req_info_t_http_protocol_set($req_info, "http");
+
+#    if (defined $request->headers_in->{'soapaction'}) {
+#        WSO2::WSF::Serverc::wsf_req_info_t_soap_action_set($req_info, $request->headers_in->{'soapaction'});
+#    }
+    if (defined $request->param('SOAPACTION')) {
+        WSO2::WSF::Serverc::wsf_req_info_t_soap_action_set($req_info, $request->param('SOAPACTION'));
     }
 
-    WSO2::WSF::Serverc::wsf_req_info_t_request_method_set($req_info, $request->method());
-    WSO2::WSF::Serverc::wsf_req_info_t_request_uri_set($req_info, $request->uri());
-    WSO2::WSF::Serverc::wsf_req_info_t_query_string_set($req_info, $request->args());
+#    WSO2::WSF::Serverc::wsf_req_info_t_request_method_set($req_info, $request->method());
+    WSO2::WSF::Serverc::wsf_req_info_t_request_method_set($req_info, $request->request_method());
 
-    my $postdata;
-    $request->read($postdata, $request->headers_in->{'content-length'});
+#    WSO2::WSF::Serverc::wsf_req_info_t_request_uri_set($req_info, $request->uri());
+    WSO2::WSF::Serverc::wsf_req_info_t_request_uri_set($req_info, $request->script_name());
+
+#    WSO2::WSF::Serverc::wsf_req_info_t_query_string_set($req_info, $request->args());
+    WSO2::WSF::Serverc::wsf_req_info_t_query_string_set($req_info, $request->query_string());
+
+    my $postdata = $request->param('POSTDATA');
+#    $request->read($postdata, $request->headers_in->{'content-length'});
     WSO2::WSF::Serverc::wsf_req_info_t_req_data_set($req_info, $postdata);
 
-    WSO2::WSF::Serverc::wsf_req_info_t_req_data_length_set($req_info, $request->headers_in->{'content-length'});
+#    WSO2::WSF::Serverc::wsf_req_info_t_req_data_length_set($req_info, $request->headers_in->{'content-length'});
+    WSO2::WSF::Serverc::wsf_req_info_t_req_data_length_set($req_info, $ENV{'CONTENT_LENGTH'});
 
     #invoking worker
     my $status = WSO2::WSF::Serverc::wsf_worker_process_request($self->{-worker}, $self->{-env}, $req_info, $self->{-svc_info});
-
     # WSO2::WSF::C::axis2_log_debug( $env, "****** $status" );
-    # $request->status($status);
-    # my $respayload = WSO2::WSF::Serverc::wsf_req_info_t_result_payload_get($req_info);
-    my $respayload = WSO2::WSF::Server::wsf_get_request_info_payload($req_info);
 
-    $request->content_type( 'application/soap+xml;charset=UTF-8' );
+    my $respayload = WSO2::WSF::Serverc::wsf_req_info_t_result_payload_get($req_info);
+    # my $respayload = WSO2::WSF::Server::wsf_get_request_info_payload($req_info);
+
+    # content type
+    my $ct = WSO2::WSF::Serverc::wsf_req_info_t_out_content_type_get($req_info);
+    # content length
+    my $cl = WSO2::WSF::Serverc::wsf_req_info_t_result_length_get($req_info);
+
+    my $status_line;
+
+    if ($status eq WSO2::WSF::C::WS_HTTP_ACCEPTED) {
+        $status_line = "HTTP 202 Accepted";
+        $cl = 0;
+    } elsif ($status eq WSO2::WSF::C::WS_HTTP_OK) {
+        $status_line = "HTTP 200 OK";
+    } elsif ($status eq WSO2::WSF::C::WS_HTTP_INTERNAL_SERVER_ERROR) {
+        $status_line = "HTTP 500 Internal Server Error";
+    }
+
+#    $request->status($status);
+    print $request->header( -status => $status, -Content-type => $ct, -Content-length => $cl );
     print $respayload;
 }
 
