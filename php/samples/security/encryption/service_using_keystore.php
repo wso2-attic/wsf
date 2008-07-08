7116cc92b2e431bfefa9660188a5b6bf2c70d340
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-07-08 05:38:22 +0000

    service that uses pkcs12 keystore added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@18938 a5903396-d722-0410-b921-86c7d4935375

---
+++ php/samples/security/encryption/service_using_keystore.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright 2005,2008 WSO2, Inc. http://wso2.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function echoFunction($inMessage) {
+
+    $returnMessage = new WSMessage($inMessage->str);
+
+    return $returnMessage;
+}
+
+$keystore = file_get_contents("../keys/bob_kstore.p12");
+
+$operations = array("echoString" => "echoFunction");
+$sec_array = array("encrypt" => TRUE,
+                   "algorithmSuite" => "Basic256Rsa15",
+                   "securityTokenReference" => "IssuerSerial");
+
+$actions = array("http://php.axis2.org/samples/echoString" => "echoString");
+ 
+$policy = new WSPolicy(array("security"=> $sec_array));
+
+$sec_token = new WSSecurityToken(array("PKCS12KeyStore" => $keystore,
+				       "user"=>"b",
+                                       "password"=>"b12345"));
+
+$svr = new WSService(array("actions" => $actions,
+                           "operations" => $operations,
+                           "policy" => $policy,
+                           "securityToken" => $sec_token));
+        
+$svr->reply();
+
+?>
