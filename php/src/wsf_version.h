78dbdc3126bbc1a9e872dee307c0a0765e8a4f5d
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2008-07-15 07:00:32 +0000

    version number updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@19290 a5903396-d722-0410-b921-86c7d4935375

--- php/src/wsf_version.h
+++ php/src/wsf_version.h
@@ -20,16 +20,16 @@
 #define WSF_VERSION_H
 
 /** major version */
-#define WSF_MAJOR_VERSION 1
+#define WSF_MAJOR_VERSION 2
 
 /** minor version */
-#define WSF_MINOR_VERSION 3
+#define WSF_MINOR_VERSION 0
 
 /** patch level */ 
-#define WSF_PATCH_VERSION 2
+#define WSF_PATCH_VERSION 0
 
 /** The formatted string of WSF version */
-#define WSF_VERSION_STRING "1.3.2"
+#define WSF_VERSION_STRING "2.0.0"
 
 /* macro for Win32 .rc files using numeric csv representation */
 #define WSF_VERSION_STRING_CSV WSF_MAJOR_VERSION ##, \
