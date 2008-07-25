6b7f76bffb51f0bc0b2dc081bcf42f1d8a7b1ded
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2008-07-25 02:31:32 +0000

    Updated build.sh to fix build issue
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@19965 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build.sh
+++ cpp/build.sh
@@ -4,7 +4,13 @@ set -e
 cp build/wsf_c/rampartc/configure.ac wsf_c/rampartc/configure.ac
 cp build/wsf_c/Makefile.am wsf_c/
 cp build/wsf_c/rampartc/src/Makefile.am  wsf_c/rampartc/src/Makefile.am
+cp build/wsf_c/rampartc/src/omxmlsec/Makefile.am  wsf_c/rampartc/src/omxmlsec
+cp build/wsf_c/rampartc/src/omxmlsec/tokens/Makefile.am  wsf_c/rampartc/src/omxmlsec/tokens/
+cp build/wsf_c/rampartc/src/omxmlsec/openssl/Makefile.am  wsf_c/rampartc/src/omxmlsec/openssl
 cp build/wsf_c/rampartc/src/core/Makefile.am  wsf_c/rampartc/src/core/Makefile.am
+cp build/wsf_c/rampartc/src/trust/Makefile.am  wsf_c/rampartc/src/trust
+cp build/wsf_c/rampartc/src/secconv/Makefile.am  wsf_c/rampartc/src/secconv
+cp build/wsf_c/rampartc/src/rahas/Makefile.am  wsf_c/rampartc/src/rahas
 cp build/wsf_c/axis2c/neethi/configure.ac wsf_c/axis2c/neethi/configure.ac
 cp build/wsf_c/axis2c/neethi/src/Makefile.am wsf_c/axis2c/neethi/src/Makefile.am
 
