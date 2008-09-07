2cf04c5d45f0737db03417ae3cef5e9a299ca11f
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2008-09-07 09:25:21 +0000

    removing the unnecceosry file
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@21540 a5903396-d722-0410-b921-86c7d4935375

--- php/samples/DataServices/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("DB_USERNAME", "dimuthu");
-define("DB_USER", "dimuthu");
-define("DB_PASSWORD", "believeitsme");
-define("DB_NAME", "wordpress");
-define("DB_HOST", "localhost");
-define("WP_TABLE_PREFIX", "wp_");
-?>
