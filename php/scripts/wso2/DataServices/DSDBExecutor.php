b140943511a7d18b620394ec8819130830cd06c8
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2008-09-08 02:33:46 +0000

    adding support for sqlite
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@21548 a5903396-d722-0410-b921-86c7d4935375

--- php/scripts/wso2/DataServices/DSDBExecutor.php
+++ php/scripts/wso2/DataServices/DSDBExecutor.php
@@ -4,66 +4,72 @@
  * for database execution
  */
 class DSDBExecutor {
-    private $dns;
+    private $dsn;
     private $username;
     private $password;
 
     public function __construct($config) {
-        $dns = "";
+        $dsn = "";
         // setting db engine
         if($config == NULL || !is_array($config)) {
             DSUtils::error_log(__FILE__, __LINE__, 
                     "Missing configuraiton: config is not provided");
         }
         else {
-            if(array_key_exists(DS_DB_ENGINE, $config) &&
-                $config[DS_DB_ENGINE] != NULL) {
-                $dns .= $config[DS_DB_ENGINE]. ":";
-            }
-            else {
-                DSUtils::error_log(__FILE__, __LINE__, 
-                        "Missing configuraiton: database engine is not provided");
-            }
-
-            // setting db name
-            if(array_key_exists(DS_DBNAME, $config) &&
-                $config[DS_DBNAME] != NULL) {
-                $dns .= DS_DBNAME. "=". $config[DS_DBNAME]. ";";
-            }
-            else {
-                DSUtils::error_log(__FILE__, __LINE__, 
-                        "Missing configuraiton: database name is not provided");
-            }
-
-            // setting db name
-            if(array_key_exists(DS_DBHOST, $config) &&
-                $config[DS_DBHOST] != NULL) {
-                $dns .= DS_HOST. "=". $config[DS_DBHOST];
-            }
-            else {
-                DSUtils::error_log(__FILE__, __LINE__, 
-                        "Missing configuraiton: database host is not provided");
-            }
-
-            // setting db port
-            if(array_key_exists(DS_DBPORT, $config) &&
-                $config[DS_DBPORT] != NULL) {
-                $dns .= ":". $config[DS_DBPORT];
-            }
 
             
             // user can give the dsn directly
             if(array_key_exists(DS_DSN, $config) &&
                 $config[DS_DSN] != NULL) {
-                $dns = $config[DS_DSN];
+                $dsn = $config[DS_DSN];
             }
+            else {
+                if(array_key_exists(DS_DB_ENGINE, $config) &&
+                    $config[DS_DB_ENGINE] != NULL) {
+                    $dsn .= $config[DS_DB_ENGINE]. ":";
+                }
+                else {
+                    DSUtils::error_log(__FILE__, __LINE__, 
+                            "Missing configuraiton: database engine is not provided");
+                }
 
-            $this->dns = $dns;
+                // setting db name
+                if(array_key_exists(DS_DBNAME, $config) &&
+                    $config[DS_DBNAME] != NULL) {
+                    $dsn .= DS_DBNAME. "=". $config[DS_DBNAME]. ";";
+                }
+                else {
+                    DSUtils::error_log(__FILE__, __LINE__, 
+                            "Missing configuraiton: database name is not provided");
+                }
+
+                // setting db name
+                if(array_key_exists(DS_DBHOST, $config) &&
+                    $config[DS_DBHOST] != NULL) {
+                    $dsn .= DS_HOST. "=". $config[DS_DBHOST];
+                }
+                else {
+                    DSUtils::error_log(__FILE__, __LINE__, 
+                            "Missing configuraiton: database host is not provided");
+                }
+
+                // setting db port
+                if(array_key_exists(DS_DBPORT, $config) &&
+                    $config[DS_DBPORT] != NULL) {
+                    $dsn .= ":". $config[DS_DBPORT];
+                }
+
+            }
+            $this->dsn = $dsn;
 
             // setting username
             if(array_key_exists(DS_USERNAME, $config) &&
                 $config[DS_USERNAME] != NULL) {
                 $this->username = $config[DS_USERNAME];
+            }
+            else if(array_key_exists(DS_DSN, $config) &&
+                $config[DS_DSN] != NULL) {
+                //user name can be in the dsn..
             }
             else {
                 DSUtils::error_log(__FILE__, __LINE__, 
@@ -111,11 +117,11 @@ class DSDBExecutor {
      */
     private function db_connect() {
         try {
-            $dbh = new PDO($this->dns, $this->username, $this->password);
+            $dbh = new PDO($this->dsn, $this->username, $this->password);
         }
         catch (PDOException $e) {
             DSUtils::error_log(__FILE__, __LINE__, "Database connection failed: ".$e->getMessage().
-                    "".$this->dns. ", ".$this->username);
+                    "".$this->dsn. ", ".$this->username);
             throw new WSFault("Sender", $e->getMessage());
   
         }
