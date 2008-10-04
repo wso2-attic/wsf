3bd9c052cd6fe192cc7b01620c134a3732c47a66
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2008-10-04 04:02:27 +0000

    updating the samples
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@22232 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/samples/sec_echo_with_policy.sh
+++ c/wsclient/samples/sec_echo_with_policy.sh
@@ -3,4 +3,38 @@ if test -z $WSFC_HOME; then
     WSFC_HOME=$PWD/../../..
 fi
 
+INST_DIR=$AXIS2C_HOME
+SERVICE_HOME="$INST_DIR/services/sec_echo"
+
+_SMPL_DIR="$PWD"
+
+if [ $# -ne 1 ]
+then
+    echo "Usage : $0 server-port"
+    exit
+fi
+
+    _PORT=$1
+    echo "-------------------------------------------------------------------------"
+    echo "Deploying server with required sec_policy for sec_echo service"
+
+    echo "Replacing sec_echo/services.xml"
+    if [ `uname -s` = Darwin ]
+    then
+        sed -e 's,AXIS2C_HOME,'$INST_DIR',g' -e 's,\.so,\.dylib,g' data/sec_echo_services.xml > $SERVICE_HOME/services.xml
+    else
+        sed 's,AXIS2C_HOME,'$INST_DIR',g' data/sec_echo_services.xml > $SERVICE_HOME/services.xml
+    fi
+
+    killall axis2_http_server
+    cd $AXIS2C_HOME/bin
+    echo "Start server @ $_PORT"
+    ./axis2_http_server -p$_PORT &
+    sleep 2
+    cd $_SMPL_DIR
+    echo "Run the sample"
+
 $WSFC_HOME/bin/wsclient --soap --no-mtom --user alice --digest --password password --key /axis2c/deploy/bin/samples/rampart/keys/ahome/alice_key.pem --certificate /axis2c/deploy/bin/samples/rampart/keys/ahome/alice_cert.cert --recipient-certificate /axis2c/deploy/bin/samples/rampart/keys/ahome/bob_cert.cert --policy-file $WSFC_HOME/bin/samples/wsclient/data/policy.xml http://localhost:9090/axis2/services/sec_echo <$WSFC_HOME/bin/samples/wsclient/data/echo.xml
+
+killall axis2_http_server
+echo "DONE"
