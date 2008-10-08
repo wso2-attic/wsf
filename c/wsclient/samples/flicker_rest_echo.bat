55c703fd0ffeca2d3a8dca4c7e243c7650a3832a
shankar <shankar@a5903396-d722-0410-b921-86c7d4935375>
2008-10-08 11:04:03 +0000

    fixing flickr rest end point reference.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@22324 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/samples/flicker_rest_echo.bat
+++ c/wsclient/samples/flicker_rest_echo.bat
@@ -6,6 +6,6 @@ IF "%1" == "" (
 )
 
 echo ^<x:FlickrRequest xmlns:x='urn:flickr'^>^<method^>flickr.test.echo^</method^>^<api_key^>%1^</api_key^>^<name^>wsclient^</name^>^</x:FlickrRequest^>  > flicker_test.xml
-"%WSFC_HOME%\bin\wsclient" --get  http://api.flickr.com/services/rest/?method=flickr.test.echo < flicker_test.xml
+"%WSFC_HOME%\bin\wsclient" --get  http://api.flickr.com/services/rest/ < flicker_test.xml
 
 :end
