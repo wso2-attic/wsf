86abdfe1124827d1cfcc2da3ee37875626ea87eb
chintana <chintana@a5903396-d722-0410-b921-86c7d4935375>
2009-01-12 06:24:06 +0000

    Checking WSFC_HOME and exiting without calling get_wsfc_home.rb.
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@28694 a5903396-d722-0410-b921-86c7d4935375

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
-wsfc_home=$(./get_wsfc_home.rb)
+# first check the env for WSFC_HOME, then the standard location, if both
+# fails, crap out. Cannot continue without WSF/C
+if [ "${WSFC_HOME}" != "" ]
+then
+    wsfc_home=$WSFC_HOME
+elif [ -a "/opt/wso2/wsf_c" ]
+then
+    wsfc_home="/opt/wso2/wsf_c"
+else
+    echo "
+Error: Cannot find WSF/C installation.
+
+If you have installed WSF/C to a non-standard location please set WSFC_HOME
+to where you have installed it. If you haven't installed WSF/C please install
+WSF/C before installing WSF/Ruby.
+"
+    exit -1
+fi
 
 cd wsdlc
 sh autogen.sh
