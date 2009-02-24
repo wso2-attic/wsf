f5125eab72fe3fc994df59d68ddf2c5bd880ef5b
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-02-24 18:44:11 +0000

    license added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@31457 a5903396-d722-0410-b921-86c7d4935375

--- cpp/examples/clients/security/sec_echo/echo.cpp
+++ cpp/examples/clients/security/sec_echo/echo.cpp
@@ -1,3 +1,20 @@
+/*
+* Copyright 2005,2009 WSO2, Inc. http://wso2.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
 #include <stdio.h>
 #include <WSSOAPClient.h>
 #include <OMElement.h>
@@ -54,15 +71,6 @@ int main(int argc, char *argv[])
 
 	sc = new WSSOAPClient(client_repo, end_point);
     cout << endl << "Using end_point: " << end_point << endl;
-    try
-    {
-        sc->initializeClient("echo.log", AXIS2_LOG_LEVEL_DEBUG);
-    }
-    catch (AxisFault & e)
-    {
-        cout << endl << "Error: " << e << endl;
-        return 0; 
-    }
     sc->engageModule(AXIS2_MODULE_ADDRESSING);
 	sc->engageModule("rampart");
 	
