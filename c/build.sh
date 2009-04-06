f2b569b9cb4441db177c4ee22c3d754df0c04696
sanjaya <sanjaya@a5903396-d722-0410-b921-86c7d4935375>
2009-04-06 14:16:17 +0000

    Changed build.sh to fix build break in savan/c
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@33313 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -8,7 +8,7 @@ export WSFC_HOME=`pwd`/deploy
 ./build/copy_build_files.sh
 
 ./autogen.sh
-./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp
+./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/include/apache2 --with-apr=/usr/include/apr-1.0 --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-xpath=/usr/include/libxml2/libxml
 make -j 10
 make install
 make samples -j 10
