0ebf3590faad1898418d435e78d0659aef26e3e5
dimuthu <dimuthu@a5903396-d722-0410-b921-86c7d4935375>
2009-04-17 08:36:24 +0000

    updating readme for new installation method
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@33683 a5903396-d722-0410-b921-86c7d4935375

--- ruby/extconf.rb
+++ ruby/extconf.rb
@@ -5,41 +5,41 @@ require 'rbconfig'
 require 'fileutils'
 
 # Check the configurations
-WSFC_HOME = '/opt/wso2/wsf_c'
+wsfc_Home = '/opt/wso2/wsf_c'
 
 # recording the dir we're at
 pwd = FileUtils.pwd()
 
 # check for the environment variable
 if not ENV['WSFC_HOME'].nil?
-  WSFC_HOME = ENV['WSFC_HOME']
+  wsfc_Home = ENV['WSFC_HOME']
 else
   begin
-    FileUtils.cd(WSFC_HOME)
+    FileUtils.cd(wsfc_Home)
     # ah, we came here, so the standard directory exists
     # let's go back then
     FileUtils.cd(pwd)
   rescue Exception => e
     puts <<E
 WSF/C not found. If you have installed WSF/C into a non standard location
-please set WSFC_HOME environment variable to where you have installed it.
+please set wsfc_Home environment variable to where you have installed it.
 E
     exit -1
   end
 end
-puts "Using WSF/C installed in #{WSFC_HOME}"
+puts "Using WSF/C installed in #{wsfc_Home}"
 
 dir_config('wsdlc', './wsdlc/include', './wsdlc/lib')
 
 # Add Build Rules
 if /mswin32|bccwin32/ =~ RUBY_PLATFORM
-    dir_config('WSFC', WSFC_HOME + '/include', WSFC_HOME + '/lib')
+    dir_config('WSFC', wsfc_Home + '/include', wsfc_Home + '/lib')
     $CFLAGS = $CFLAGS + " -DWIN32 -DSWIG_NOINCLUDE"
     have_library('libxml2')
     have_library('axiom')
 else
-    dir_config('WSFC', WSFC_HOME + '/include/axis2-1.4.0', WSFC_HOME + '/lib')
-    dir_config('Rampart', WSFC_HOME + '/include/rampart-1.2.0', WSFC_HOME + '/modules/rampart')
+    dir_config('WSFC', wsfc_Home + '/include/axis2-1.4.0', wsfc_Home + '/lib')
+    dir_config('Rampart', wsfc_Home + '/include/rampart-1.2.0', wsfc_Home + '/modules/rampart')
     have_library('axis2_axiom')
 end
 
