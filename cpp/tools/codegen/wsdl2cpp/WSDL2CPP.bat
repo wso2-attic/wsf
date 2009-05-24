076b095230e3e0aa6d8a40f814e848920de81153
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-05-24 17:08:08 +0000

    scripts added for codegen
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@36643 a5903396-d722-0410-b921-86c7d4935375

---
+++ cpp/tools/codegen/wsdl2cpp/WSDL2CPP.bat
@@ -0,0 +1,9 @@
+echo off
+setlocal EnableDelayedExpansion
+set CODEGEN_HOME=%CD%\..\lib
+set CODEGEN_CLASSPATH=%CODEGEN_HOME%
+FOR %%c in ("%CODEGEN_HOME%\lib\*.jar") DO set CODEGEN_CLASSPATH=!CODEGEN_CLASSPATH!;%%c;
+
+java -classpath %CODEGEN_CLASSPATH% org.apache.axis2.wsdl.WSDL2C %*
+
+
---
+++ cpp/tools/codegen/wsdl2cpp/WSDL2CPP.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export CODEGEN_HOME='pwd'/../lib
+for f in $CODEGEN_HOME/lib/*.jar
+do
+  CODEGEN_CLASSPATH=$CODEGEN_CLASSPATH:$f
+done
+export CODEGEN_CLASSPATH
+
+echo the classpath $CODEGEN_CLASSPATH
+java -classpath $CODEGEN_CLASSPATH org.apache.axis2.wsdl.WSDL2C $*
