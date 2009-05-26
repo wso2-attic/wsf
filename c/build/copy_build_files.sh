44cb95ecf3866015b239a4241ec4d26b6dbcdfd4
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-05-26 09:12:27 +0000

    file coping updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@36852 a5903396-d722-0410-b921-86c7d4935375

--- c/build/copy_build_files.sh
+++ c/build/copy_build_files.sh
@@ -17,7 +17,8 @@ cp build/axis2c/util/src/stomp/*.h axis2c/util/include
 cp -rf build/axis2c/util/src/stomp  axis2c/util/src/ 
 #end copying for stomp
 
-cp build/rampartc/Makefile.am rampartc
+#cp build/rampartc/Makefile.am rampartc
+#cp build/rampartc/src/Makefile.am rampartc/src
 cp build/rampartc/src/core/Makefile.am rampartc/src/core
 cp build/rampartc/src/util/Makefile.am rampartc/src/util
 cp build/rampartc/src/omxmlsec/Makefile.am rampartc/src/omxmlsec
@@ -27,7 +28,6 @@ cp build/rampartc/src/omxmlsec/openssl/Makefile.am rampartc/src/omxmlsec/openssl
 cp build/rampartc/src/omxmlsec/tokens/Makefile.am rampartc/src/omxmlsec/tokens
 cp build/rampartc/src/secconv/Makefile.am rampartc/src/secconv
 cp build/rampartc/src/trust/Makefile.am rampartc/src/trust
-cp build/rampartc/src/Makefile.am rampartc/src
 cp build/sandesha2c/configure.ac sandesha2c
 cp build/sandesha2c/Makefile.am sandesha2c
 cp build/axis2c/src/core/engine/Makefile.am axis2c/src/core/engine/
