ba1cf9c558ebc6cd4b131357b280b0153476941d
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-05 09:47:51 +0000

    build.sh updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@37883 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build.sh
+++ cpp/build.sh
@@ -2,7 +2,6 @@
 set -e
 
 cp wsf_c/build/rampartc/configure.ac wsf_c/rampartc/configure.ac
-cp build/wsf_c/Makefile.am wsf_c/
 cp wsf_c/build/rampartc/src/Makefile.am  wsf_c/rampartc/src/Makefile.am
 cp wsf_c/build/rampartc/src/omxmlsec/Makefile.am  wsf_c/rampartc/src/omxmlsec
 cp wsf_c/build/rampartc/src/omxmlsec/tokens/Makefile.am  wsf_c/rampartc/src/omxmlsec/tokens/
@@ -14,7 +13,7 @@ cp wsf_c/build/rampartc/src/rahas/Makefile.am  wsf_c/rampartc/src/rahas/
 cp wsf_c/build/rampartc/src/util/Makefile.am wsf_c/rampartc/src/util/
 cp build/wsf_c/axis2c/neethi/configure.ac wsf_c/axis2c/neethi/configure.ac
 cp build/wsf_c/axis2c/neethi/src/Makefile.am wsf_c/axis2c/neethi/src/Makefile.am
-
+cp build/wsf_c/rampartc/Makefile.am wsf_c/rampartc/Makefile.am
 
 sh autogen.sh
 
