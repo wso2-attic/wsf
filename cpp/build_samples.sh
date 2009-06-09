92b94c477da3d20742590809e2ff9b29d3b251f1
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-09 12:09:15 +0000

    build_samples file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38317 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build_samples.sh
+++ cpp/build_samples.sh
@@ -8,16 +8,28 @@ make
 make install
 cd ..
 
-cd wsf_c/axis2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.6.0; make; make install ;
-if test -e ../../sandesha2c/Makefile; then
-cd ../../sandesha2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.6.0; make; make install; 
-fi
+
+if ! test -d $1/samples/src/savanc; then 
+    mkdir $1/samples/src/savanc;
+    cp -r wsf_c/savanc/samples/* $1/samples/src/savanc;
+fi    
+
+if ! test -d $1/samples/src/sandesha2c; then 
+    mkdir $1/samples/src/sandesha2c;
+    cp -r wsf_c/sandesha2c/samples/* $1/samples/src/sandesha2c;
+fi    
+if ! test -d $1/samples/src/rampartc; then 
+    mkdir $1/samples/src/rampartc;
+    cp -r wsf_c/rampartc/samples/* $1/samples/src/rampartc;
+fi    
+if ! test -d $1/samples/src/wsclient; then 
+    mkdir $1/samples/src/wsclient;
+    cp -r wsf_c/wsclient/samples/* $1/samples/src/wsclient;
+
+cd wsf_c/axis2c/samples
 if test -e ../../rampartc/Makefile; then
 cd ../../rampartc/samples; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.6.0; make; make install;
-#cd client/sec_echo; cd ../../secpolicy; sh deploy.sh scenario5; cd ../
-fi
-if test -e ../../savanc/Makefile; then
-cd ../../savanc/samples; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.6.0 --with-savan=$1/include/savan-1.0; make; make install;
+cd client/sec_echo; cd ../../secpolicy; sh deploy.sh scenario5; cd ../
 fi
 cd ../../examples; ./configure --prefix=$1  --with-wsfc=$1/include; make; make install
 cd ..
