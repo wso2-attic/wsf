b5b215edd933bf8bd818fafbb4d98eaa8a5c3a14
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-09 06:39:48 +0000

    build_samples.sh updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38239 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build_samples.sh
+++ cpp/build_samples.sh
@@ -2,8 +2,23 @@
 set -e
 export LD_LIBRARY_PATH=$1/lib; 
 
-
 cd examples
 ./configure --prefix=$1
 make
+make install
 cd ..
+
+cd wsf_c/axis2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.6.0; make; make install ;
+if test -e ../../sandesha2c/Makefile; then
+cd ../../sandesha2c/samples; ./configure --prefix=$1 --with-axis2=$1/include/axis2-1.6.0; make; make install; 
+fi
+if test -e ../../rampartc/Makefile; then
+cd ../../rampartc/samples; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.6.0; make; make install;
+#cd client/sec_echo; cd ../../secpolicy; sh deploy.sh scenario5; cd ../
+fi
+if test -e ../../savanc/Makefile; then
+cd ../../savanc/samples; ./configure --prefix=$1  --with-axis2=$1/include/axis2-1.6.0 --with-savan=$1/include/savan-1.0; make; make install;
+fi
+cd ../../examples; ./configure --prefix=$1  --with-wsfc=$1/include; make; make install
+cd ..
+
