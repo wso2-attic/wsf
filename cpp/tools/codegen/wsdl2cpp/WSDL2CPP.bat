e02fe8f76324d9ebf7e7e993e1954adf2cb66f7d
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-10 08:11:25 +0000

    wsdl2cpp bat file corrected
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38456 a5903396-d722-0410-b921-86c7d4935375

--- cpp/tools/codegen/wsdl2cpp/WSDL2CPP.bat
+++ cpp/tools/codegen/wsdl2cpp/WSDL2CPP.bat
@@ -4,6 +4,6 @@ set CODEGEN_HOME=%CD%\..\lib
 set CODEGEN_CLASSPATH=%CODEGEN_HOME%
 FOR %%c in ("%CODEGEN_HOME%\lib\*.jar") DO set CODEGEN_CLASSPATH=!CODEGEN_CLASSPATH!;%%c;
 
-java -classpath %CODEGEN_CLASSPATH% org.apache.axis2.wsdl.WSDL2C %*
+java -classpath %CODEGEN_CLASSPATH% org.apache.axis2.wsdl.WSDL2CPP %*
 
 
