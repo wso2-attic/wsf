9e2030a9b32bf6d7038da02da48fe65e89233691
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-10 08:08:01 +0000

    build codegen script updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38451 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build_codegen.sh
+++ cpp/build_codegen.sh
@@ -8,10 +8,10 @@ mvn clean install -Drelease
 cd modules/distribution/target/ 
 unzip wsf-cpp-1.0-bin.zip
 cd wsf-cpp-1.0
-if [ -a $DIL/bin/codegen ] ; then
-	cp -rf lib $DIL/bin/codegen
+if [ -a $DIL/bin/tools/codegen ] ; then
+	cp -rf lib $DIL/bin/tools/codegen
 else
-	mkdir  $DIL/bin/codegen
-	cp -rf lib $DIL/bin/codegen
+	mkdir  $DIL/bin/tools/codegen
+	cp -rf lib $DIL/bin/tools/codegen
 fi
 
