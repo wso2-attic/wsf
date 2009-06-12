fd8a89561f1dc4d785a3b12b34d464b647754e99
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-12 11:53:42 +0000

    build_codegen.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38788 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build_codegen.bat
+++ cpp/build_codegen.bat
@@ -1,22 +1,7 @@
 echo off
 
-call init.bat
-
-set DIL=%CD%\wso2-wsf-cpp-bin-%WSFCPP_VERSION%-win32
-
 cd wsf_c\build\codegen
 
 mvn clean install -Drelease
 
-cd modules\distribution\target\ 
-
-unzip wsf-cpp-1.0-bin.zip
-
-cd wsf-cpp-1.0
-
-if not exist %DIL%\bin\tools\codegen mkdir  %DIL%\bin\tools\codegen
-
-xcopy lib %DIL%\bin\tools\codegen
-
-cd %DIL%
-
+exit
