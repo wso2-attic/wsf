d76df3ac1e8a60c52dbc4cf4f7bd7ec27d097448
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-06-15 08:45:58 +0000

    build_codegen.sh file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@38935 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build_codegen.sh
+++ cpp/build_codegen.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/sh
 set -e
 
-export DIL=`pwd`/deploy
-
-cd wsf_c/build/codegen
-mvn clean install -Drelease
-cd modules/distribution/target/ 
-unzip wsf-cpp-1.0-bin.zip
-cd wsf-cpp-1.0
-if [ -a $DIL/bin/tools/codegen ] ; then
-	cp -rf lib $DIL/bin/tools/codegen
+if test ! -z $1; then
+export DIL=`pwd`/$1
 else
-	mkdir  $DIL/bin/tools/codegen
-	cp -rf lib $DIL/bin/tools/codegen
+export DIL=`pwd`/deploy
 fi
 
+
+cd wsf_c/build/codegen
+#mvn clean install -Drelease
+cd modules/distribution/target/ 
+#unzip wsf-cpp-1.0-bin.zip
+cd wsf-cpp-1.0
+if [ -a $DIL/bin/codegen ] ; then
+	cp -rf lib $DIL/bin/codegen
+else
+	if test ! -a $DIL/bin  ; then 
+		mkdir $DIL/bin
+	fi
+	if test ! -a $DIL/bin/codegen ; then
+		mkdir $DIL/bin/codegen
+	fi
+	cp -rf lib $DIL/bin/codegen/lib
+fi
+
+cd ../../../../../../../
+cp -rf `pwd`/tools/codegen/wsdl2cpp $DIL/bin/codegen/wsdl2cpp
