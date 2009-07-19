adcadb1a094129df33b65d731832977fc313d1a6
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-07-19 09:51:42 +0000

    script added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@41462 a5903396-d722-0410-b921-86c7d4935375

---
+++ cpp/examples/secscripts/update_n_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#If your client repository is different, change the value.
+CLIENT_REPO="$AXIS2C_HOME/client_repo"
+
+#INSTALL MODULE to make sure that both server and client have the same module.
+echo "Copying latest module to client_repo"
+cp -r $AXIS2C_HOME/modules/rampart $CLIENT_REPO/modules
+
+#RUN
+./sec_echo http://localhost:9090/axis2/services/sec_echo $CLIENT_REPO
+
