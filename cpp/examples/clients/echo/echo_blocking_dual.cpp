bb3712f351fed075a7618e6969cc83c8aa057b06
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-07-19 06:55:11 +0000

    complie error fixed
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@41452 a5903396-d722-0410-b921-86c7d4935375

--- cpp/examples/clients/echo/echo_blocking_dual.cpp
+++ cpp/examples/clients/echo/echo_blocking_dual.cpp
@@ -82,7 +82,6 @@ int main(int argc, char *argv[])
             cout << endl << "Error: " << e << endl;
         }
     }
-	Sleep(1000);
 	sc.getLastResponseSoapEnvelopeString();
     delete payload;
 }
