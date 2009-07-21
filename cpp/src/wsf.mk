29b07e59250cd61278100767e470f47ded81aaa4
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-07-21 07:20:10 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@41579 a5903396-d722-0410-b921-86c7d4935375

--- cpp/src/wsf.mk
+++ cpp/src/wsf.mk
@@ -51,18 +51,19 @@ wso2_wsf_dll :
 	$(CC) $(CFLAGS) $(INCLUDE_PATH) main\*.cpp /Foint.msvc\ /c
 	$(RC) /r /fo "int.msvc\wsf.res" main\wsf.rc
 	$(LD) $(LDFLAGS) int.msvc\*.obj int.msvc\wsf.res $(LIBS) /DLL  /OUT:$(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll /IMPLIB:$(WSFCPP_HOME_DIR)\lib\wso2_wsf.lib
-	-@$(_VC_MANIFEST_EMBED_DLL)
+	if exist $(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll.manifest mt.exe -manifest $(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll.manifest -outputresource:$(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll;2
 
 wsf_cpp_msg_recv_dll:
 	@if not exist int.msvc\msg_recv mkdir int.msvc\msg_recv
 	$(CC) $(CFLAGS) $(INCLUDE_PATH) msg_recv\wsf_cpp_msg_recv.cpp /Foint.msvc\msg_recv\ /c
 	$(LD) $(LDFLAGS) int.msvc\msg_recv\wsf_cpp_msg_recv.obj $(LIBS) wso2_wsf.lib /DLL /OUT:$(WSFCPP_HOME_DIR)\lib\wsf_cpp_msg_recv.dll
-	-@$(_VC_MANIFEST_EMBED_DLL)
+	if exist $(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll.manifest mt.exe -manifest $(WSFCPP_HOME_DIR)\lib\wsf_cpp_msg_recv.dll.manifest -outputresource:$(WSFCPP_HOME_DIR)\lib\wsf_cpp_msg_recv.dll;2
 
 wso2_wsf_security_dll:
 	@if not exist int.msvc\security mkdir int.msvc\security
 	$(CC) $(CFLAGS) $(INCLUDE_PATH) security\*.cpp /Foint.msvc\security\ /c
 	$(LD) $(LDFLAGS) int.msvc\security\*.obj $(LIBS) wso2_wsf.lib rampart.lib /DLL  /OUT:$(WSFCPP_HOME_DIR)\lib\wso2_wsf_security.dll /IMPLIB:$(WSFCPP_HOME_DIR)\lib\wso2_wsf_security.lib
+	if exist $(WSFCPP_HOME_DIR)\lib\wso2_wsf.dll.manifest mt.exe -manifest $(WSFCPP_HOME_DIR)\lib\wso2_wsf_security.dll.manifest -outputresource:$(WSFCPP_HOME_DIR)\lib\wso2_wsf_security.dll;2
 
 !if "$(ENABLE_RAMPARTC)" == "0"
 wsfcpp: wso2_wsf_dll wsf_cpp_msg_recv_dll
