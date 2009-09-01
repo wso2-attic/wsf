baa30c2a805104229b203de800560ee56ebc7968
shankar <shankar@a5903396-d722-0410-b921-86c7d4935375>
2009-09-01 11:47:57 +0000

    fixing memory leak
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@44649 a5903396-d722-0410-b921-86c7d4935375

--- c/wsclient/src/wsclient.c
+++ c/wsclient/src/wsclient.c
@@ -21,7 +21,7 @@
 #include "error.h"
 #include <axiom.h>
 
-#define BUFF_SIZE 32
+#define BUFF_SIZE 1024
 
 static void 
 print_help ();
@@ -199,32 +199,44 @@ static void
 parse_input(const axutil_env_t *env,
 			axis2_char_t **input)
 {
-	int c, nc, index;
-	axis2_char_t buff[BUFF_SIZE];
-	nc = 0;
-	index = 0;
-	*input = "";
-	c = getchar();
-	while(!feof(stdin) && !ferror(stdin))
-	{
-	    nc++;
-	    buff[index] = (axis2_char_t) c;
-	    if(index == (BUFF_SIZE - 2))
-	    {
-			buff[BUFF_SIZE - 1] = '\0';
-			*input = axutil_strcat(env, *input, buff, NULL);
-			index = 0;
-			c = getchar();
-			continue;
-	    }
-	    c = getchar();
-	    index++;
-	}
-	if(feof(stdin) && !ferror(stdin))
-	{
-	    buff[index] = '\0';
-	    *input = axutil_strcat(env, *input, buff, NULL);
-	}
+    int c, nc, index;
+    axis2_char_t buff[BUFF_SIZE];
+    nc = 0;
+    index = 0;
+    *input = NULL;
+    c = getchar();
+    while(!feof(stdin) && !ferror(stdin))
+    {
+        nc++;
+        buff[index] = (axis2_char_t) c;
+        if(index == (BUFF_SIZE - 2))
+        {
+            axis2_char_t *tmp = NULL;
+            buff[BUFF_SIZE - 1] = '\0';
+            tmp = axutil_stracat(env, *input, buff);
+            if(*input)
+            {
+                AXIS2_FREE(env->allocator, *input);
+            }
+            *input = tmp;
+            index = 0;
+            c = getchar();
+            continue;
+        }
+        c = getchar();
+        index++;
+    }
+    if(feof(stdin) && !ferror(stdin))
+    {
+        axis2_char_t *tmp = NULL;
+        buff[index] = '\0';
+        tmp = axutil_stracat(env, *input, buff);
+        if(*input)
+        {
+            AXIS2_FREE(env->allocator, *input);
+        }
+        *input = tmp;
+    }
 }
 
 
