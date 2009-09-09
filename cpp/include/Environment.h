fde92f0a3f66f9271e5b4f029bba3e6baec42fed
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-09-09 12:09:45 +0000

    doc comments added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@45221 a5903396-d722-0410-b921-86c7d4935375

--- cpp/include/Environment.h
+++ cpp/include/Environment.h
@@ -53,7 +53,9 @@ namespace wso2wsf
 		pthread_key_t tls_key;
 #endif		
 		public:
-
+		/**
+		 *  Constructor for the TLSKey class.
+		 */
 		TLSKey()
 		{
 #ifdef WIN32
@@ -63,7 +65,9 @@ namespace wso2wsf
 			pthread_key_create(&tls_key, NULL);
 #endif
 		}
-
+		/**
+		 * Returns the unique key associated with the index created for thread local storage.
+		 */
 #ifdef WIN32 
 		DWORD getTLSKey()
 #else
@@ -72,7 +76,9 @@ namespace wso2wsf
 		{
 			return tls_key;
 		}
-
+		/**
+		 * Destructor for TLSKey class.
+		 */
 		~TLSKey()
 		{
 #ifdef WIN32
@@ -91,14 +97,22 @@ namespace wso2wsf
 	{
 
 	private:
-		
+		/**
+		 * variable to store to the thread local storage key.
+		 */
 		static TLSKey key;
 	public:
-		/** keeps the log file name */
+		/** 
+		 * keeps the log file name 
+		 */
 		static std::string _logFileName;
-		/** keeps the log level */
+		/** 
+ 		* keeps the log level 
+ 		*/
 		static axutil_log_levels_t _logLevel;
-		
+		/**
+		 * Constructor for the Environment class 
+		*/ 
 		Environment();
 		/**
 		*  Method to store the current thread specific environment. 
@@ -124,6 +138,14 @@ namespace wso2wsf
 		/**
 		* Initialize with log file and log level. This function must be called prior to using the framework for client
 		* side. For the server, calling the method for initialization is not required 
+		* @param logFileName name of the logfile name which will be created by the environment and used to write logs.
+		* @param logLevel The level of details that should be written to the log file. There are several log levels available.
+		* 1. WSF_LOG_LEVEL_CRITICAL Only Critical level logs will be written. Suitable for production deployment.
+		* 2. WSF_LOG_LEVEL_ERROR  Only Error messages will be wrriten to the log.
+		* 3. WSF_LOG_LEVEL_INFO   Logs information 
+		* 4. WSF_LOG_LEVEL_DEBUG  logs everything
+		* 5. WSF_LOG_LEVEL_USER   log user lovel messages.
+		* 6. WSF_LOG_LEVEL_TRACE  Trace Level loggging.
 		*/
 		static WSF_EXTERN void WSF_CALL initialize(std::string logFileName,  axutil_log_levels_t logLevel);
 		/**
