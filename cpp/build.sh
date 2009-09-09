c5d312d731ea95777f8feede99be80112cebf85f
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2009-09-09 15:21:25 +0000

    Updating the build file
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@45226 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build.sh
+++ cpp/build.sh
@@ -7,7 +7,9 @@ cp build/wsf_c/axis2c/neethi/src/Makefile.am wsf_c/axis2c/neethi/src/Makefile.am
 
 sh autogen.sh
 
-./configure --prefix=`pwd`/deploy 
+./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-xpath=/usr/include/libxml2/libxml
 make -j 10 
 make install
 make examples
+
+
