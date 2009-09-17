e34a9d59637d226a37cc6c731f0bf745e90cce95
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-09-17 18:13:17 +0000

    Calculator Skeleton updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@45738 a5903396-d722-0410-b921-86c7d4935375

--- cpp/examples/codegen/calculator/service/CalculatorSkeleton.cpp
+++ cpp/examples/codegen/calculator/service/CalculatorSkeleton.cpp
@@ -11,24 +11,44 @@
      #include "CalculatorSkeleton.h"
 
     
+     #include <Mul.h>
+    
+     #include <MulResponse.h>
+    
      #include <Div.h>
     
      #include <DivResponse.h>
-    
-     #include <Add.h>
-    
-     #include <AddResponse.h>
     
      #include <Sub.h>
     
      #include <SubResponse.h>
     
-     #include <Mul.h>
+     #include <Add.h>
     
-     #include <MulResponse.h>
+     #include <AddResponse.h>
     
     using namespace localhost_axis_calculator;
+		 
+        /**
+         * Auto generated function definition signature
+         * for "mul|http://localhost/axis/Calculator" operation.
+         * 
+         * @param _mul of the localhost_axis_calculator::Mul
+         *
+         * @return localhost_axis_calculator::MulResponse*
+         */
+        localhost_axis_calculator::MulResponse* CalculatorSkeleton::mul(wso2wsf::MessageContext *outCtx ,localhost_axis_calculator::Mul* _mul)
 
+        {
+			int value1 =0, value2 =0, result =0;
+			value1 = _mul->getA();
+			value2 = _mul->getB();
+			result = value1*value2;
+			localhost_axis_calculator::MulResponse *mulResponse = new localhost_axis_calculator::MulResponse();
+			mulResponse->setMulReturn(result);
+			return mulResponse;
+		}
+     
 
 		 
         /**
@@ -39,40 +59,21 @@
          *
          * @return localhost_axis_calculator::DivResponse*
          */
-        localhost_axis_calculator::DivResponse* CalculatorSkeleton::div(localhost_axis_calculator::Div* _div)
+        localhost_axis_calculator::DivResponse* CalculatorSkeleton::div(wso2wsf::MessageContext *outCtx ,localhost_axis_calculator::Div* _div)
 
         {
-			localhost_axis_calculator::DivResponse *divRes = NULL;
-			int val1 =0, val2 =0, retval =0;
-			val1 = _div->getArg_0_3();
-			val2 = _div->getArg_1_3();
-			retval = val1/val2;
-			divRes = new localhost_axis_calculator::DivResponse();
-			divRes->setDivReturn(retval);
-			return divRes;
-        }
-     
-
-		 
-        /**
-         * Auto generated function definition signature
-         * for "add|http://localhost/axis/Calculator" operation.
-         * 
-         * @param _add of the localhost_axis_calculator::Add
-         *
-         * @return localhost_axis_calculator::AddResponse*
-         */
-        localhost_axis_calculator::AddResponse* CalculatorSkeleton::add(localhost_axis_calculator::Add* _add)
-
-        {
-			localhost_axis_calculator::AddResponse *addRes = NULL;
-			int val1 = 0, val2=0, retval = 0;
-			val1 = _add->getArg_0_0();
-			val2 = _add->getArg_1_0();
-			retval = val1 + val2;
-			addRes = new localhost_axis_calculator::AddResponse();
-			addRes->setAddReturn(retval);
-			return addRes;
+			int value1 =0, value2 =0, result =0;
+			value1 = _div->getA();
+			value2 = _div->getB();
+			if(value2 == 0)
+			{
+				std::cout<<"Cannot divide by Zero"<<std::endl;
+				return NULL;
+			}
+			result = value1/value2;
+			localhost_axis_calculator::DivResponse *divResponse = new localhost_axis_calculator::DivResponse();
+			divResponse->setDivReturn(result);
+	        return divResponse;
         }
      
 
@@ -85,40 +86,38 @@
          *
          * @return localhost_axis_calculator::SubResponse*
          */
-        localhost_axis_calculator::SubResponse* CalculatorSkeleton::sub(localhost_axis_calculator::Sub* _sub)
+        localhost_axis_calculator::SubResponse* CalculatorSkeleton::sub(wso2wsf::MessageContext *outCtx ,localhost_axis_calculator::Sub* _sub)
 
         {
-			localhost_axis_calculator::SubResponse *subRes = NULL;
-			int val1 =0, val2 = 0, retval = 0;
-			val1 = _sub->getArg_0_1();
-			val2 = _sub->getArg_1_1();
-			retval = val1 - val2;
-			subRes = new localhost_axis_calculator::SubResponse();
-			subRes->setSubReturn(retval);
-			return subRes;
+			int value1 =0, value2 = 0, result = 0;
+			value1 = _sub->getA();
+			value2 = _sub->getB();
+			result = value1 - value2;
+			localhost_axis_calculator::SubResponse *subResponse = new localhost_axis_calculator::SubResponse();
+			subResponse->setSubReturn(result);
+			return subResponse;
         }
      
 
 		 
         /**
          * Auto generated function definition signature
-         * for "mul|http://localhost/axis/Calculator" operation.
+         * for "add|http://localhost/axis/Calculator" operation.
          * 
-         * @param _mul of the localhost_axis_calculator::Mul
+         * @param _add of the localhost_axis_calculator::Add
          *
-         * @return localhost_axis_calculator::MulResponse*
+         * @return localhost_axis_calculator::AddResponse*
          */
-        localhost_axis_calculator::MulResponse* CalculatorSkeleton::mul(localhost_axis_calculator::Mul* _mul)
+        localhost_axis_calculator::AddResponse* CalculatorSkeleton::add(wso2wsf::MessageContext *outCtx ,localhost_axis_calculator::Add* _add)
 
         {
-			localhost_axis_calculator::MulResponse *mulRes = NULL;
-			int val1 =0, val2 =0, retval =0;
-			val1 = _mul->getArg_0_2();
-			val2 = _mul->getArg_1_2();
-			retval = val1+val2;
-			mulRes = new localhost_axis_calculator::MulResponse();
-			mulRes->setMulReturn(retval);
-			return mulRes;
+			int value1 = 0, value2=0, result = 0;
+			value1 = _add->getA();
+			value2 = _add->getB();
+			result = value1 + value2;
+			localhost_axis_calculator::AddResponse *addResponse = new localhost_axis_calculator::AddResponse();
+			addResponse->setAddReturn(result);
+			return addResponse;
 		}
      
 
