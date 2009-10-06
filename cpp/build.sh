afcd081c1aa4b78da78cea412b5d3312d1acc353
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2009-10-06 09:07:43 +0000

    Need to call wsf_c/build/copy_build_files.sh script. Fixed it
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@46655 a5903396-d722-0410-b921-86c7d4935375

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+cd wsf_c
+./build/copy_build_files.sh
+cd ..
 cp wsf_c/build/rampartc/configure.ac wsf_c/rampartc/configure.ac
 cp build/wsf_c/axis2c/neethi/configure.ac wsf_c/axis2c/neethi/configure.ac
 cp build/wsf_c/axis2c/neethi/src/Makefile.am wsf_c/axis2c/neethi/src/Makefile.am
-
 sh autogen.sh
 
 ./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-xpath=/usr/include/libxml2/libxml
