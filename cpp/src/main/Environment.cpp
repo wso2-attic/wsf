094ea168f520490bc65816eb3947e1d2a1c6de43
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-10-11 16:24:10 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@46909 a5903396-d722-0410-b921-86c7d4935375

--- cpp/src/main/Environment.cpp
+++ cpp/src/main/Environment.cpp
@@ -112,7 +112,7 @@ void WSF_CALL Environment::switchToLocalPool()
 	}
 }
 
-void WSF_CALL Environment::initialize(const char* logFileName, axutil_log_levels_t logLevel)
+void WSF_CALL Environment::initialize(std::string logFileName, axutil_log_levels_t logLevel)
 {
 	_logFileName = logFileName;
 	_logLevel = logLevel;
