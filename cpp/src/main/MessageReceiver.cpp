63d4ced4e906db3ce54cdc37eaff851830a41ef1
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2009-12-31 06:00:18 +0000

    MessageReceiver code updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@52462 a5903396-d722-0410-b921-86c7d4935375

--- cpp/src/main/MessageReceiver.cpp
+++ cpp/src/main/MessageReceiver.cpp
@@ -15,6 +15,9 @@
  */
 
 #include <MessageReceiver.h>
+#include <ServiceSkeleton.h>
+#include <axutil_class_loader.h>
+
 using namespace std;
 using namespace wso2wsf;
 
@@ -29,6 +32,8 @@ WSF_EXTERN axis2_status_t WSF_CALL wso2wsf::MessageReceiver::processBusinessLogi
 	MessageReceiver *receiver = (wso2wsf::MessageReceiver*)(derived);
 	if(receiver)
 	{
+		Environment::setEnv(env);
+
 		MessageContext *inMsgCtx = new MessageContext(msg_ctx);
 		MessageContext *outMsgCtx = new MessageContext(msg_ctx);
 		status = receiver->invokeBusinessLogicSync(inMsgCtx, outMsgCtx);
@@ -36,6 +41,65 @@ WSF_EXTERN axis2_status_t WSF_CALL wso2wsf::MessageReceiver::processBusinessLogi
 		outMsgCtx->setAxis2MessageContext(NULL);
 		delete inMsgCtx;
 		delete outMsgCtx;
+		Environment::removeEnv();
 	}
 	return status ? AXIS2_SUCCESS : AXIS2_FAILURE;
+}
+
+WSF_EXTERN  axis2_status_t WSF_CALL MessageReceiver::loadAndInitService(
+			axis2_msg_recv_t *msg_recv,
+			const axutil_env_t* env,
+			struct axis2_svc *svc)
+{
+	ServiceSkeleton *impl_class = NULL;
+	axutil_param_t *impl_info_param = NULL;
+	axis2_conf_ctx_t *conf_ctx = NULL;
+	AXIS2_ENV_CHECK(env, NULL);
+	if (!svc)
+	{
+		return AXIS2_FAILURE;
+	}
+	Environment::setEnv(env);
+
+	conf_ctx = axis2_msg_recv_get_conf_ctx(msg_recv, env);
+	axutil_thread_mutex_lock(axis2_svc_get_mutex(svc, env));
+	impl_class = (ServiceSkeleton*)axis2_svc_get_impl_class(svc, env);
+	if (impl_class)
+	{
+		axutil_thread_mutex_unlock(axis2_svc_get_mutex(svc, env));
+		return AXIS2_SUCCESS;
+	}
+	impl_info_param = axis2_svc_get_param(svc, env, AXIS2_SERVICE_CLASS);
+	if (!impl_info_param)
+	{
+		AXIS2_ERROR_SET(env->error, AXIS2_ERROR_INVALID_STATE_SVC,
+			AXIS2_FAILURE);
+		axutil_thread_mutex_unlock(axis2_svc_get_mutex(svc, env));
+		return AXIS2_FAILURE;
+	}
+
+	axutil_allocator_switch_to_global_pool(env->allocator);
+
+	axutil_class_loader_init(env);
+	
+	impl_class = (ServiceSkeleton*)axutil_class_loader_create_dll(env, impl_info_param);
+	
+
+	if (impl_class)
+	{
+		const axis2_char_t *svc_name = axis2_svc_get_name(svc, env);
+		axis2_svc_ctx_t *svc_ctx = axis2_conf_ctx_get_svc_ctx(conf_ctx, env, svc_name); 
+		impl_class->setServiceContext(svc_ctx);
+		bool value = impl_class->init();
+		if(!value)
+				AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI, "Service Initinalization Failed");
+	}
+
+	axis2_svc_set_impl_class(svc, env, impl_class);
+	axutil_allocator_switch_to_local_pool(env->allocator);
+	axutil_thread_mutex_unlock(axis2_svc_get_mutex(svc, env));
+
+	Environment::removeEnv();
+	return AXIS2_SUCCESS;		
+
 }
