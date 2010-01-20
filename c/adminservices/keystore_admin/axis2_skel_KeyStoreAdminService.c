879c4d72bbfbb679fea6c301579b05eb8096547f
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-01-20 07:37:15 +0000

    KeyStore Admin added
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@53258 a5903396-d722-0410-b921-86c7d4935375

---
+++ c/adminservices/keystore_admin/axis2_skel_KeyStoreAdminService.c
@@ -0,0 +1,120 @@
+
+
+    /**
+     * axis2_skel_KeyStoreAdminService.c
+     *
+     * This file was auto-generated from WSDL for "KeyStoreAdminService|http://service.keystore.security.carbon.wso2.org" service
+     * by the Apache Axis2/C version: #axisVersion# #today#
+     * axis2_skel_KeyStoreAdminService Axis2/C skeleton for the axisService
+     */
+
+     #include "axis2_skel_KeyStoreAdminService.h"
+
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "getStoreEntries|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         * @param _getStoreEntries of the adb_getStoreEntries_t*
+         *
+         * @return adb_getStoreEntriesResponse_t*
+         */
+        adb_getStoreEntriesResponse_t* axis2_skel_KeyStoreAdminService_getStoreEntries(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                              adb_getStoreEntries_t* _getStoreEntries,
+                                          axis2_skel_KeyStoreAdminService_getStoreEntries_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return (adb_getStoreEntriesResponse_t*)NULL;
+        }
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "addKeyStore|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         * @param _addKeyStore of the adb_addKeyStore_t*
+         *
+         * @return 
+         */
+        axis2_status_t  axis2_skel_KeyStoreAdminService_addKeyStore(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                              adb_addKeyStore_t* _addKeyStore,
+                                          axis2_skel_KeyStoreAdminService_addKeyStore_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return AXIS2_SUCCESS;
+        }
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "importCertToStore|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         * @param _importCertToStore of the adb_importCertToStore_t*
+         *
+         * @return 
+         */
+        axis2_status_t  axis2_skel_KeyStoreAdminService_importCertToStore(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                              adb_importCertToStore_t* _importCertToStore,
+                                          axis2_skel_KeyStoreAdminService_importCertToStore_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return AXIS2_SUCCESS;
+        }
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "getKeystoreInfo|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         * @param _getKeystoreInfo of the adb_getKeystoreInfo_t*
+         *
+         * @return adb_getKeystoreInfoResponse_t*
+         */
+        adb_getKeystoreInfoResponse_t* axis2_skel_KeyStoreAdminService_getKeystoreInfo(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                              adb_getKeystoreInfo_t* _getKeystoreInfo,
+                                          axis2_skel_KeyStoreAdminService_getKeystoreInfo_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return (adb_getKeystoreInfoResponse_t*)NULL;
+        }
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "deleteStore|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         * @param _deleteStore of the adb_deleteStore_t*
+         *
+         * @return 
+         */
+        axis2_status_t  axis2_skel_KeyStoreAdminService_deleteStore(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                              adb_deleteStore_t* _deleteStore,
+                                          axis2_skel_KeyStoreAdminService_deleteStore_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return AXIS2_SUCCESS;
+        }
+     
+
+		 
+        /**
+         * auto generated function definition signature
+         * for "getKeyStores|http://service.keystore.security.carbon.wso2.org" operation.
+         * @param env environment ( mandatory)* @param MessageContext the outmessage context
+         *
+         * @return adb_getKeyStoresResponse_t*
+         */
+        adb_getKeyStoresResponse_t* axis2_skel_KeyStoreAdminService_getKeyStores(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
+                                          axis2_skel_KeyStoreAdminService_getKeyStores_fault *fault )
+        {
+          /* TODO fill this with the necessary business logic */
+          return (adb_getKeyStoresResponse_t*)NULL;
+        }
+     
+
