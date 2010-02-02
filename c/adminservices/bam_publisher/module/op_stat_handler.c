67b5873d0a60f552e5e3af315696d16a8ab249c1
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2010-02-02 09:35:40 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@53893 a5903396-d722-0410-b921-86c7d4935375

--- c/adminservices/bam_publisher/module/op_stat_handler.c
+++ c/adminservices/bam_publisher/module/op_stat_handler.c
@@ -16,8 +16,16 @@
 
 #include <axis2_handler_desc.h>
 #include <axis2_msg_ctx.h>
+#include <axis2_svc.h>
+#include <axis2_op.h>
 #include <axutil_property.h>
-
+#include <service_admin_counter.h>
+#include <service_admin_util.h>
+#include <codegen/adb_OperationStatistics.h>
+#include <codegen/adb_getOperationStatistics.h>
+#include <codegen/adb_getOperationStatisticsResponse.h>
+#include <bam_publisher_statistics.h>
+#include "bam_publisher_util.h"
 axis2_status_t AXIS2_CALL
 bam_publisher_op_stat_handler_invoke(
         struct axis2_handler *handler, 
@@ -52,7 +60,84 @@ bam_publisher_op_stat_handler_invoke(struct axis2_handler *handler,
                         struct axis2_msg_ctx *msg_ctx)
 {
     axis2_status_t status = AXIS2_SUCCESS;
-    
+    axis2_char_t *svc_name = NULL;
+    axis2_char_t *op_name = NULL;
+    axis2_svc_t *svc = NULL;
+    axis2_op_t *op = NULL;
+    axis2_svc_t *stat_admin_svc = NULL;
+    adb_getOperationStatistics_t *get_op_stat = NULL;
+    adb_getOperationStatisticsResponse_t * get_op_stat_res = NULL;
+    adb_OperationStatistics_t *op_stat = NULL;
+    int current_count = 0;
+    int last_count = 0;
+    axis2_char_t *str_threshold_count = NULL;
+    int threshold_count = 0;
+    axutil_param_t *param = NULL;
+    axis2_conf_ctx_t *conf_ctx = NULL;
+    axis2_conf_t *conf = NULL;
+
+    AXIS2_LOG_TRACE(env->log, AXIS2_LOG_SI, "[adminservices] Entry:bam_publisher_op_stat_handler_invoke");
+    conf_ctx = axis2_msg_ctx_get_conf_ctx(msg_ctx, env);
+    conf = axis2_conf_ctx_get_conf(conf_ctx, env);
+    svc = axis2_msg_ctx_get_svc(msg_ctx, env);
+    if(svc)
+    {
+        svc_name = (axis2_char_t *) axis2_svc_get_name(svc, env);
+    }
+    op = axis2_msg_ctx_get_op(msg_ctx, env);
+    if(op)
+    {
+        op_name = axutil_qname_get_localpart(axis2_op_get_qname(op, env), env);
+    }
+    get_op_stat = adb_getOperationStatistics_create_with_values(env, svc_name, op_name);
+    get_op_stat_res = bam_publisher_statistics_get_operation_statistics(env, msg_ctx, get_op_stat);
+    if(get_op_stat_res)
+    {
+        op_stat = adb_getOperationStatisticsResponse_get_return(get_op_stat_res, env);
+        if(op_stat)
+        {
+            current_count = adb_OperationStatistics_get_requestCount(op_stat, env);
+        }
+    }
+    param = axis2_conf_get_param(conf, env, BAM_PUBLISHER_OPERATION_REQUEST_THRESHOLD_COUNT_PARAM);
+    if(param)
+    {
+        str_threshold_count = axutil_param_get_value(param, env);
+        if(str_threshold_count)
+        {
+            threshold_count = axutil_atoi(str_threshold_count);
+        }
+    }
+    last_count = service_admin_counter_get_last_count(env, msg_ctx, svc_name, op_name);
+    if((current_count - last_count) > threshold_count)
+    {
+        axis2_char_t *server_name = NULL;
+        axiom_node_t *payload = NULL;
+        int avg_res_time = adb_OperationStatistics_get_avgResponseTime(op_stat, env);
+        int min_res_time = adb_OperationStatistics_get_minResponseTime(op_stat, env);
+        int max_res_time = adb_OperationStatistics_get_maxResponseTime(op_stat, env);
+        int request_count = current_count;
+        int response_count = adb_OperationStatistics_get_responseCount(op_stat, env);
+        int fault_count = adb_OperationStatistics_get_faultCount(op_stat, env);
+
+        /* Eventing threshold count reached. So let's fire the event */
+        axis2_counter_set_last_count(env, msg_ctx, svc_name, op_name, current_count);
+        server_name = service_admin_util_get_epr_address(env, msg_ctx, svc_name);
+        payload = bam_publisher_util_get_payload(env, server_name, avg_res_time, 
+                min_res_time, max_res_time, request_count, response_count, fault_count, svc_name, 
+                op_name);
+        if(payload)
+        {
+            bam_publisher_util_publish(env, msg_ctx, payload);
+        }
+        else
+        {
+            AXIS2_LOG_ERROR(env->log, AXIS2_LOG_SI, 
+                    "[adminservices] Event payload creation failed");
+            status = AXIS2_FAILURE;
+        }
+    }
+ 
     AXIS2_LOG_TRACE(env->log, AXIS2_LOG_SI, "[adminservices] End:bam_publisher_op_stat_handler_invoke");
     
     return status;
