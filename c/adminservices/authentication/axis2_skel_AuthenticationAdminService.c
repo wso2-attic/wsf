96a16709f9c42f105a27cbf0fdb643198cf30aeb
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-02-22 11:14:35 +0000

    authentication/axis2_skel_AuthenticationAdminService.c login corrected
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@55908 a5903396-d722-0410-b921-86c7d4935375

--- c/adminservices/authentication/axis2_skel_AuthenticationAdminService.c
+++ c/adminservices/authentication/axis2_skel_AuthenticationAdminService.c
@@ -40,18 +40,27 @@
          *
          * @return adb_loginResponse_t*
          */
-        adb_loginResponse_t* 
-			axis2_skel_AuthenticationAdminService_login(
-			const axutil_env_t *env , 
-			axis2_msg_ctx_t *msg_ctx,
-            adb_login_t* _login,
-            axis2_skel_AuthenticationAdminService_login_fault *fault )
-        {
-          /* TODO fill this with the necessary business logic */
-			adb_loginResponse_t *loginResponse = adb_loginResponse_create(env);
-			adb_loginResponse_set_return(loginResponse, env, AXIS2_TRUE);
-          return loginResponse;
-        }
+adb_loginResponse_t* 
+	axis2_skel_AuthenticationAdminService_login(
+	const axutil_env_t *env , 
+	axis2_msg_ctx_t *msg_ctx,
+    adb_login_t* _login,
+    axis2_skel_AuthenticationAdminService_login_fault *fault )
+{
+  /* TODO fill this with the necessary business logic */
+	axis2_char_t *username = NULL, *password = NULL;
+	adb_loginResponse_t *loginResponse = adb_loginResponse_create(env);
+	username = adb_login_get_username(_login, env);
+	password = adb_login_get_password(_login, env);
+	if(!axutil_strcmp(username,"admin") && !axutil_strcmp(password,"admin"))
+	{
+		adb_loginResponse_set_return(loginResponse, env, AXIS2_TRUE);
+	}else
+	{
+		adb_loginResponse_set_return(loginResponse, env, AXIS2_FALSE);
+	}
+  return loginResponse;
+}
      
 
 		 
@@ -63,31 +72,31 @@
          *
          * @return adb_getSystemPermissionOfUserResponse_t*
          */
-        adb_getSystemPermissionOfUserResponse_t* 
-			axis2_skel_AuthenticationAdminService_getSystemPermissionOfUser(
-			const axutil_env_t *env , 
-			axis2_msg_ctx_t *msg_ctx,
-            adb_getSystemPermissionOfUser_t* _getSystemPermissionOfUser,
-            axis2_skel_AuthenticationAdminService_getSystemPermissionOfUser_fault *fault )
-        {
-			adb_getSystemPermissionOfUserResponse_t* response = NULL;
-			axutil_array_list_t *list = NULL;
+adb_getSystemPermissionOfUserResponse_t* 
+	axis2_skel_AuthenticationAdminService_getSystemPermissionOfUser(
+	const axutil_env_t *env , 
+	axis2_msg_ctx_t *msg_ctx,
+    adb_getSystemPermissionOfUser_t* _getSystemPermissionOfUser,
+    axis2_skel_AuthenticationAdminService_getSystemPermissionOfUser_fault *fault )
+{
+	adb_getSystemPermissionOfUserResponse_t* response = NULL;
+	axutil_array_list_t *list = NULL;
 
-			/* TODO fill this with the necessary business logic */
-			list = axutil_array_list_create(env, 0);
-			axutil_array_list_add(list, env, axutil_strdup(env, "login"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "manage-configuration"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "manage-security"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "upload-services"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "manage-services"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "manage-lc-configuration"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "manage-mediation"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "monitor-system"));
-			axutil_array_list_add(list, env, axutil_strdup(env, "delegate-identity"));
+	/* TODO fill this with the necessary business logic */
+	list = axutil_array_list_create(env, 0);
+	axutil_array_list_add(list, env, axutil_strdup(env, "login"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "manage-configuration"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "manage-security"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "upload-services"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "manage-services"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "manage-lc-configuration"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "manage-mediation"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "monitor-system"));
+	axutil_array_list_add(list, env, axutil_strdup(env, "delegate-identity"));
 
-			response = adb_getSystemPermissionOfUserResponse_create_with_values(env, list);
-			return response;
-        }
+	response = adb_getSystemPermissionOfUserResponse_create_with_values(env, list);
+	return response;
+}
      
 
 		 
@@ -99,17 +108,17 @@
          *
          * @return adb_loginWithDelegationResponse_t*
          */
-        adb_loginWithDelegationResponse_t* 
-			axis2_skel_AuthenticationAdminService_loginWithDelegation(
-			const axutil_env_t *env , 
-			axis2_msg_ctx_t *msg_ctx,
-			adb_loginWithDelegation_t* _loginWithDelegation,
-            axis2_skel_AuthenticationAdminService_loginWithDelegation_fault *fault )
-        {
-			adb_loginWithDelegationResponse_t* loginWithDelegationRes = adb_loginWithDelegationResponse_create(env);
-			adb_loginWithDelegationResponse_set_return(loginWithDelegationRes, env, AXIS2_TRUE);
-			return loginWithDelegationRes;
-        }
+adb_loginWithDelegationResponse_t* 
+axis2_skel_AuthenticationAdminService_loginWithDelegation(
+	const axutil_env_t *env , 
+	axis2_msg_ctx_t *msg_ctx,
+	adb_loginWithDelegation_t* _loginWithDelegation,
+    axis2_skel_AuthenticationAdminService_loginWithDelegation_fault *fault )
+{
+	adb_loginWithDelegationResponse_t* loginWithDelegationRes =									    adb_loginWithDelegationResponse_create(env);
+	adb_loginWithDelegationResponse_set_return(loginWithDelegationRes, env, AXIS2_TRUE);
+	return loginWithDelegationRes;
+}
      
 
 		 
@@ -121,11 +130,14 @@
          *
          * @return adb_getUserPermissionsResponse_t*
          */
-        adb_getUserPermissionsResponse_t* axis2_skel_AuthenticationAdminService_getUserPermissions(const axutil_env_t *env , axis2_msg_ctx_t *msg_ctx,
-                                              adb_getUserPermissions_t* _getUserPermissions,
-                                          axis2_skel_AuthenticationAdminService_getUserPermissions_fault *fault )
-        {
-			adb_getUserPermissionsResponse_t* response = NULL;
+adb_getUserPermissionsResponse_t* 
+axis2_skel_AuthenticationAdminService_getUserPermissions(
+	const axutil_env_t *env , 
+	axis2_msg_ctx_t *msg_ctx,
+    adb_getUserPermissions_t* _getUserPermissions,
+    axis2_skel_AuthenticationAdminService_getUserPermissions_fault *fault )
+{
+	adb_getUserPermissionsResponse_t* response = NULL;
     axutil_array_list_t *list = NULL;
 
     /* TODO fill this with the necessary business logic */
@@ -142,6 +154,6 @@
 
 	response = adb_getUserPermissionsResponse_create_with_values(env, list);
     return response;
-        }
+}
      
 
