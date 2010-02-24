5ba55054f586d778fbbd70d339f4ea4b2a346243
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2010-02-24 10:51:39 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@56202 a5903396-d722-0410-b921-86c7d4935375

--- c/adminservices/build.sh
+++ c/adminservices/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./autogen.sh
 export WSFC_HOME=/axis2c/deploywsfc
-./configure --prefix=${WSFC_HOME} --with-axis2=${WSFC_HOME}/include/axis2-1.6.0 --enable-static=no --enable-openssl=no --enable-rampart=yes
+./configure --prefix=${WSFC_HOME} --with-wsfc=${WSFC_HOME}/include/wsfc-2.0.0 --enable-static=no --enable-openssl=no --enable-rampart=yes
 make 
 #make install
 
