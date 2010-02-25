bda8efd16e0378a72aa1273a287bd7b2e56a0c77
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-02-25 09:41:13 +0000

    build.sh file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@56383 a5903396-d722-0410-b921-86c7d4935375

--- c/adminservices/build.sh
+++ c/adminservices/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export WSFC_HOME=./../deploy
+sh autogen.sh
 ./configure --prefix=${WSFC_HOME} --with-wsfc=${WSFC_HOME}/include/wsfc-2.0.0 --enable-static=no --enable-openssl=no --enable-rampart=yes
 make 
 #make install
