e4ffd6f04815e49f1e732f177b034999777a2419
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-02-25 09:39:37 +0000

    build.sh file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@56382 a5903396-d722-0410-b921-86c7d4935375

--- c/adminservices/build.sh
+++ c/adminservices/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export WSFC_HOME=/axis2c/deploywsfc
+export WSFC_HOME=./../deploy
 ./configure --prefix=${WSFC_HOME} --with-wsfc=${WSFC_HOME}/include/wsfc-2.0.0 --enable-static=no --enable-openssl=no --enable-rampart=yes
 make 
 #make install
