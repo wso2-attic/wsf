8566b48b45437de81e2c192f33b6032676cb2f8e
danushka <danushka@a5903396-d722-0410-b921-86c7d4935375>
2010-03-05 06:39:23 +0000

    adding deploy.js
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@57062 a5903396-d722-0410-b921-86c7d4935375

---
+++ add-ons/c/adminservices/deploy.js
@@ -0,0 +1,40 @@
+var WshShell = new ActiveXObject("WScript.Shell");
+fso = new ActiveXObject("Scripting.FileSystemObject");
+
+var fso, f, s, r;
+var ForReading = 1, ForWriting = 2;
+
+var args = WScript.Arguments;
+
+var read_file = args.Item(0);
+var deploy_file = args.Item(1);
+var axis2c_home = args.Item(2);
+
+// Deploy the client policy file
+f = fso.OpenTextFile(read_file, ForReading);
+s = f.ReadAll();
+f.Close();
+
+f = fso.OpenTextFile(deploy_file, ForWriting, true);
+while (s.search(/AXIS2C_HOME/) != -1) {
+	r = s.replace(/AXIS2C_HOME/, axis2c_home);
+	s = r;
+}
+while (s.search(/libpwcb\.so/) != -1) {
+	r = s.replace(/libpwcb\.so/, "pwcb.dll");
+	s = r;
+}
+while (s.search(/librdflatfile\.so/) != -1) {
+	r = s.replace(/librdflatfile\.so/, "rdflatfile.dll");
+	s = r;
+}
+while (s.search(/libsctprovider\.so/) != -1) {
+	r = s.replace(/libsctprovider\.so/, "sctprovider.dll");
+	s = r;
+}
+while (s.search(/libsctprovider_hashdb\.so/) != -1) {
+	r = s.replace(/libsctprovider_hashdb\.so/, "sctprovider_hashdb.dll");
+	s = r;
+}
+f.write(s);
+f.close();
--- add-ons/c/adminservices/setup.bat
+++ add-ons/c/adminservices/setup.bat
@@ -53,26 +53,48 @@
 
 @if not exist %carbon_home% goto end_carbon_error
 
-@goto install
+@goto deploy_services
 
-:install
+:deploy_services
 @echo.
 @echo Step 4
 @echo ------ 
 @xcopy /E /Q /I /Y .\services %home%\services 
+@if not errorlevel 0 goto deploy_services_error 
 @echo Installing services ... [OK]
+@goto deploy_modules
+
+:deploy_modules
 @echo.
 @xcopy /E /Q /I /Y .\modules %home%\modules 
+@if not errorlevel 0 goto deploy_modules_error
 @echo Installing modules ... [OK]
+@goto deploy_carbon 
+
+:deploy_carbon
 @echo.
 @xcopy /E /Q /I /Y .\carbon\wso2carbon-2.0.2 %carbon_home%\wso2carbon-2.0.2 
+@if not errorlevel 0 goto deploy_carbon_error
 @echo Installing Carbon console ... [OK]
+@goto customize_services
+
+:customize_services
+@echo.
+@deploy.js %home%\services\SecurityAdminService\policies\scenario1-policy.xml %home%\services\SecurityAdminService\policies\scenario1-policy.xml %home%
+@echo Customizing services ... [OK]
+@goto customize_carbon
+
+:customize_carbon
 @echo.
 @if "%product%"=="1" copy .\carbon\conf\carbon.c.xml %carbon_home%\wso2carbon-2.0.2\conf\carbon.xml
+@if not errorlevel 0 goto customize_carbon_error
 @if "%product%"=="2" copy .\carbon\conf\carbon.cpp.xml %carbon_home%\wso2carbon-2.0.2\conf\carbon.xml
+@if not errorlevel 0 goto customize_carbon_error
 @if "%product%"=="1" copy .\carbon\styles\org.wso2.wsas.styles-3.1.2.c.jar %carbon_home%\wso2carbon-2.0.2\repository\components\plugins\org.wso2.wsas.styles-3.1.2.jar
+@if not errorlevel 0 goto customize_carbon_error
 @if "%product%"=="2" copy .\carbon\styles\org.wso2.wsas.styles-3.1.2.cpp.jar %carbon_home%\wso2carbon-2.0.2\repository\components\plugins\org.wso2.wsas.styles-3.1.2.jar
-@echo Customizing ... [OK]
+@if not errorlevel 0 goto customize_carbon_error
+@echo Customizing Carbon ... [OK]
 @echo.
 @echo Admin installed successfully ...
 @goto end 
@@ -90,6 +112,31 @@
 :end_carbon_error
 @echo.
 @echo [ERROR] The path does not exist
+@goto end
+
+:deploy_services_error
+@echo.
+@echo Installing services ... [ERROR]
+@goto end
+
+:deploy_modules_error
+@echo.
+@echo Installing modules ... [ERROR]
+@goto end
+
+:deploy_carbon_error
+@echo.
+@echo Installing Carbon console ... [ERROR]
+@goto end
+
+:customize_services_error
+@echo.
+@echo Customizing services ... [ERROR]
+@goto end
+
+:customize_carbon_error
+@echo.
+@echo Customizing Carbon ... [ERROR]
 @goto end
 
 :end
