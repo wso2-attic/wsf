ff611b52601f9170dc12ebcff1960d8d633435ef
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2010-03-08 19:29:27 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@57354 a5903396-d722-0410-b921-86c7d4935375

--- c/build.sh
+++ c/build.sh
@@ -9,6 +9,6 @@ export AXIS2C_HOME=`pwd`/deploy
 ./build/copy_build_files.sh
 
 ./autogen.sh
-./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-xpath=/usr/include/libxml2/libxml
+./configure --prefix=`pwd`/deploy --enable-openssl=yes --enable-tests=yes --with-apache2=/usr/local/apache2/include --with-axis2=`pwd`/axis2c/include --with-wsfc=`pwd`/axis2c/include --enable-sandesha=yes --enable-rampart=yes --enable-wsclient=yes --enable-savan=yes --with-xmpp --with-xpath=/usr/include/libxml2/libxml
 make -j 10
 make install
