999619b580189ae8b7872eca9bcfbb712c71a57a
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-04-16 17:07:22 +0000

    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@65778 a5903396-d722-0410-b921-86c7d4935375

--- php/scripts/wsdl/WS_WSDL_Creator.php
+++ php/scripts/wsdl/WS_WSDL_Creator.php
@@ -56,7 +56,7 @@ class WS_WSDL_Creator
      * @param string $ns namespace
      * @param string $wsdl_ver wsdl_version(wsdl1.1 or wsdl2)
      */
-    function __construct($f_arry, $class_arry, $service, $endpoints,
+    function __construct($f_arry, $class_arry, $service, $endpoint,
                          $binding_style,  $ns , $wsdl_ver,
                          $op_arry, $classmap, $annotations, $r_actions, $use_wsa)
     {
@@ -67,7 +67,7 @@ class WS_WSDL_Creator
             $this->namespace = $ns;
         }
 
-        $this->endpoint = $endpoints;
+        $this->endpoint = $endpoint;
         $this->f_arry = $f_arry;
         $this->class_arry = $class_arry;
         $this->service_name = $service;
