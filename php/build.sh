a206a7c0d000c0b63d355d730bb28217047d4ef2
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-05-13 18:48:50 +0000

    build.sh updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@66837 a5903396-d722-0410-b921-86c7d4935375

--- php/build.sh
+++ php/build.sh
@@ -7,6 +7,7 @@ cd wsf_c
 build/copy_build_files.sh
 cd ..
 cp build/wsf_c/axis2c/configure.ac wsf_c/axis2c/
+cp build/wsf_c/axis2c/util/configure.ac wsf_c/axis2c/util/
 cp build/wsf_c/axis2c/axiom/configure.ac wsf_c/axis2c/axiom
 cp build/wsf_c/axis2c/axiom/src/parser/Makefile.am wsf_c/axis2c/axiom/src/parser/
 cp build/wsf_c/axis2c/tools/tcpmon/configure.ac wsf_c/axis2c/tools/tcpmon
