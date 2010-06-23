fa85af11db001986af29813f598cbdc49b14deda
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-06-23 05:52:48 +0000

    build.mk file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@68376 a5903396-d722-0410-b921-86c7d4935375

--- php/src/build.mk
+++ php/src/build.mk
@@ -10,9 +10,12 @@ CFLAGS = /nologo /w /D "WIN32" /D "_WINDOWS" /D "PHP_WIN32" /D "COMPILE_DL_WSF" 
 LDFLAGS = /nologo /LIBPATH:.\..\$(WSFPHP_BIN_DIR)\wsf_c\lib /LIBPATH:$(PHP_BIN_DIR)\dev \
 	  /LIBPATH:$(LIBXML2_BIN_DIR)\lib
 
-LIBS = axutil.lib axis2_engine.lib axis2_parser.lib \
-       axiom.lib libxml2.lib rampart.lib axis2_http_sender.lib \
-       neethi_util.lib neethi.lib sandesha2.lib sandesha2_client.lib libxml2.lib wsock32.lib
+LIBS = axutil.lib axis2_engine.lib axis2_parser.lib axiom.lib rampart.lib axis2_http_sender.lib \
+       neethi_util.lib neethi.lib sandesha2.lib sandesha2_client.lib wsock32.lib
+
+!if "$(ENABLE_LIBXML2)" =="1"
+LIBS=$(LIBS) libxml2.lib
+!endif
 
 INCLUDE_PATH = /I$(PHP_SRC_DIR) /I$(PHP_SRC_DIR)\main /I$(PHP_SRC_DIR)\regex \
 	       /I$(PHP_SRC_DIR)\ext /I$(PHP_SRC_DIR)\sapi /I$(PHP_SRC_DIR)\Zend \
