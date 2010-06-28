799d73a9a717c036d0ef4a766706238f4613a6be
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-06-28 05:47:23 +0000

    clean.bat file updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@68618 a5903396-d722-0410-b921-86c7d4935375

--- php/clean.bat
+++ php/clean.bat
@@ -1,6 +1,6 @@
 @echo off
 @call wsf_c\build\init.bat
-set WSFPHP_VERSION=2.0.0
+set WSFPHP_VERSION=2.1.0
 
 @if exist wsf_c\wso2-wsf-c-bin-%WSFC_VERSION% rmdir /S /Q wsf_c\wso2-wsf-c-bin-%WSFC_VERSION%
 
