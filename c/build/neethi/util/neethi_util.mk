73be8c4b6a7b6a3ed6d1d5004e5df4ec7a5d4e35
nandika <nandika@a5903396-d722-0410-b921-86c7d4935375>
2010-06-28 06:47:36 +0000

    makefile updated
    git-svn-id: http://svn.wso2.org/repos/wso2/trunk/wsf@68624 a5903396-d722-0410-b921-86c7d4935375

--- c/build/neethi/util/neethi_util.mk
+++ c/build/neethi/util/neethi_util.mk
@@ -1,4 +1,5 @@
 !include "..\..\..\..\build\versions.in"
+!include "..\..\..\build\win32\configure.in"
 
 AXIS2_BIN_DIR= ..\..\..\build\axis2c-bin-$(AXIS2C_VERSION)-win32
 
@@ -7,7 +8,7 @@ LDFLAGS = /nologo
 
 
 !if "$(DEBUG)" == "1"
-CFLAGS = $(CFLAGS) /D "_DEBUG" /Od /Z7
+CFLAGS = $(CFLAGS) /D "_DEBUG" /Od /Z7 $(CRUNTIME)d
 LDFLAGS = $(LDFLAGS) /DEBUG /INCREMENTAL
 !else
 CFLAGS = $(CFLAGS) /D "NDEBUG" /O2 $(CRUNTIME)
@@ -16,11 +17,11 @@ LDFLAGS = $(LDFLAGS)
 
 
 neethi_util: copy_include
-	cl.exe $(CFLAGS)  *.C /I$(AXIS2_BIN_DIR)\include /c
+	cl.exe $(CFLAGS)  *.c /I$(AXIS2_BIN_DIR)\include /c
 	link.exe $(LDFLAGS) *.obj /LIBPATH:$(AXIS2_BIN_DIR)\lib axiom.lib axutil.lib \
 	axis2_engine.lib axis2_parser.lib /DLL /OUT:$(AXIS2_BIN_DIR)\lib\neethi_util.dll \
 	/IMPLIB:$(AXIS2_BIN_DIR)\lib\neethi_util.lib
-
+	if exist $(AXIS2_BIN_DIR)\lib\neethi_util.dll.manifest mt.exe -manifest $(AXIS2_BIN_DIR)\lib\neethi_util.dll.manifest -outputresource:$(AXIS2_BIN_DIR)\lib\neethi_util.dll;2
 
 copy_include :
 	copy /Y neethi_options.h .\..\..\..\include\
